b36d8ec25491232ac8bcad9a427d448741edd3ce
Reese Wang <rewang@nvidia.com>
2023-01-05 07:10:35 +0000

    Fix the support matrix on the document
    Signed-off-by: Reese Wang <rewang@nvidia.com>

--- transformer_engine/common/rmsnorm/rmsnorm_api.cpp
+++ transformer_engine/common/rmsnorm/rmsnorm_api.cpp
@@ -19,12 +19,14 @@ input    compute   weights   output
 fp32     fp32      fp32      fp32
 fp16     fp32      fp16      fp16
 bf16     fp32      bf16      bf16
-fp32     fp32      fp16      fp16
-fp32     fp32      bf16      bf16
+fp32     fp32      fp32      fp16
+fp32     fp32      fp32      bf16
+fp32     fp32      fp32      fp8
+fp16     fp32      fp16      fp8
 bf16     fp32      bf16      fp8
 
 Remarks:
-Output type = Weight type
+Input type = Weight type
 Compute always in FP32
 
 */
