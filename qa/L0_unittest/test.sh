6c9ce179222313d93273d1c7c10cb11a5d1bab1f
asfiyab-nvidia <117682710+asfiyab-nvidia@users.noreply.github.com>
2023-01-18 12:45:16 -0800

    Add ONNX export support for TE modules (#41)
    * Add ONNX export support for TE modules (#1)
    
    * Add TorchScript Operators
    * Add symbolic methods to ONNX exporter
    * Add tests for the ONNX export
    
    Signed-off-by: Asfiya Baig <asfiyab@nvidia.com>
    
    * fixes for pylint tests
    
    Signed-off-by: Asfiya Baig <asfiyab@nvidia.com>
    
    * fix pylint warning in softmax.py
    
    Signed-off-by: Asfiya Baig <asfiyab@nvidia.com>
    
    * move FP8 ORT lib inside tests/
    
    Signed-off-by: Asfiya Baig <asfiyab@nvidia.com>
    
    * enable cross attention tests
    
    Signed-off-by: Asfiya Baig <asfiyab@nvidia.com>
    
    * refactor code by @nzmora
    * Increase layernorm FP16 threshold
    * Normalize onnx file names: _ separates configs; - separates words in a single config
    * Add get_attn_mask_str and fix mask string
    * Add missing ONNX files
    * Moved generated ONNX files to tests/gen_onnx_models/
    
    Signed-off-by: Asfiya Baig <asfiyab@nvidia.com>
    
    * fix merge conflict changes
    
    Signed-off-by: Asfiya Baig <asfiyab@nvidia.com>
    
    * fix Q/DQ scale input
    
    Signed-off-by: Asfiya Baig <asfiyab@nvidia.com>
    
    * enable FP16 config when bias is disabled
    
    Signed-off-by: Asfiya Baig <asfiyab@nvidia.com>
    
    * fix pylint check errors
    
    Signed-off-by: Asfiya Baig <asfiyab@nvidia.com>
    
    * updates
    1. remove List import for pylint failure
    2. address comments: remove state tensors from GPU
    3. address comments: Update reverse_map_dtype function and add to namespace
    
    Signed-off-by: Asfiya Baig <asfiyab@nvidia.com>
    
    * minor fix: coding guidelines
    
    Signed-off-by: Asfiya Baig <asfiyab@nvidia.com>
    
    * changes:
    1. skip FP8 tests on  non-hopper devices
    2. minor fix for C++ lint check
    
    Signed-off-by: Asfiya Baig <asfiyab@nvidia.com>
    
    * fix onnxruntime version
    
    Signed-off-by: Asfiya Baig <asfiyab@nvidia.com>
    
    * minor fix: add space between code and comment
    
    Signed-off-by: Asfiya Baig <asfiyab@nvidia.com>
    
    * changes
    1. update copyrights
    2. update path to ORT .so
    
    Signed-off-by: Asfiya Baig <asfiyab@nvidia.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>
    Signed-off-by: asfiyab-nvidia <117682710+asfiyab-nvidia@users.noreply.github.com>
    
    Signed-off-by: Asfiya Baig <asfiyab@nvidia.com>
    Signed-off-by: asfiyab-nvidia <117682710+asfiyab-nvidia@users.noreply.github.com>
    Co-authored-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>

--- qa/L0_unittest/test.sh
+++ qa/L0_unittest/test.sh
@@ -6,5 +6,6 @@ set -e
 
 : ${TE_PATH:=/opt/transformerengine}
 
-pip install pytest==6.2.5
+pip install pytest==6.2.5 onnxruntime==1.13.1
 pytest -v -s $TE_PATH/tests/test_transformerengine.py
+pytest -v -s $TE_PATH/tests/test_onnx_export.py
