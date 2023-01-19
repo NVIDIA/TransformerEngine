a369b5ed920c450016ca8394f0b9fb6141af1eb7
Ryan Jeng <rjeng@nvidia.com>
2023-01-19 00:46:26 -0800

    replace bit_cast with reinterpret_cast
    Signed-off-by: Ryan Jeng <rjeng@nvidia.com>

--- transformer_engine/jax/csrc/utils.h
+++ transformer_engine/jax/csrc/utils.h
@@ -18,23 +18,9 @@
 namespace transformer_engine {
 namespace jax {
 
-template <class To, class From>
-typename std::enable_if<sizeof(To) == sizeof(From) &&
-                            std::is_trivially_copyable<From>::value &&
-                            std::is_trivially_copyable<To>::value,
-                        To>::type
-bit_cast(const From &src) noexcept {
-  static_assert(std::is_trivially_constructible<To>::value,
-                "This implementation additionally requires destination type to "
-                "be trivially constructible");
-  To dst;
-  memcpy(&dst, &src, sizeof(To));
-  return dst;
-}
-
 template <typename T>
 std::string PackDescriptorAsString(const T &descriptor) {
-  return std::string(bit_cast<const char *>(&descriptor), sizeof(T));
+  return std::string(reinterpret_cast<const char *>(&descriptor), sizeof(T));
 }
 
 template <typename T>
@@ -47,12 +33,12 @@ const T *UnpackDescriptor(const char *opaque, std::size_t opaque_len) {
   if (opaque_len != sizeof(T)) {
     throw std::runtime_error("Invalid opaque object size");
   }
-  return bit_cast<const T *>(opaque);
+  return reinterpret_cast<const T *>(opaque);
 }
 
 template <typename T>
 pybind11::capsule EncapsulateFunction(T *fn) {
-  return pybind11::capsule(bit_cast<void *>(fn), "xla._CUSTOM_CALL_TARGET");
+  return pybind11::capsule(reinterpret_cast<void *>(fn), "xla._CUSTOM_CALL_TARGET");
 }
 
 class cublasLtMetaManager {
