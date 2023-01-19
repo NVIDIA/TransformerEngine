94737f2b78f288699415b5fa055f47a3b65c4a21
Kirthi Shankar Sivamani <ksivamani@nvidia.com>
2023-01-19 09:30:20 -0800

    Fix NVTX name for LN backward
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>

--- transformer_engine/common/layer_norm/ln_api.cpp
+++ transformer_engine/common/layer_norm/ln_api.cpp
@@ -403,7 +403,7 @@ void nvte_layernorm_bwd(const NVTETensor dz,       // BxSxhidden_size
                         const int multiprocessorCount,
                         NVTETensor workspace,
                         NVTETensor barrier) {
-  NVTE_API_CALL(nvte_layernorm_fwd);
+  NVTE_API_CALL(nvte_layernorm_bwd);
   using namespace transformer_engine;
   layernorm_bwd(*reinterpret_cast<const Tensor*>(dz),
                 *reinterpret_cast<const Tensor*>(x),
