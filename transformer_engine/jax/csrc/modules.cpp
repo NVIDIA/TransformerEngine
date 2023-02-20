80fdb305718c504789c8f7fafe44c79e8318948c
Ryan Jeng <rjeng@nvidia.com>
2023-02-20 08:45:05 -0800

    fix license
    Signed-off-by: Ryan Jeng <rjeng@nvidia.com>

--- transformer_engine/jax/csrc/modules.cpp
+++ transformer_engine/jax/csrc/modules.cpp
@@ -1,5 +1,5 @@
 /*************************************************************************
- * Copyright (c) 2022-2023, NVIDIA CORPORATION & AFFILIATES. All rights reservedesc.
+ * Copyright (c) 2022-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  *
  * See LICENSE for license information.
  ************************************************************************/
