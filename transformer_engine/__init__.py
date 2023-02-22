c7b4390f2df10da5e5a39e711329bec5a5008e29
Jeng Bai-Cheng <jeng1220@users.noreply.github.com>
2023-02-22 10:44:50 +0800

    Update transformer_engine/__init__.py
    Co-authored-by: Tim Moon <4406448+timmoon10@users.noreply.github.com>
    Signed-off-by: Jeng Bai-Cheng <jeng1220@users.noreply.github.com>

--- transformer_engine/__init__.py
+++ transformer_engine/__init__.py
@@ -9,20 +9,12 @@
 from . import common
 
 
-def generate_error_msh(framework):
-    return f"Skip importing transformer_engine.{framework}," \
-           f" since that module is not found!" \
-           f" Please ignore this if transformer_engine.{framework}" \
-           f" is no needed, otherwise make sure transformer_engine is" \
-           f" installed via \"FRAMEWORK={framework}\" or \"FRAMEWORK=all\""
-
-
 try:
     from . import pytorch
 except ImportError as e:
-    print(generate_error_msh("pytorch"))
+    pass
 
 try:
     from . import jax
 except ImportError as e:
-    print(generate_error_msh("jax"))
+    pass
