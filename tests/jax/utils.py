Fix the wrong broadcasting dim to dropout masks when enable transpose_bs.
Signed-off-by: Ryan Jeng <rjeng@nvidia.com>

tests/jax/utils.py
@@ -804,7 +804,7 @@ class EncoderLayer(nn.Module):
                                                  encoder_mask,
                                                  encoder_bias,
                                                  deterministic=deterministic)
-        x = nn.Dropout(rate=self.dropout_rate, broadcast_dims=(-2,))(x, deterministic=deterministic)
+        x = nn.Dropout(rate=self.dropout_rate, broadcast_dims=(sequence_dim,))(x, deterministic=deterministic)
         if self.drop_path > 0.0:
             drop_path_shape = _generate_drop_path_shape(x.shape, batch_dim)
             x = nn.Dropout(rate=self.drop_path,
@@ -830,7 +830,7 @@ class EncoderLayer(nn.Module):
             fuse_wi=self.fuse_mlp_wi,
             name='mlp',
         )(y, deterministic=deterministic)
-        y = nn.Dropout(rate=self.dropout_rate, broadcast_dims=(-2,))(y, deterministic=deterministic)
+        y = nn.Dropout(rate=self.dropout_rate, broadcast_dims=(sequence_dim,))(y, deterministic=deterministic)
         if self.drop_path > 0.0:
             drop_path_shape = _generate_drop_path_shape(y.shape, batch_dim)
             y = nn.Dropout(rate=self.drop_path,
@@ -920,7 +920,7 @@ class DecoderLayer(nn.Module):
                                                       decoder_bias,
                                                       deterministic=deterministic,
                                                       decode=decode)
-        x = nn.Dropout(rate=self.dropout_rate, broadcast_dims=(-2,))(x, deterministic=deterministic)
+        x = nn.Dropout(rate=self.dropout_rate, broadcast_dims=(sequence_dim,))(x, deterministic=deterministic)
         if self.drop_path > 0.0:
             drop_path_shape = _generate_drop_path_shape(x.shape, batch_dim)
             x = nn.Dropout(rate=self.drop_path,
@@ -948,7 +948,7 @@ class DecoderLayer(nn.Module):
                                                                  encoded,
                                                                  encoder_decoder_mask,
                                                                  deterministic=deterministic)
-        y = nn.Dropout(rate=self.dropout_rate, broadcast_dims=(-2,))(y, deterministic=deterministic)
+        y = nn.Dropout(rate=self.dropout_rate, broadcast_dims=(sequence_dim,))(y, deterministic=deterministic)
         y = y + residual
 
         # MLP block.
@@ -967,7 +967,7 @@ class DecoderLayer(nn.Module):
             fuse_wi=self.fuse_mlp_wi,
             name='mlp',
         )(z, deterministic=deterministic)
-        z = nn.Dropout(rate=self.dropout_rate, broadcast_dims=(-2,))(z, deterministic=deterministic)
+        z = nn.Dropout(rate=self.dropout_rate, broadcast_dims=(sequence_dim,))(z, deterministic=deterministic)
         if self.drop_path > 0.0:
             drop_path_shape = _generate_drop_path_shape(z.shape, batch_dim)
             z = nn.Dropout(rate=self.drop_path,

transformer_engine/jax/transformer.py
@@ -790,11 +790,12 @@ class TransformerLayer(nn.Module):
                     l = inputs.shape[sequence_dim]
                 attn_bias = rel_emb(l, l, False)
 
+        assert inputs.ndim == 3
+
         self_attn_type = None
         # Make name be the exactly same as T5X, since names would affect
         # RNGKey during init and apply. Myabe no need in the feature.
         if self.layer_type == TransformerLayerType.ENCODER:
-            assert inputs.ndim == 3
             mha_name = 'attention'
             self_attn_type = AttentionType.PADDING
         else:
@@ -829,7 +830,7 @@ class TransformerLayer(nn.Module):
                            deterministic=deterministic,
                            decode=decode)
 
-        x = nn.Dropout(rate=self.hidden_dropout, broadcast_dims=(-2,))(x,
+        x = nn.Dropout(rate=self.hidden_dropout, broadcast_dims=(sequence_dim,))(x,
                                                                        deterministic=deterministic)
         if self.drop_path > 0.0:
             drop_path_shape = _generate_drop_path_shape(x.shape, batch_dim)
@@ -867,7 +868,7 @@ class TransformerLayer(nn.Module):
                                                   encoder_decoder_mask,
                                                   deterministic=deterministic)
             y = nn.Dropout(rate=self.hidden_dropout,
-                           broadcast_dims=(-2,))(y, deterministic=deterministic)
+                           broadcast_dims=(sequence_dim,))(y, deterministic=deterministic)
             mlp_input = y + residual
 
         # MlpBlock
@@ -895,7 +896,7 @@ class TransformerLayer(nn.Module):
             assert ln_out is not None
             residual = ln_out
 
-        z = nn.Dropout(rate=self.hidden_dropout, broadcast_dims=(-2,))(z,
+        z = nn.Dropout(rate=self.hidden_dropout, broadcast_dims=(sequence_dim,))(z,
                                                                        deterministic=deterministic)
         if self.drop_path > 0.0:
             drop_path_shape = _generate_drop_path_shape(z.shape, batch_dim)