9b7c98a5813d1cae006a95459fb4649e0725a640
Ming Huang <mingh@nvidia.com>
2023-03-10 02:01:47 +0000

    Updating license and exporting more modules
    Signed-off-by: Ming Huang <mingh@nvidia.com>

--- transformer_engine/jax/__init__.py
+++ transformer_engine/jax/__init__.py
@@ -3,8 +3,9 @@
 # See LICENSE for license information.
 """Transformer Engine bindings for JAX"""
 from .fp8 import fp8_autocast, update_collections, update_fp8_metas
-from .module import DenseGeneral, LayerNormDenseGeneral, LayerNormMLP, TransformerEngineBase
+from .module import DenseGeneral, LayerNorm
+from .module import LayerNormDenseGeneral, LayerNormMLP, TransformerEngineBase
 from .transformer import extend_logical_axis_rules
-from .transformer import RelativePositionBiases, MultiHeadAttention
+from .transformer import MultiHeadAttention, RelativePositionBiases
 from .transformer import TransformerLayer, TransformerLayerType
-from .sharding import ShardingResource
+from .sharding import MajorShardingType, ShardingResource, ShardingType
