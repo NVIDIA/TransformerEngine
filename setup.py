b1f6327de128cc59e00e73c0336dd8173ddb2396
Vasudevan Rengasamy <vrengasamy@nvidia.com>
2023-03-16 22:14:22 -0700

    cleanup

--- setup.py
+++ setup.py
@@ -87,7 +87,6 @@ pytorch_sources = [
     "transformer_engine/pytorch/csrc/extensions.cu",
     "transformer_engine/pytorch/csrc/common.cu",
     "transformer_engine/pytorch/csrc/ts_fp8_op.cpp",
-#    "transformer_engine/pytorch/csrc/comm_gemm_overlap.cu",
 ]
 pytorch_sources = make_abs_path(pytorch_sources)
 
@@ -160,7 +159,7 @@ class PyTorchBuilder(FrameworkBuilderBase):
 
     @staticmethod
     def install_requires():
-        return []#"flash-attn @ git+https://github.com/ksivaman/flash-attention.git@hopper",]
+        return ["flash-attn @ git+https://github.com/ksivaman/flash-attention.git@hopper",]
 
 class JaxBuilder(FrameworkBuilderBase):
     def cmake_flags(self):
