23b172d6240a93727bdef944a9aaa6f8f883cd17
Kirthi Shankar Sivamani <ksivamani@nvidia.com>
2023-03-21 13:51:47 -0700

    Change FP8 recipe defaults
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>

--- transformer_engine/pytorch/module.py
+++ transformer_engine/pytorch/module.py
@@ -13,6 +13,7 @@ from contextlib import contextmanager
 
 import numpy as np
 import torch
+import torch.nn.functional as F
 from torch.nn.parameter import Parameter
 from torch.nn import init
 
@@ -176,7 +177,7 @@ class TransformerEngineBaseModule(torch.nn.Module, ABC):
         self.fp8_meta = {}
         self.fp8_meta["fp8_group"] = None
         self.fp8_meta["recipe"] = get_default_fp8_recipe()
-        self.fp8_meta_tensors_initialized = False
+        self.fp8_checkpoint_loaded = False
         self.tp_group = None
         self.tp_group_initialized = False
         self.tp_size = 1
@@ -187,6 +188,23 @@ class TransformerEngineBaseModule(torch.nn.Module, ABC):
     def set_meta_tensor(self, fwd: bool) -> None:
         """Init scales and amaxes for fwd | bwd."""
         fp8_meta_tensor_key = "scaling_fwd" if fwd else "scaling_bwd"
+
+        if self.fp8_checkpoint_loaded:
+            # Handle different amax history size from that of the loaded checkpoint.
+            curr_len = self.fp8_meta[fp8_meta_tensor_key].amax_history.shape[0]
+            need_len = self.fp8_meta["recipe"].amax_history_len
+            if need_len < curr_len:
+                self.fp8_meta[fp8_meta_tensor_key].amax_history = (
+                    self.fp8_meta[fp8_meta_tensor_key]
+                    .amax_history[: self.fp8_meta["recipe"].amax_history_len].clone()
+                )
+            elif need_len > curr_len:
+                extra_rows = need_len - curr_len
+                self.fp8_meta[fp8_meta_tensor_key].amax_history = F.pad(
+                    self.fp8_meta[fp8_meta_tensor_key].amax_history, pad=(0, 0, 0, extra_rows)
+                )
+            return
+
         # Max. number of fp8 tensors per GEMM = 3 (input, weight, output) for fwd and
         # 2 (grad_output and grad_input) for bwd
         num_fp8_tensors = (
@@ -222,10 +240,6 @@ class TransformerEngineBaseModule(torch.nn.Module, ABC):
 
     def init_fp8_meta_tensors(self) -> None:
         """Init scales and amaxes."""
-        # Checkpoint loaded
-        if self.fp8_meta_tensors_initialized:
-            return
-
         self.set_meta_tensor(True)
         self.set_meta_tensor(False)
 
@@ -280,7 +294,7 @@ class TransformerEngineBaseModule(torch.nn.Module, ABC):
             self.fp8_meta["scaling_fwd"].amax_history.copy_(amax_history_fwd)
             self.fp8_meta["scaling_bwd"].scale.copy_(scale_bwd)
             self.fp8_meta["scaling_bwd"].amax_history.copy_(amax_history_bwd)
-            self.fp8_meta_tensors_initialized = True
+            self.fp8_checkpoint_loaded = True
 
             # Restore global FP8 buffer state.
             set_global_fp8_buffer(state[4])
@@ -310,7 +324,7 @@ class TransformerEngineBaseModule(torch.nn.Module, ABC):
         self.fp8_meta["scaling_fwd"].amax_history.copy_(state["amax_history_fwd"])
         self.fp8_meta["scaling_bwd"].scale.copy_(state["scale_bwd"])
         self.fp8_meta["scaling_bwd"].amax_history.copy_(state["amax_history_bwd"])
-        self.fp8_meta_tensors_initialized = True
+        self.fp8_checkpoint_loaded = True
 
     def set_activation_dtype(self, inp: torch.Tensor) -> None:
         """Get activation data type for AMP."""
