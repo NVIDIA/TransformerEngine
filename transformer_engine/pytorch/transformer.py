657e8c2cb46cbd1dc30ce90b3db59b717eeb7c1b
Kirthi Shankar Sivamani <ksivamani@nvidia.com>
2023-03-27 20:52:15 +0000

    Fix FA version checking
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>

--- transformer_engine/pytorch/transformer.py
+++ transformer_engine/pytorch/transformer.py
@@ -7,6 +7,7 @@ import os
 import re
 import math
 import warnings
+from importlib.metadata import version
 from contextlib import nullcontext
 from typing import Any, Callable, Optional, Tuple, Union
 
@@ -42,7 +43,7 @@ from transformer_engine.pytorch.distributed import (
     checkpoint,
 )
 
-_flash_attn_version = re.search("Version: (.*)", os.popen("pip show flash_attn").read()).group(1)
+_flash_attn_version = version("flash-attn")
 warnings.filterwarnings("module", category=DeprecationWarning, module="transformer")
 
 
