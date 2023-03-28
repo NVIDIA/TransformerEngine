dd6001f5ddb531d74b36c02a2c7465ca72214544
Jeng Bai-Cheng <jeng1220@users.noreply.github.com>
2023-03-29 01:39:20 +0800

    [JAX] Add TE examples (#108)
    * refactor JAX examples
    
    Signed-off-by: Ryan Jeng <rjeng@nvidia.com>
    
    * fix doc-string
    
    Signed-off-by: Ryan Jeng <rjeng@nvidia.com>
    
    * add dp example
    
    Signed-off-by: Ryan Jeng <rjeng@nvidia.com>
    
    * refactor
    
    Signed-off-by: Ryan Jeng <rjeng@nvidia.com>
    
    * fix params_axes_pspec
    
    Signed-off-by: Ryan Jeng <rjeng@nvidia.com>
    
    * Add model parallel example and refactor
    Update readme
    
    Signed-off-by: Ryan Jeng <rjeng@nvidia.com>
    
    * align code and readme
    
    Signed-off-by: Ryan Jeng <rjeng@nvidia.com>
    
    * update verification
    
    Signed-off-by: Ryan Jeng <rjeng@nvidia.com>
    
    * add mask
    
    Signed-off-by: Ryan Jeng <rjeng@nvidia.com>
    
    * num_gpu is configurable
    
    Signed-off-by: Ryan Jeng <rjeng@nvidia.com>
    
    * update readme
    
    Signed-off-by: Ryan Jeng <rjeng@nvidia.com>
    
    * update readme
    
    Signed-off-by: Ryan Jeng <rjeng@nvidia.com>
    
    * solvepylint issue
    
    Signed-off-by: Ryan Jeng <rjeng@nvidia.com>
    
    * ignore markdown and txt file from license check
    
    Signed-off-by: Ryan Jeng <rjeng@nvidia.com>
    
    * Update README.md
    
    Signed-off-by: Ryan Jeng <rjeng@nvidia.com>
    
    * add flax into requirements.txt
    
    Signed-off-by: Ryan Jeng <rjeng@nvidia.com>
    
    ---------
    
    Signed-off-by: Ryan Jeng <rjeng@nvidia.com>
    Signed-off-by: Charlene Yang <charleney@nvidia.com>

--- qa/L0_jax_unittest/test.sh
+++ qa/L0_jax_unittest/test.sh
@@ -6,4 +6,7 @@ set -xe
 
 : ${TE_PATH:=/opt/transformerengine}
 pytest -Wignore -v $TE_PATH/tests/jax
+
+pip install -r $TE_PATH/examples/jax/mnist/requirements.txt
+pip install -r $TE_PATH/examples/jax/encoder/requirements.txt
 pytest -Wignore -v $TE_PATH/examples/jax
