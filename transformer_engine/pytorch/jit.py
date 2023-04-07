ae521b226f8a142c50bad16c8ec92ad9b4bdd28a
Kirthi Shankar Sivamani <ksivamani@nvidia.com>
2023-04-07 11:57:27 -0700

    jit fusion improvement
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>

--- transformer_engine/pytorch/jit.py
+++ transformer_engine/pytorch/jit.py
@@ -3,7 +3,7 @@
 # See LICENSE for license information.
 
 """NVFuser functions and JIT utilities"""
-from typing import Callable, Tuple
+from typing import Callable, Optional, Tuple
 import torch
 
 
@@ -34,6 +34,7 @@ def bias_gelu_fused_(inp: torch.Tensor, bias: torch.Tensor) -> torch.Tensor:
     """Bias-GeLU fused"""
     x = inp + bias
     return x * 0.5 * (1.0 + torch.tanh(0.79788456 * x * (1 + 0.044715 * x * x)))
+
 
 @torch.jit.script
 def gelu_fused_(inp: torch.Tensor) -> torch.Tensor:
@@ -90,12 +91,12 @@ def bias_gelu_fused(inp: torch.Tensor, bias: torch.Tensor) -> torch.Tensor:
 
 def bgrad_dgelu_fused(
     grad_output: torch.Tensor, inp: torch.Tensor, bias: torch.Tensor
-) -> Tuple[torch.Tensor, torch.Tensor]:
+) -> Tuple[Optional[torch.Tensor], torch.Tensor]:
     """Disable native AMP for `bgrad_dgelu_fused_`"""
     with torch.cuda.amp.autocast(enabled=False):
         if bias.numel() != 0:
             return bgrad_dgelu_fused_(grad_output, inp, bias)
-        return torch.zeros_like(bias), dgelu_fused_(grad_output, inp)
+        return None, dgelu_fused_(grad_output, inp)
 
 
 def bias_dropout_add(
