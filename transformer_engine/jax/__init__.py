e3419c7d06fbd96d45663a9693becb9a231e53a7
Ming Huang <mingh@nvidia.com>
2023-04-24 05:05:30 +0000

    Adjust Module Structure.
    1. Collect Flax related modules to a sub-folder, flax.
    2. Add a function to unify scale_init for zero-centered-gamma LN.
    
    Signed-off-by: Ming Huang <mingh@nvidia.com>

--- transformer_engine/jax/__init__.py
+++ transformer_engine/jax/__init__.py
@@ -3,9 +3,4 @@
 # See LICENSE for license information.
 """Transformer Engine bindings for JAX"""
 from .fp8 import fp8_autocast, update_collections, update_fp8_metas, get_delayed_scaling
-from .module import DenseGeneral, LayerNorm
-from .module import LayerNormDenseGeneral, LayerNormMLP, TransformerEngineBase
-from .transformer import extend_logical_axis_rules
-from .transformer import MultiHeadAttention, RelativePositionBiases
-from .transformer import TransformerLayer, TransformerLayerType
 from .sharding import MajorShardingType, ShardingResource, ShardingType
