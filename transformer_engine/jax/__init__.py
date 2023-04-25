085f70be085ebde7bd2685675c04a54dfe5b74cc
Ming Huang <mingh@nvidia.com>
2023-04-25 03:36:49 +0000

    Make changes be compatible to previous versions.
    Signed-off-by: Ming Huang <mingh@nvidia.com>

--- transformer_engine/jax/__init__.py
+++ transformer_engine/jax/__init__.py
@@ -2,5 +2,10 @@
 #
 # See LICENSE for license information.
 """Transformer Engine bindings for JAX"""
+from .flax import DenseGeneral, LayerNorm
+from .flax import LayerNormDenseGeneral, LayerNormMLP, TransformerEngineBase
+from .flax import extend_logical_axis_rules
+from .flax import MultiHeadAttention, RelativePositionBiases
+from .flax import TransformerLayer, TransformerLayerType
 from .fp8 import fp8_autocast, update_collections, update_fp8_metas, get_delayed_scaling
 from .sharding import MajorShardingType, ShardingResource, ShardingType
