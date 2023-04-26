8faaa5ece7340f4b6cd411ee544d6022cebfc2d8
Ming Huang <mingh@nvidia.com>
2023-04-26 05:11:01 +0000

    Adding deprecated warning to flax modules which imported via transformer_engine.jax
    Signed-off-by: Ming Huang <mingh@nvidia.com>

--- transformer_engine/jax/__init__.py
+++ transformer_engine/jax/__init__.py
@@ -2,10 +2,41 @@
 #
 # See LICENSE for license information.
 """Transformer Engine bindings for JAX"""
-from .flax import DenseGeneral, LayerNorm
-from .flax import LayerNormDenseGeneral, LayerNormMLP, TransformerEngineBase
-from .flax import extend_logical_axis_rules
-from .flax import MultiHeadAttention, RelativePositionBiases
-from .flax import TransformerLayer, TransformerLayerType
+
+from . import flax
 from .fp8 import fp8_autocast, update_collections, update_fp8_metas, get_delayed_scaling
 from .sharding import MajorShardingType, ShardingResource, ShardingType
+from ..common.utils import deprecate_wrapper
+
+extend_logical_axis_rules = deprecate_wrapper(
+    flax.extend_logical_axis_rules,
+    "extend_logical_axis_rules is moving to transformer_engine.jax.flax module")
+DenseGeneral = deprecate_wrapper(flax.DenseGeneral,
+                                 "DenseGeneral is moving to transformer_engine.jax.flax module")
+LayerNorm = deprecate_wrapper(flax.LayerNorm,
+                              "LayerNorm is moving to transformer_engine.jax.flax module")
+LayerNormDenseGeneral = deprecate_wrapper(
+    flax.LayerNormDenseGeneral,
+    "LayerNormDenseGeneral is moving to transformer_engine.jax.flax module")
+LayerNormMLP = deprecate_wrapper(flax.LayerNormMLP,
+                                 "LayerNormMLP is moving to transformer_engine.jax.flax module")
+TransformerEngineBase = deprecate_wrapper(
+    flax.TransformerEngineBase,
+    "TransformerEngineBase is moving to transformer_engine.jax.flax module")
+MultiHeadAttention = deprecate_wrapper(
+    flax.MultiHeadAttention, "MultiHeadAttention is moving to transformer_engine.jax.flax module")
+RelativePositionBiases = deprecate_wrapper(
+    flax.RelativePositionBiases,
+    "RelativePositionBiases is moving to transformer_engine.jax.flax module")
+TransformerLayer = deprecate_wrapper(
+    flax.TransformerLayer, "TransformerLayer is moving to transformer_engine.jax.flax module")
+TransformerLayerType = deprecate_wrapper(
+    flax.TransformerLayerType,
+    "TransformerLayerType is moving to transformer_engine.jax.flax module")
+
+__all__ = [
+    'fp8_autocast', 'update_collections', 'update_fp8_metas', 'get_delayed_scaling',
+    'MajorShardingType', 'ShardingResource', 'ShardingType', 'flax', 'DenseGeneral', 'LayerNorm',
+    'LayerNormDenseGeneral', 'LayerNormMLP', 'TransformerEngineBase', 'MultiHeadAttention',
+    'RelativePositionBiases', 'TransformerLayer', 'TransformerLayerType'
+]
