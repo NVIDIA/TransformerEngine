b893dca8d5d872ed3a73eb0f5316e68140ee51e4
Przemek Tredak <ptredak@nvidia.com>
2023-04-27 12:01:38 -0700

    Remove the nonexistent parameter from fused attention documentation
    Signed-off-by: Przemek Tredak <ptredak@nvidia.com>

--- transformer_engine/common/include/transformer_engine/fused_attn.h
+++ transformer_engine/common/include/transformer_engine/fused_attn.h
@@ -133,7 +133,6 @@ void nvte_fused_attn_fwd_qkvpacked(
  *  \param[in]     Aux_CTX_Tensors       Auxiliary tensors from forward when in training mode.
  *  \param[out]    dQKV                  The gradient of the QKV tensor.
  *  \param[in]     cu_seqlens            Accumulative sequence lengths, [batch_size + 1].
- *  \param[in]     rng_state             Seed and offset of CUDA random number generator.
  *  \param[in]     max_seqlen            Max sequence length used for computing,
  *                                       it may be >= max(cu_seqlens). 
  *  \param[in]     attn_scale            Scaling factor for Q * K.T.
