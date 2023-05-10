a5e2c4ba880bd12423f48ec95ea31e79fb9839be
Neta Zmora <nzmora@nvidia.com>
2023-05-10 22:11:56 +0000

    Dynamically-generated causal attention mask (for ONNX export)
    TE's default causal mask is square (seq_len, seq_len) and is
    dynamically allocated for different sequence sizes. Dynamic
    allocation and dictionary lookups are not supported by ONNX.
    GPT generative phase uses rectangular masks.
    
    This commit forces softmax to use `forward_torch_softmax` and
    to dynamically generate an attention mask when exporting to ONNX.
    The mask is generated w/o using conditional control-flow by generating
    a  (k_seq_len, k_seq_len) mask and slicing it to (q_seq_len, k_seq_len)
    
    An alternate implementation is to pre-allocate a mask of shape
    (max_seq, max_seq) and to slice that. This solution is more performant
    at the expense of space, but the problem is the TE doesn't have a concept
    of max_seq.
    
    * Add to test_export_softmax a test for te.softmax.FusedScaleMaskSoftmax.
    * Add test_softmax_mask_fn to test that TE's default attention mask and
    the new ONNX-compatible mask produce the same behavior.
    * Add test_export_gpt_generation to test that the ONNX model can correctly
    handle inputs with different shapes and that the attention mask it adjusted
    on-the-fly to different sequence lengths.
    
    Misc:
    * Add a PRNG seeding fixture for more stability in tests.
    * Add dynamic shapes for ONNX input/output tests.
    * Allow validate_result to compare ORT output to pre-computed TE outputs.
    
    Signed-off-by: Neta Zmora <nzmora@nvidia.com>

--- transformer_engine/pytorch/softmax.py
+++ transformer_engine/pytorch/softmax.py
@@ -9,6 +9,7 @@ import torch
 from torch import nn
 import torch._C._onnx as _C_onnx
 from torch.onnx import _type_utils
+from transformer_engine.pytorch.export import is_in_onnx_export_mode
 
 import transformer_engine_extensions as tex
 
@@ -23,6 +24,17 @@ def _get_default_causal_mask(sq: int) -> torch.Tensor:
     if sq not in _default_causal_mask:
         _default_causal_mask[sq] = torch.triu(torch.ones(sq, sq, device="cuda"), diagonal=1).bool()
     return _default_causal_mask[sq]
+
+
+def _get_onnx_export_causal_mask(seq_q: int, seq_k: int, onnx_causal_mask) -> torch.Tensor:
+    """Return the causal upper triangular mask for softmax input, for ONNX export.
+
+    ONNX does not support dynamic control-flow and requires non-square masks when
+    using a KV-cache (seq_k's length len(context)+len(generative) while seq_q's length is 1).
+    """
+    onnx_causal_mask = torch.triu(torch.ones(seq_k, seq_k, device="cuda"), diagonal=1).bool()
+    derived_mask = onnx_causal_mask[seq_k-seq_q:seq_k, :seq_k].bool()
+    return derived_mask
 
 
 class ScaledUpperTriangMaskedSoftmax(torch.autograd.Function):
@@ -231,7 +243,7 @@ class FusedScaleMaskSoftmax(nn.Module):
             scale is None or self.softmax_in_fp32
         ), "softmax should be in fp32 when scaled"
 
-        if self.is_kernel_available(*inp.size()):
+        if self.is_kernel_available(*inp.size()) and not is_in_onnx_export_mode():
             return self.forward_fused_softmax(inp, mask, scale)
         return self.forward_torch_softmax(inp, mask, scale)
 
@@ -287,7 +299,11 @@ class FusedScaleMaskSoftmax(nn.Module):
             inp = inp * scale
 
         if self.attn_mask_type == "causal":
-            mask = _get_default_causal_mask(inp.size()[2])
+            if is_in_onnx_export_mode():
+                seq_len_q, seq_len_k = inp.size(2), inp.size(3)
+                mask = _get_onnx_export_causal_mask(seq_len_q, seq_len_k, None)
+            else:
+                mask = _get_default_causal_mask(inp.size(2))
 
         mask_output = self.mask_func(inp, mask) if mask is not None else inp
         probs = torch.nn.Softmax(dim=-1)(mask_output)
