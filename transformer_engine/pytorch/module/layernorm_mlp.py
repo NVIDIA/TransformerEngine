80825fde111cca493240f3217030ee22135a285a
Sudhakar Singh <sudhakars@nvidia.com>
2023-06-01 14:10:07 -0700

    Don't save fp8 weight tensors if `is_first_microbatch` is None (#244)
    * extend fp8 weight placeholders logic for Linear, LNLinear, LNMLP
    
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * Update transformer_engine/pytorch/module/base.py
    
    Co-authored-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * Update transformer_engine/pytorch/module/base.py
    
    Co-authored-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * Update transformer_engine/pytorch/module/base.py
    
    Co-authored-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * Update transformer_engine/pytorch/module/base.py
    
    Co-authored-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * Update transformer_engine/pytorch/module/base.py
    
    Co-authored-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * Update transformer_engine/pytorch/module/layernorm_linear.py
    
    Co-authored-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * Update transformer_engine/pytorch/module/layernorm_mlp.py
    
    Co-authored-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * Update transformer_engine/pytorch/module/linear.py
    
    Co-authored-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * Update linear.py
    
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * Update layernorm_linear.py
    
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * Update layernorm_mlp.py
    
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * lint
    
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>
    
    ---------
    
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>
    Co-authored-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>

--- transformer_engine/pytorch/module/layernorm_mlp.py
+++ transformer_engine/pytorch/module/layernorm_mlp.py
@@ -4,7 +4,7 @@
 
 """LayerNormMLP API"""
 import os
-from typing import Union, Optional, Callable, Tuple, Dict, Any
+from typing import Union, Optional, Callable, Tuple, List, Dict, Any
 
 import torch
 from torch.nn.parameter import Parameter
@@ -1063,6 +1063,31 @@ class LayerNormMLP(TransformerEngineBaseModule):
             init.zeros_(self.layer_norm_weight)
         init.zeros_(self.layer_norm_bias)
 
+    def get_fp8_weights_scratchpad(
+        self,
+        is_first_microbatch: Union[bool, None],
+    ) -> List[torch.Tensor]:
+        """
+        Fetch the fp8 weight tensor placeholders if they exist (when
+        `is_first_microbatch` is not `None`) or return empty fp8 weight
+        tensors (if `is_first_microbatch is None`)
+        """
+        if not self.fp8:
+            return [None, None, None, None]
+
+        if is_first_microbatch is None:
+            # Return empty weight placeholders for each fwd/bwd pass
+            fp8_weight_tensors = self.get_fp8_weights_empty_tensors(
+                is_first_microbatch
+            )
+        else:
+            # These persistent weight placeholders should've been created in
+            # `set_fp8_weights` method
+            fp8_weight_tensors = [self.weight1_fp8, self.weight1_t_fp8,
+                                  self.weight2_fp8, self.weight2_t_fp8]
+
+        return fp8_weight_tensors
+
     def forward(
         self, inp: torch.Tensor, is_first_microbatch: Optional[bool] = None
     ) -> Union[torch.Tensor, Tuple[torch.Tensor, ...]]:
@@ -1089,6 +1114,12 @@ class LayerNormMLP(TransformerEngineBaseModule):
         """
 
         with self.prepare_forward(inp, is_first_microbatch, num_gemms=2) as inp:
+            # Fetch the fp8 weights placeholders (for linear/gemm)
+            weight1_fp8, weight1_t_fp8, weight2_fp8, weight2_t_fp8 = \
+                self.get_fp8_weights_scratchpad(
+                        is_first_microbatch
+                )
+
             if torch.is_grad_enabled():
                 fwd_fn = _LayerNormMLP.apply
                 args = []
@@ -1100,13 +1131,13 @@ class LayerNormMLP(TransformerEngineBaseModule):
                 self.layer_norm_weight,
                 self.layer_norm_bias,
                 self.fc1_weight,
-                self.weight1_fp8 if self.fp8 else None,
-                self.weight1_t_fp8 if self.fp8 else None,
+                weight1_fp8,
+                weight1_t_fp8,
                 self.fc1_bias,
                 self.use_bias,
                 self.fc2_weight,
-                self.weight2_fp8 if self.fp8 else None,
-                self.weight2_t_fp8 if self.fp8 else None,
+                weight2_fp8,
+                weight2_t_fp8,
                 self.fc2_bias,
                 self.apply_bias and not self.gemm_bias_unfused_add,
                 self.eps,
