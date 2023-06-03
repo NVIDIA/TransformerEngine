b434a11cfefe14798e1350249cc92a23acf68203
Charlene Yang <8636796+cyanguwa@users.noreply.github.com>
2023-06-03 14:30:22 -0700

    fix lint
    Signed-off-by: Charlene Yang <8636796+cyanguwa@users.noreply.github.com>

--- transformer_engine/common/include/transformer_engine/transformer_engine.h
+++ transformer_engine/common/include/transformer_engine/transformer_engine.h
@@ -134,9 +134,12 @@ float *nvte_tensor_scale_inv(const NVTETensor tensor);
     \brief Pack of tensors, generally used for auxiliary outputs.
  */
 struct NVTETensorPack {
-  static const int MAX_SIZE = 10;  /*!< Max number of tensors in the pack. Assumed <= 10. */
-  NVTETensor tensors[MAX_SIZE];  /*!< Wrappers of tensors. They do not hold the associated memory. */
-  size_t size = 0;  /*!< Actual number of tensors in the pack, 0 <= size <= MAX_SIZE. */
+  /*! Max number of tensors in the pack. Assumed <= 10. */
+  static const int MAX_SIZE = 10;
+  /*! Wrappers of tensors. They do not hold the associated memory. */
+  NVTETensor tensors[MAX_SIZE];
+  /*! Actual number of tensors in the pack, 0 <= size <= MAX_SIZE. */
+  size_t size = 0;
 };
 
 /*! \brief Create `tensors` in NVTETensorPack.
