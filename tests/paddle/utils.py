207b231e0bce2b3f4550210bd2874d25bc710ee4
Tian Zheng <murkydepth@163.com>
2023-06-07 00:13:43 +0800

    First step of PaddlePaddle integration (#249)
    * First step of PaddlePaddle integration
    - Add build option for paddle
    - Add basic test framework
    - Add 3 basic operators: cast_from_fp8, cast_to_fp8, gemm
    
    Signed-off-by: Tian Zheng <tizheng@nvidia.com>
    Signed-off-by: Tian Zheng (Engrg-Hardware 1) <tizheng@nvidia.com>
    
    * Fix review comments
    
    Signed-off-by: Tian Zheng (Engrg-Hardware 1) <tizheng@nvidia.com>
    
    * Support paddle build
    
    Signed-off-by: Tian Zheng (Engrg-Hardware 1) <tizheng@nvidia.com>
    
    * Add paddle build support for new building framework
    
    Signed-off-by: Tian Zheng (Engrg-Hardware 1) <tizheng@nvidia.com>
    
    * Fix review comments
    
    Signed-off-by: Tian Zheng (Engrg-Hardware 1) <tizheng@nvidia.com>
    
    * Clean up build process for Paddle stub file
    
    Signed-off-by: Tim Moon <tmoon@nvidia.com>
    
    * Minor fixes
    
    Signed-off-by: Tian Zheng (Engrg-Hardware 1) <tizheng@nvidia.com>
    
    * Fix pylint "wrong-import-order" warning
    
    Signed-off-by: Tian Zheng (Engrg-Hardware 1) <tizheng@nvidia.com>
    
    * Fix review comments
    
    Signed-off-by: Tian Zheng (Engrg-Hardware 1) <tizheng@nvidia.com>
    
    * Skip BF16 GEMM tests for unsupported arch
    
    Signed-off-by: Tian Zheng (Engrg-Hardware 1) <tizheng@nvidia.com>
    
    ---------
    
    Signed-off-by: Tian Zheng <tizheng@nvidia.com>
    Signed-off-by: Tian Zheng (Engrg-Hardware 1) <tizheng@nvidia.com>
    Signed-off-by: Tim Moon <tmoon@nvidia.com>
    Co-authored-by: Tim Moon <tmoon@nvidia.com>
    Co-authored-by: Tim Moon <4406448+timmoon10@users.noreply.github.com>
    Co-authored-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>

---
+++ tests/paddle/utils.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2022-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# See LICENSE for license information.
+"""Utils for testing"""
+
+import paddle
+import numpy as np
+import transformer_engine    # pylint: disable=unused-import
+import transformer_engine_paddle as tex    # pylint: disable=wrong-import-order
+
+
+def create_fp8_meta(num_fp8_tensors, amax_history_len):
+    """
+    Create and initialize FP8TensorMeta
+    """
+    fp8_meta = tex.FP8TensorMeta()
+    fp8_meta.scale = paddle.ones(num_fp8_tensors, dtype='float32')
+    fp8_meta.scale_inv = paddle.ones(num_fp8_tensors, dtype='float32')
+    fp8_meta.amax_history = paddle.zeros((amax_history_len, num_fp8_tensors), dtype='float32')
+    return fp8_meta
+
+
+def assert_allclose(actual,
+                    desired,
+                    rtol=1e-05,
+                    atol=1e-08,
+                    equal_nan=True,
+                    err_msg='',
+                    verbose=True):
+    """Compare two input paddle tensors"""
+    if isinstance(actual, paddle.Tensor):
+        actual = paddle.cast(actual, 'float32').numpy()
+    if isinstance(desired, paddle.Tensor):
+        desired = paddle.cast(desired, 'float32').numpy()
+    np.testing.assert_allclose(actual, desired, rtol, atol, equal_nan, err_msg, verbose)
