Fix softmax ONNX export
* BF16 is validated using "fake i/o": ie. instead of using BF16 as input/output, use FP32 input/output and convert to/from BF16 in the forward method.

* Wrap softmax symbolic functions with conversion to/from FP32 to produce the same semantics as TE's softmax (compute is performed at FP32 precision regardless of input/output data type).

Signed-off-by: Neta Zmora <nzmora@nvidia.com>

tests/pytorch/test_onnx_export.py
@@ -676,7 +676,7 @@ def test_export_layernorm(
     serialize_inputs_outputs(fname, inp, te_outputs)
     if fake_bf16_io or precision != torch.bfloat16:
         validate_result(
-        fname, inp, model, atol=atol, is_fp8=use_fp8, allow_cnt_errors=3, te_outputs=te_outputs)
+            fname, inp, model, atol=atol, is_fp8=use_fp8, allow_cnt_errors=3, te_outputs=te_outputs)
 
 
 @skip_FP8
@@ -689,10 +689,6 @@ def test_export_layernorm(
 # Softmax kernel only supports FP16 or BF16!
 @pytest.mark.parametrize("precision", [torch.float16, torch.bfloat16, "fake-torch.bfloat16"])
 def test_export_softmax(seed_default_rng, set_max_seq_len, softmax_fn, precision):
-    fake_bf16_io = precision == "fake-torch.bfloat16"
-    # reset precision to torch.bfloat16 after capturing fake BF16 mode
-    precision = torch.bfloat16 if precision == "fake-torch.bfloat16" else precision
-
     class Test_Softmax(nn.Module):
         def __init__(self, softmax_fn, fake_bf16_io, mask_inp=False):
             super().__init__()
@@ -709,6 +705,9 @@ def test_export_softmax(seed_default_rng, set_max_seq_len, softmax_fn, precision
                 )
 
         def forward(self, inp, mask):
+            if self.fake_bf16_io:
+                inp = inp.type(torch.bfloat16)
+
             if self.fused_scaled_softmax:
                 ret = self.fused_scaled_softmax(inp, mask, self.scale)
             else:
@@ -717,12 +716,14 @@ def test_export_softmax(seed_default_rng, set_max_seq_len, softmax_fn, precision
                 else:
                     ret = self.softmax_fn.apply(inp, self.scale)
             if self.fake_bf16_io:
-                ret = ret.type(torch.float16)
+                ret = ret.type(torch.float32)
             return ret
 
+    fake_bf16_io = precision == "fake-torch.bfloat16"
+    precision = torch.bfloat16 if fake_bf16_io else precision
+
     # Set dimensions (these are arbitrary).
-    in_features = 64
-    hidden_size = 256
+    in_features, hidden_size = 64, 256
     mask = None
     input_names = ["input", "mask"]
     inp_shape = [hidden_size, in_features, in_features, in_features]
@@ -742,17 +743,18 @@ def test_export_softmax(seed_default_rng, set_max_seq_len, softmax_fn, precision
     elif softmax_fn == te.softmax.FusedScaleMaskSoftmax:
         kernel_str = "TorchSoftmax"
         model = Test_Softmax(softmax_fn, fake_bf16_io)
-    input_tensor = torch.randn(*inp_shape, device="cuda")
-    # WAR for BF16 test as ORT doesn't support BF16 IO: FP16 input for both BF16 and FP16 precision types
-    input_tensor = input_tensor.half()
+
+    input_tensor = torch.randn(*inp_shape, device="cuda", dtype=torch.float32 if fake_bf16_io else precision)
     high_prec_str = dtype2str(precision, fake_bf16_io=fake_bf16_io)
     fname = f"{kernel_str}{high_prec_str}.onnx"
     inp = (input_tensor, mask)
+
     do_export(model, inp, fname, input_names=input_names)
     te_outputs = te_infer(model, inp, is_fp8=False)
     serialize_inputs_outputs(fname, inp, te_outputs, input_names=input_names)
     if fake_bf16_io or precision != torch.bfloat16:
-        validate_result(fname, inp, model, atol=1e-3, input_names=input_names, te_outputs=te_outputs)
+        atol = 5e-2 if fake_bf16_io else 1e-3
+        validate_result(fname, inp, model, atol=atol, input_names=input_names, te_outputs=te_outputs)
 
 
 # Test dynamically generated softmax mask.
@@ -762,13 +764,13 @@ def test_export_softmax(seed_default_rng, set_max_seq_len, softmax_fn, precision
 def test_softmax_mask_fn(seed_default_rng, set_max_seq_len, precision):
     fake_bf16_io = precision == "fake-torch.bfloat16"
     # reset precision to torch.bfloat16 after capturing fake BF16 mode
-    precision = torch.bfloat16 if precision == "fake-torch.bfloat16" else precision
+    precision = torch.bfloat16 if fake_bf16_io else precision
 
     class Test_Softmax(nn.Module):
         def __init__(self, use_onnx_mask_fn: bool, fake_bf16_io: bool):
             super().__init__()
-            self.scale = 1 # arbitrary value
-            self.fake_bf16_io = fake_bf16_io
+            self.scale=1 # arbitrary value
+            self.fake_bf16_io=fake_bf16_io
             # Use NVTE_MASKED_SOFTMAX_FUSION to force TE to use forward_torch_softmax
             # even when is_in_onnx_export_mode()==False.
             os.environ["NVTE_MASKED_SOFTMAX_FUSION"] = "0"
@@ -779,9 +781,11 @@ def test_softmax_mask_fn(seed_default_rng, set_max_seq_len, precision):
             )
 
         def forward(self, inp, mask):
+            if self.fake_bf16_io:
+                inp = inp.type(torch.bfloat16)
             ret = self.fused_scaled_softmax(inp, mask, self.scale)
             if self.fake_bf16_io:
-                ret = ret.type(torch.float16)
+                ret = ret.type(torch.float)
             return ret
 
     # Set dimensions (these are arbitrary).
@@ -789,9 +793,8 @@ def test_softmax_mask_fn(seed_default_rng, set_max_seq_len, precision):
     hidden_size = 256
     mask = None
     inp_shape = [hidden_size, in_features, in_features, in_features]
-    input_tensor = torch.randn(*inp_shape, device="cuda")
-    # WAR for BF16 test as ORT doesn't support BF16 IO: FP16 input for both BF16 and FP16 precision types
-    input_tensor = input_tensor.half()
+    input_tensor = torch.randn(
+            *inp_shape, device="cuda", dtype=torch.float if fake_bf16_io else precision)
     inp = (input_tensor, mask)
     high_prec_str = dtype2str(precision, fake_bf16_io=fake_bf16_io)
 
@@ -814,7 +817,10 @@ def test_softmax_mask_fn(seed_default_rng, set_max_seq_len, precision):
     do_export(model, inp, fname, input_names=input_names)
     serialize_inputs_outputs(fname, inp, te_outputs=te_outputs_default_mask, input_names=input_names)
     if fake_bf16_io or precision != torch.bfloat16:
-        validate_result(fname, inp, model_onnx_mask, atol=1e-3, input_names=input_names, te_outputs=te_outputs_default_mask)
+        atol = 1e-2 if fake_bf16_io else 1e-3
+        validate_result(
+                fname, inp, model_onnx_mask, atol=atol,
+                input_names=input_names, te_outputs=te_outputs_default_mask)
 
 
 @pytest.mark.parametrize("scale_factor", [1])

transformer_engine/pytorch/softmax.py
@@ -45,6 +45,38 @@ def _get_onnx_export_causal_mask(
     return derived_mask
 
 
+def get_TensorProtoDataType(t):
+    """Return the _C_onnx.TensorProtoDataType of the input tensor"""
+    try:
+        return {
+            "Float": _C_onnx.TensorProtoDataType.FLOAT,
+            "Half": _C_onnx.TensorProtoDataType.FLOAT16,
+            "BFloat16": _C_onnx.TensorProtoDataType.BFLOAT16,
+        }[t.type().scalarType()]
+    except KeyError as e:
+        raise TypeError(f"Onnx export for dtype {t.type().scalarType()} not supported.") from e
+
+
+def fp32_compute(onnx_symbolic_fn):
+    """A decorator that wraps an ONNX symoblic function with FP32 compute operators."""
+
+    def compute_in_fp32(g: torch.Graph, inp: torch._C.Value, scale: float, *args, **kwargs):
+        """Wrap subgraph with casts to/from FP32 so that its precision is FP32.
+
+        If `inp` data type is not FP32, add a cast of `inp` to FP32 and feed that into `subgraph`;
+        then cast subgraphs's output back to `inp` data type.
+        """
+        inp_dtype = get_TensorProtoDataType(inp)
+        is_fp32 = inp_dtype == _type_utils.JitScalarType.FLOAT
+        if not is_fp32:
+            inp = g.op("Cast", inp, to_i=_C_onnx.TensorProtoDataType.FLOAT)
+        sym_out = onnx_symbolic_fn(g, inp, scale, *args, **kwargs)
+        if not is_fp32:
+            sym_out = g.op("Cast", sym_out, to_i=inp_dtype)
+        return sym_out
+    return compute_in_fp32
+
+
 class ScaledUpperTriangMaskedSoftmax(torch.autograd.Function):
     """
     Fused operation which performs following three operations in sequence
@@ -77,6 +109,7 @@ class ScaledUpperTriangMaskedSoftmax(torch.autograd.Function):
         return input_grads, None
 
     @staticmethod
+    @fp32_compute
     def symbolic(g: torch.Graph, inputs: torch._C.Value, scale: float) -> torch._C.Value:
         """ScaledUpperTriangMaskedSoftmax symbolic method"""
         def triangular_mask():
@@ -88,8 +121,6 @@ class ScaledUpperTriangMaskedSoftmax(torch.autograd.Function):
             return mask
 
         # Captures the logic of function scaled_upper_triang_masked_softmax_warp_forward
-        if inputs.type().scalarType() == "BFloat16":
-            inputs = g.op("Cast", inputs, to_i=_C_onnx.TensorProtoDataType.FLOAT16)
         mask = triangular_mask()
         one = g.op("Constant", value_t=torch.tensor(1, dtype=torch.int64))
         inv_mask = g.op("Sub", one, mask)
@@ -102,8 +133,6 @@ class ScaledUpperTriangMaskedSoftmax(torch.autograd.Function):
         masked_scaled = g.op("Mul", inv_mask, scaled)
         masked = g.op("Add", masked_scaled, softmax_mask)
         out = g.op("Softmax", masked)
-        if inputs.type().scalarType() == "BFloat16":
-            out = g.op("Cast", out, to_i=_C_onnx.TensorProtoDataType.FLOAT16)
         return out
 
 
@@ -139,6 +168,7 @@ class ScaledMaskedSoftmax(torch.autograd.Function):
         return input_grads, None, None
 
     @staticmethod
+    @fp32_compute
     def symbolic(
         g: torch.Graph,
         inputs: torch._C.Value,
@@ -151,8 +181,6 @@ class ScaledMaskedSoftmax(torch.autograd.Function):
         #   masked_scaled = (1 - mask)*(input*scale)
         #   softmax_mask = mask * -10000
         #   output = softmax(masked_scaled + softmax_mask)
-        if inputs.type().scalarType() == "BFloat16":
-            inputs = g.op("Cast", inputs, to_i=_C_onnx.TensorProtoDataType.FLOAT16)
         scale_input = g.op("Constant", value_t=torch.tensor(scale, dtype=torch.float16))
         scaled = g.op("Mul", inputs, scale_input)
         one = g.op("Constant", value_t=torch.tensor(1, dtype=torch.int64))
@@ -163,8 +191,6 @@ class ScaledMaskedSoftmax(torch.autograd.Function):
         masked_scaled = g.op("Mul", inv_mask, scaled)
         masked = g.op("Add", masked_scaled, softmax_mask)
         out = g.op("Softmax", masked)
-        if inputs.type().scalarType() == "BFloat16":
-            out = g.op("Cast", out, to_i=_C_onnx.TensorProtoDataType.FLOAT16)
         return out
 
 
@@ -197,15 +223,12 @@ class ScaledSoftmax(torch.autograd.Function):
         return input_grads, None, None
 
     @staticmethod
+    @fp32_compute
     def symbolic(g: torch.Graph, inputs: torch._C.Value, scale: float) -> torch._C.Value:
         """ScaledSoftmax symbolic method"""
-        if inputs.type().scalarType() == "BFloat16":
-            inputs = g.op("Cast", inputs, to_i=_C_onnx.TensorProtoDataType.FLOAT16)
         scale_input = g.op("Constant", value_t=torch.tensor(scale, dtype=torch.float16))
         scaled = g.op("Mul", inputs, scale_input)
         out = g.op("Softmax", scaled)
-        if inputs.type().scalarType() == "BFloat16":
-            out = g.op("Cast", out, to_i=_C_onnx.TensorProtoDataType.FLOAT16)
         return out
 
 