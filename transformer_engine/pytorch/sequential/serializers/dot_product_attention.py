c5f29bc6319d0a0840a64bb73e8c7e361fba833b
Jan Bielak <jbielak@nvidia.com>
2023-06-19 11:48:24 +0000

    Remove unused import
    Signed-off-by: Jan Bielak <jbielak@nvidia.com>

--- transformer_engine/pytorch/sequential/serializers/dot_product_attention.py
+++ transformer_engine/pytorch/sequential/serializers/dot_product_attention.py
@@ -1,4 +1,3 @@
-from transformer_engine.pytorch.sequential.ops.base import Op
 from ..custom_serializer_holder import COMPUTE_PIPELINE_CUSTOM_SERIALIZERS
 from ...attention import DotProductAttention
 from ..ops import OpGraph
