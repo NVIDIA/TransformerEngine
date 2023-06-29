0d4f1a71a5b524d822c81cfe747f4f039d0c696d
Jan Bielak <jbielak@nvidia.com>
2023-06-29 12:16:08 +0000

    Deduplicate code
    Signed-off-by: Jan Bielak <jbielak@nvidia.com>

--- transformer_engine/pytorch/cpp_extensions/activation.py
+++ transformer_engine/pytorch/cpp_extensions/activation.py
@@ -8,135 +8,42 @@ import torch
 import transformer_engine_extensions as tex
 
 
-__all__ = ['gelu', 'relu', 'reglu', 'geglu', 'swiglu']
+__all__ = ["gelu", "relu", "reglu", "geglu", "swiglu"]
 
 
-def gelu(
-    inp: torch.Tensor,
-    fp8_meta_tensor: tex.FP8TensorMeta,
-    fp8_tensor: Union[tex.FP8FwdTensors, tex.FP8BwdTensors],
-    otype: tex.DType,
-) -> torch.Tensor:
-    """GeLU with FP8 output"""
-    empty_tensor = torch.Tensor()
-    if fp8_meta_tensor is not None:
-        scale = fp8_meta_tensor.scale
-        amax_history = fp8_meta_tensor.amax_history
-        scale_inv = fp8_meta_tensor.scale_inv
-    else:
-        scale = empty_tensor
-        amax_history = empty_tensor
-        scale_inv = empty_tensor
+def register_activation(name: str):
+    def f(
+        inp: torch.Tensor,
+        fp8_meta_tensor: tex.FP8TensorMeta,
+        fp8_tensor: Union[tex.FP8FwdTensors, tex.FP8BwdTensors],
+        otype: tex.DType,
+    ) -> torch.Tensor:
+        empty_tensor = torch.Tensor()
+        if fp8_meta_tensor is not None:
+            scale = fp8_meta_tensor.scale
+            amax_history = fp8_meta_tensor.amax_history
+            scale_inv = fp8_meta_tensor.scale_inv
+        else:
+            scale = empty_tensor
+            amax_history = empty_tensor
+            scale_inv = empty_tensor
 
-    return torch.ops.tex_ts.gelu_ts(
-        inp,
-        scale,
-        amax_history,
-        scale_inv,
-        fp8_tensor,
-        otype,
-    )
+        return getattr(torch.ops.tex_ts, f"{name.lower()}_ts")(
+            inp,
+            scale,
+            amax_history,
+            scale_inv,
+            fp8_tensor,
+            otype,
+        )
 
-
-def relu(
-    inp: torch.Tensor,
-    fp8_meta_tensor: tex.FP8TensorMeta,
-    fp8_tensor: Union[tex.FP8FwdTensors, tex.FP8BwdTensors],
-    otype: tex.DType,
-) -> torch.Tensor:
-    """ReLU with FP8 output"""
-    empty_tensor = torch.Tensor()
-    if fp8_meta_tensor is not None:
-        scale = fp8_meta_tensor.scale
-        amax_history = fp8_meta_tensor.amax_history
-        scale_inv = fp8_meta_tensor.scale_inv
-    else:
-        scale = empty_tensor
-        amax_history = empty_tensor
-        scale_inv = empty_tensor
-    return torch.ops.tex_ts.relu_ts(
-        inp,
-        scale,
-        amax_history,
-        scale_inv,
-        fp8_tensor,
-        otype,
-    )
+    f.__doc__ = f"""{name} with FP8 output"""
+    __all__.append(name.lower())
+    return f
 
 
-def geglu(
-    inp: torch.Tensor,
-    fp8_meta_tensor: tex.FP8TensorMeta,
-    fp8_tensor: Union[tex.FP8FwdTensors, tex.FP8BwdTensors],
-    otype: tex.DType,
-) -> torch.Tensor:
-    """GeGLU with FP8 output"""
-    empty_tensor = torch.Tensor()
-    if fp8_meta_tensor is not None:
-        scale = fp8_meta_tensor.scale
-        amax_history = fp8_meta_tensor.amax_history
-        scale_inv = fp8_meta_tensor.scale_inv
-    else:
-        scale = empty_tensor
-        amax_history = empty_tensor
-        scale_inv = empty_tensor
-    return torch.ops.tex_ts.geglu_ts(
-        inp,
-        scale,
-        amax_history,
-        scale_inv,
-        fp8_tensor,
-        otype,
-    )
-
-
-def reglu(
-    inp: torch.Tensor,
-    fp8_meta_tensor: tex.FP8TensorMeta,
-    fp8_tensor: Union[tex.FP8FwdTensors, tex.FP8BwdTensors],
-    otype: tex.DType,
-) -> torch.Tensor:
-    """ReGLU with FP8 output"""
-    empty_tensor = torch.Tensor()
-    if fp8_meta_tensor is not None:
-        scale = fp8_meta_tensor.scale
-        amax_history = fp8_meta_tensor.amax_history
-        scale_inv = fp8_meta_tensor.scale_inv
-    else:
-        scale = empty_tensor
-        amax_history = empty_tensor
-        scale_inv = empty_tensor
-    return torch.ops.tex_ts.reglu_ts(
-        inp,
-        scale,
-        amax_history,
-        scale_inv,
-        fp8_tensor,
-        otype,
-    )
-
-
-def swiglu(
-    inp: torch.Tensor,
-    fp8_meta_tensor: tex.FP8TensorMeta,
-    fp8_tensor: Union[tex.FP8FwdTensors, tex.FP8BwdTensors],
-    otype: tex.DType,
-) -> torch.Tensor:
-    """SwiGLU with FP8 output"""
-    empty_tensor = torch.Tensor()
-    if fp8_meta_tensor is not None:
-        scale = fp8_meta_tensor.scale
-        amax_history = fp8_meta_tensor.amax_history
-        scale_inv = fp8_meta_tensor.scale_inv
-    else:
-        scale = empty_tensor
-        amax_history = empty_tensor
-        scale_inv = empty_tensor
-    return torch.ops.tex_ts.swiglu_ts(
-        inp,
-        scale,
-        amax_history,
-        scale_inv,
-        fp8_tensor,
-        otype,
-    )
+gelu = register_activation("GeLU")
+relu = register_activation("ReLU")
+geglu = register_activation("GeGLU")
+reglu = register_activation("ReGLU")
+swiglu = register_activation("SwiGLU")
