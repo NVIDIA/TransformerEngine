a1e9b23394e8c92c0b98134f3b96289092d217e2
Jan Bielak <jbielak@nvidia.com>
2023-06-29 08:43:53 +0000

    Deduplicate code
    Signed-off-by: Jan Bielak <jbielak@nvidia.com>

--- transformer_engine/pytorch/csrc/extensions.cu
+++ transformer_engine/pytorch/csrc/extensions.cu
@@ -1597,19 +1597,9 @@ at::Tensor cast_to_fp8(const at::Tensor &input,
                        at::Tensor scale_inv,
                        transformer_engine::DType otype
 ) {
-    using namespace transformer_engine;
-    auto input_shape = input.sizes().vec();
-    std::vector<size_t> shape{input_shape.begin(), input_shape.end()};
-
     auto output = at::empty_like(input, at::CUDA(GetATenDType(otype)));
 
-    auto input_cu     = makeTransformerEngineTensor(input);
-    auto output_cu    = makeTransformerEngineTensor(output.data_ptr(), shape, otype,
-                                                    amax.data_ptr(), scale.data_ptr(),
-                                                    scale_inv.data_ptr());
-
-    nvte_fp8_quantize(input_cu.data(), output_cu.data(),
-                      at::cuda::getCurrentCUDAStream());
+    cast_to_fp8_noalloc(input, scale, output, amax, scale_inv, otype);
 
     return output;
 }
@@ -1623,11 +1613,11 @@ void cast_to_fp8_noalloc(const at::Tensor &input,
                                transformer_engine::DType otype
 ) {
     using namespace transformer_engine;
-    size_t N = static_cast<size_t>(input.size(0));
-    size_t H = static_cast<size_t>(input.size(1));
+    auto input_shape = input.sizes().vec();
+    std::vector<size_t> shape{input_shape.begin(), input_shape.end()};
 
     auto input_cu     = makeTransformerEngineTensor(input);
-    auto output_cu    = makeTransformerEngineTensor(output.data_ptr(), {N, H}, otype,
+    auto output_cu    = makeTransformerEngineTensor(output.data_ptr(), shape, otype,
                                                     amax.data_ptr(), scale.data_ptr(),
                                                     scale_inv.data_ptr());
 
