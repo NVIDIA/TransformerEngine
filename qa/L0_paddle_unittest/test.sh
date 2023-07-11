fc4135483b447e8e390210b9ec39801888294701
Charlene Yang <8636796+cyanguwa@users.noreply.github.com>
2023-07-11 16:09:33 -0700

    debug paddle CI; force LD_LIBRARY to /opt
    Signed-off-by: Charlene Yang <8636796+cyanguwa@users.noreply.github.com>

--- qa/L0_paddle_unittest/test.sh
+++ qa/L0_paddle_unittest/test.sh
@@ -6,7 +6,7 @@ set -xe
 
 : ${TE_PATH:=/opt/transformerengine}
 #pytest -Wignore -v $TE_PATH/tests/paddle
-export LD_LIBRARY_PATH=/workspace/cudnn/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/cudnn/lib64:$LD_LIBRARY_PATH
 echo LD_LIBRARY_PATH is $LD_LIBRARY_PATH
 echo CUDNN_PATH is $CUDNN_PATH
 pytest -s -v $TE_PATH/tests/paddle
