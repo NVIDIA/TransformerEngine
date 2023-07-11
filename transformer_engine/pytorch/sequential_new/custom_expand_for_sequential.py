0bdd84a2ba91d0ad73357226cbdce31879e0c825
Jan Bielak <jbielak@nvidia.com; git config --global format.signoff true>
2023-07-11 15:08:23 +0000

    fix cyclical import
    Signed-off-by: Jan Bielak <jbielak@nvidia.com; git config --global format.signoff true>

--- transformer_engine/pytorch/sequential_new/custom_expand_for_sequential.py
+++ transformer_engine/pytorch/sequential_new/custom_expand_for_sequential.py
@@ -7,4 +7,12 @@ CUSTOM_EXPAND_FOR_SEQUENTIAL: dict[
     type, Callable[[nn.Module, CompileEnv], list[Op]]
 ] = {}
 
-__all__ = ["CUSTOM_EXPAND_FOR_SEQUENTIAL"]
+def expand(m: nn.Module, compile_env: CompileEnv) -> list[Op]:
+    if hasattr(m, "expand_for_sequential"):
+        return m.expand_for_sequential(compile_env)  # type: ignore
+    elif type(m) in CUSTOM_EXPAND_FOR_SEQUENTIAL:
+        return CUSTOM_EXPAND_FOR_SEQUENTIAL[type(m)](m, compile_env)
+    else:
+        raise NotImplementedError
+
+__all__ = ["CUSTOM_EXPAND_FOR_SEQUENTIAL", "expand"]
--- transformer_engine/pytorch/sequential_new/expand_for_sequential.py
+++ transformer_engine/pytorch/sequential_new/expand_for_sequential.py
@@ -1,17 +1,4 @@
-from torch import nn
-from .compile_env import CompileEnv
-from .ops import Op
-from .custom_expand_for_sequential import CUSTOM_EXPAND_FOR_SEQUENTIAL
+from .custom_expand_for_sequential import expand
 from . import custom_expand  # loads custom expanders
-
-
-def expand(m: nn.Module, compile_env: CompileEnv) -> list[Op]:
-    if hasattr(m, "expand_for_sequential"):
-        return m.expand_for_sequential(compile_env)  # type: ignore
-    elif type(m) in CUSTOM_EXPAND_FOR_SEQUENTIAL:
-        return CUSTOM_EXPAND_FOR_SEQUENTIAL[type(m)](m, compile_env)
-    else:
-        raise NotImplementedError
-
 
 __all__ = ["expand"]
