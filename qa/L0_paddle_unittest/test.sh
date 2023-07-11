de7b81007379be3e5b0d7f19104f5c2c256207c9
Charlene Yang <8636796+cyanguwa@users.noreply.github.com>
2023-07-11 14:39:57 -0700

    debug paddle CI; force LD_LIBRARY
    Signed-off-by: Charlene Yang <8636796+cyanguwa@users.noreply.github.com>

--- qa/L0_paddle_unittest/test.sh
+++ qa/L0_paddle_unittest/test.sh
@@ -6,6 +6,7 @@ set -xe
 
 : ${TE_PATH:=/opt/transformerengine}
 #pytest -Wignore -v $TE_PATH/tests/paddle
+export LD_LIBRARY_PATH=/workspace/cudnn/lib64:$LD_LIBRARY_PATH
 echo LD_LIBRARY_PATH is $LD_LIBRARY_PATH
 echo CUDNN_PATH is $CUDNN_PATH
 pytest -s -v $TE_PATH/tests/paddle
