2e11c61603b7e7e21f15b8b3fff759da002fcd63
Charlene Yang <8636796+cyanguwa@users.noreply.github.com>
2023-07-12 12:01:54 -0700

    remove debug info for paddle
    Signed-off-by: Charlene Yang <8636796+cyanguwa@users.noreply.github.com>

--- qa/L0_paddle_unittest/test.sh
+++ qa/L0_paddle_unittest/test.sh
@@ -5,8 +5,4 @@
 set -xe
 
 : ${TE_PATH:=/opt/transformerengine}
-#pytest -Wignore -v $TE_PATH/tests/paddle
-export LD_LIBRARY_PATH=/opt/cudnn/lib64:$LD_LIBRARY_PATH
-echo LD_LIBRARY_PATH is $LD_LIBRARY_PATH
-echo CUDNN_PATH is $CUDNN_PATH
-pytest -s -v $TE_PATH/tests/paddle
+pytest -Wignore -v $TE_PATH/tests/paddle
