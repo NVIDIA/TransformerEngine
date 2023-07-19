33320fd0a31efe25e1b0f25ea3a452ff637bd519
Tian Zheng (Engrg-Hardware 1) <tizheng@nvidia.com>
2023-07-19 02:16:30 +0000

    Use Paddle nvtx bindings
    Signed-off-by: Tian Zheng (Engrg-Hardware 1) <tizheng@nvidia.com>

--- transformer_engine/paddle/profile.py
+++ transformer_engine/paddle/profile.py
@@ -3,41 +3,14 @@
 # See LICENSE for license information.
 """Utils for profiling"""
 
-import ctypes
 from contextlib import contextmanager
-import os
 
-_cuda_home = os.environ.get('CUDA_HOME', '/usr/local/cuda')
-
-_cudart = ctypes.CDLL(os.path.join(_cuda_home, 'lib64/libcudart.so'))
-
-
-def cuda_profile_start():
-    """Calls cudaProfilerStart"""
-    _cudart.cudaProfilerStart()
-
-
-def cuda_profile_stop():
-    """Calls cudaProfilerStop"""
-    _cudart.cudaProfilerStop()
-
-
-_nvtx = ctypes.CDLL(os.path.join(_cuda_home, 'lib64/libnvToolsExt.so'))
-
-
-def cuda_nvtx_range_push(name):
-    """Calls nvtxRangePushW"""
-    _nvtx.nvtxRangePushW(ctypes.c_wchar_p(name))
-
-
-def cuda_nvtx_range_pop():
-    """Calls nvtxRangePop"""
-    _nvtx.nvtxRangePop()
+from paddle.fluid import core
 
 
 @contextmanager
 def nvtx_range(msg):
     """Context to insert NVTX"""
-    cuda_nvtx_range_push(msg)
+    core.nvprof_nvtx_push(msg)
     yield
-    cuda_nvtx_range_pop()
+    core.nvprof_nvtx_pop()
