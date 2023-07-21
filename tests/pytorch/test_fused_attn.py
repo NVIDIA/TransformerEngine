ecd7b644bd77ef149dedd1dd727bce131405ed87
Charlene Yang <8636796+cyanguwa@users.noreply.github.com>
2023-07-21 14:03:40 -0700

    add support for multi-query/grouped-query attention
    Signed-off-by: Charlene Yang <8636796+cyanguwa@users.noreply.github.com>

--- tests/pytorch/test_fused_attn.py
+++ tests/pytorch/test_fused_attn.py
@@ -13,6 +13,10 @@ from transformer_engine.pytorch import TransformerLayer
 from transformer_engine.pytorch.attention import DotProductAttention
 import os
 
+from pkg_resources import packaging
+_flash_attn_version = packaging.version.Version(version("flash-attn"))
+_flash_attn_2_available = _flash_attn_version >= packaging.version.Version("2")
+
 class ModelConfig:
     def __init__(
         self, num_layers, hidden_size, num_attention_heads, head_dim, seq_len,
@@ -172,6 +176,105 @@ def _run_transformer_layer(dtype, bs, config, backend):
             config.hidden_size,
             4 * config.hidden_size,
             config.num_attention_heads,
+            layernorm_epsilon = 1e-5,
+            hidden_dropout = 0.0,
+            attention_dropout = config.dropout_p,
+            init_method = init_method,
+            output_layer_init_method = output_layer_init_method,
+            layer_number = layer_number,
+            kv_channels = config.head_dim,
+            self_attn_mask_type = config.attn_mask_type,
+            tp_group = None,
+            tp_size =  1,
+            params_dtype = dtype,
+            get_rng_state_tracker = None,
+            fuse_wgrad_accumulation = False,
+            seq_length = config.seq_len,
+            micro_batch_size = bs,
+            sequence_parallel = False,
+            apply_residual_connection_post_layernorm = False,
+            output_layernorm = False,
+            layer_type = "encoder",
+            drop_path_rate = drop_path_rates[layer_number - 1],
+            set_parallel_mode = True,
+            fuse_qkv_params = True,
+            zero_centered_gamma = False,
+            qkv_weight_interleaved = False,
+            ub_tp_comm_overlap = False,
+            bias = True,
+        )
+        .to(dtype = dtype)
+        .cuda()
+    )
+
+    op = block(inp)
+    op.backward(op_grad)
+
+    return op, inp.grad
+
+@pytests.mark.skipif(not _flash_attn_2_available)
+@pytest.mark.parametrize("dtype", param_types)
+@pytest.mark.parametrize("bs", batch_sizes)
+@pytest.mark.parametrize("model", model_configs.keys())
+def test_transformer_layer_gqa(dtype, bs, model):
+    """Test TransformerLayer module when its DotProductAttention is enabled with
+    FlashAttention, FusedAttention, or UnfusedDotProductAttention backend"""
+
+    config = model_configs[model]
+    def find_factors(x):
+       f = []
+       for i in range(1, x + 1):
+           if x % i == 0:
+               f.append(i)
+       return f
+
+    num_querys_per_gqa_group = find_factors(config.num_attention_heads)
+
+    for num_q_per_gqa_group in num_querys_per_gqa_group:
+        flash_attn_fwd, flash_attn_bwd = _run_transformer_layer_gqa(
+                dtype, bs, config, "FlashAttention", num_q_per_gqa_group)
+        unfused_attn_fwd, unfused_attn_bwd = _run_transformer_layer_gqa(
+                dtype, bs, config, "UnfusedDotProductAttention", num_q_per_gqa_group)
+
+        atol, rtol = (5e-1, 5e-1) if dtype == torch.bfloat16 else (5e-1, 5e-1)
+        assert torch.allclose(flash_attn_fwd, unfused_attn_fwd, atol = atol, rtol = rtol)
+        assert torch.allclose(flash_attn_bwd, unfused_attn_bwd, atol = atol, rtol = rtol)
+
+def _run_transformer_layer_gqa(dtype, bs, config, backend, num_querys_per_gqa_group):
+
+    torch.manual_seed(1234)
+    torch.cuda.manual_seed(1234)
+    os.environ["NVTE_FLASH_ATTN"] = "0"
+    if backend == "FlashAttention":
+        os.environ["NVTE_FLASH_ATTN"] = "1"
+
+    inp = 0.1 * torch.randn(
+            config.seq_len, bs, config.num_attention_heads * config.head_dim,
+            dtype = dtype).cuda()
+    inp.requires_grad=True
+    seqlens = torch.empty(bs, dtype = torch.int32).cuda()
+    seqlens.fill_(config.seq_len)
+    cu_seqlens = torch.zeros(bs + 1, device = inp.device, dtype = torch.int32)
+    cu_seqlens[1:] = torch.cumsum(seqlens, dim = 0)
+    op_grad = 0.001 * torch.randint(0, 200, (
+        config.seq_len, bs, config.num_attention_heads * config.head_dim
+        ), dtype = dtype).cuda()
+
+    sigma = 0.02
+    init_method = init_method_normal(sigma)
+    output_layer_init_method = scaled_init_method_normal(sigma, config.num_layers)
+
+    layer_number = 1
+    drop_path_rate = 0.0
+    drop_path_rates = [
+            rate.item() for rate in torch.linspace(0, drop_path_rate, config.num_layers)]
+
+    block = (
+        TransformerLayer(
+            config.hidden_size,
+            4 * config.hidden_size,
+            config.num_attention_heads,
+            num_gqa_groups = config.num_attention_heads / num_querys_per_gqa_group,
             layernorm_epsilon = 1e-5,
             hidden_dropout = 0.0,
             attention_dropout = config.dropout_p,
