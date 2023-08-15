a6ee1dc945f41248cce9af72ea8bc4837a30af6b
Jan Bielak <jbielak@nvidia.com>
2023-08-15 15:08:07 +0200

    use fp8_transpose_dbias
    Signed-off-by: Jan Bielak <jbielak@nvidia.com>

--- transformer_engine/pytorch/sequential/nvte/misc_fusions.py
+++ transformer_engine/pytorch/sequential/nvte/misc_fusions.py
@@ -1,3 +1,4 @@
+from .dtype import is_fp8
 from . import _nvte
 from .cast_transpose import cast_transpose_checked
 from .empty import multi_empty_share_metadata, empty, empty_like
@@ -23,6 +24,14 @@ def cast_transpose_dbias_checked(
             )
             workspace = empty_like(workspace)
         return out_cast, out_transpose, out_dbias
+    elif is_fp8(grad.dtype) and cast_dtype is None or cast_dtype == grad.dtype:
+        out_transpose = empty(grad.shape[::-1], grad.dtype)
+        out_dbias = empty((grad.shape[1],), dbias_dtype)
+        workspace = empty()
+        for _ in range(2):
+            _nvte.fp8_transpose_dbias(grad, out_transpose, out_dbias, workspace)
+            workspace = empty_like(workspace)
+        return grad, out_transpose, out_dbias
     else:
         out_cast, out_transpose = cast_transpose_checked(grad, cast_dtype)
         out_dbias = dbias(grad, dbias_dtype)
