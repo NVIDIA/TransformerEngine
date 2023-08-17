e078506a066735c0aeb9957328bd23af45382afc
Jan Bielak <jbielak@nvidia.com>
2023-08-17 18:53:11 +0200

    fix dimension again
    Signed-off-by: Jan Bielak <jbielak@nvidia.com>

--- transformer_engine/pytorch/sequential/nvte/activation.py
+++ transformer_engine/pytorch/sequential/nvte/activation.py
@@ -27,36 +27,36 @@ def dgelu(grad: _nvte.Tensor, x: _nvte.Tensor, out_dtype: _nvte.DType):
 
 
 def reglu(x: _nvte.Tensor, out_dtype: _nvte.DType):
-    output = empty((x.shape[0] // 2, x.shape[1]), out_dtype)
+    output = empty((x.shape[0], x.shape[1] // 2), out_dtype)
     _nvte.reglu(x, output)
     return output
 
 
 def dreglu(grad: _nvte.Tensor, x: _nvte.Tensor, out_dtype: _nvte.DType):
-    output = empty((x.shape[0] * 2, x.shape[1]), out_dtype)
+    output = empty((x.shape[0], x.shape[1] * 2), out_dtype)
     _nvte.dreglu(grad, x, output)
     return output
 
 
 def geglu(x: _nvte.Tensor, out_dtype: _nvte.DType):
-    output = empty((x.shape[0] // 2, x.shape[1]), out_dtype)
+    output = empty((x.shape[0], x.shape[1] // 2), out_dtype)
     _nvte.geglu(x, output)
     return output
 
 
 def dgeglu(grad: _nvte.Tensor, x: _nvte.Tensor, out_dtype: _nvte.DType):
-    output = empty((x.shape[0] * 2, x.shape[1]), out_dtype)
+    output = empty((x.shape[0], x.shape[1] * 2), out_dtype)
     _nvte.dgeglu(grad, x, output)
     return output
 
 
 def swiglu(x: _nvte.Tensor, out_dtype: _nvte.DType):
-    output = empty((x.shape[0] // 2, x.shape[1]), out_dtype)
+    output = empty((x.shape[0], x.shape[1] // 2), out_dtype)
     _nvte.swiglu(x, output)
     return output
 
 
 def dswiglu(grad: _nvte.Tensor, x: _nvte.Tensor, out_dtype: _nvte.DType):
-    output = empty((x.shape[0] * 2, x.shape[1]), out_dtype)
+    output = empty((x.shape[0], x.shape[1] * 2), out_dtype)
     _nvte.dswiglu(grad, x, output)
     return output
