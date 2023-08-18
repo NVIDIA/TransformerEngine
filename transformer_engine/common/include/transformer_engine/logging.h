2e7558dcbcb6d00788f4074eae6fab7d1ed1f02b
Jan Bielak <jbielak@nvidia.com>
2023-08-18 10:38:33 +0200

    Improve upon error reporting in common
    Signed-off-by: Jan Bielak <jbielak@nvidia.com>

--- transformer_engine/common/include/transformer_engine/logging.h
+++ transformer_engine/common/include/transformer_engine/logging.h
@@ -7,68 +7,70 @@
 #ifndef TRANSFORMER_ENGINE_LOGGING_H_
 #define TRANSFORMER_ENGINE_LOGGING_H_
 
-#include <cuda_runtime_api.h>
 #include <cublas_v2.h>
+#include <cuda_runtime_api.h>
 #include <cudnn.h>
 #include <nvrtc.h>
-#include <string>
 #include <stdexcept>
+#include <string>
 
-#define NVTE_ERROR(x) \
-    do { \
-        throw std::runtime_error(std::string(__FILE__ ":") + std::to_string(__LINE__) +            \
-                                 " in function " + __func__ + ": " + x);                           \
-    } while (false)
+#define NVTE_ERROR(x)                                                          \
+  do {                                                                         \
+    throw std::runtime_error(std::string(__FILE__ ":") +                       \
+                             std::to_string(__LINE__) + " in function " +      \
+                             __func__ + ": " + x);                             \
+  } while (false)
 
-#define NVTE_CHECK(x, ...)                                                                         \
-    do {                                                                                           \
-        if (!(x)) {                                                                                \
-            NVTE_ERROR(std::string("Assertion failed: "  #x ". ") + std::string(__VA_ARGS__));     \
-        }                                                                                          \
-    } while (false)
+#define NVTE_CHECK(x, ...)                                                     \
+  do {                                                                         \
+    if (!(x)) {                                                                \
+      NVTE_ERROR(std::string("Assertion failed: " #x ". ") +                   \
+                 std::string(__VA_ARGS__));                                    \
+    }                                                                          \
+  } while (false)
 
-namespace {
+#define NVTE_CHECK_CUDA(status)                                                \
+  do {                                                                         \
+    if (status != cudaSuccess) {                                               \
+      NVTE_ERROR("CUDA Error: " + std::string(cudaGetErrorString(status)));    \
+    }                                                                          \
+  } while (false)
 
-inline void check_cuda_(cudaError_t status) {
-    if ( status != cudaSuccess ) {
-        NVTE_ERROR("CUDA Error: " + std::string(cudaGetErrorString(status)));
-    }
-}
+#define NVTE_CHECK_CUBLAS(status)                                              \
+  do {                                                                         \
+    if (status != CUBLAS_STATUS_SUCCESS) {                                     \
+      std::string message;                                                     \
+      message.reserve(1024);                                                   \
+      message += "CUBLAS Error: ";                                             \
+      message += cublasGetStatusString(status);                                \
+      message += (". "                                                         \
+                  "For more information, increase CUBLASLT_LOG_LEVEL, "        \
+                  "by setting CUBLASLT_LOG_LEVEL=N [0-5] "                     \
+                  "in the environment.");                                      \
+      NVTE_ERROR(message);                                                     \
+    }                                                                          \
+  } while (false)
 
-inline void check_cublas_(cublasStatus_t status) {
-    if ( status != CUBLAS_STATUS_SUCCESS ) {
-        NVTE_ERROR("CUBLAS Error: " + std::string(cublasGetStatusString(status)));
-    }
-}
+#define NVTE_CHECK_CUDNN(status)                                               \
+  do {                                                                         \
+    if (status != CUDNN_STATUS_SUCCESS) {                                      \
+      std::string message;                                                     \
+      message.reserve(1024);                                                   \
+      message += "CUDNN Error: ";                                              \
+      message += cudnnGetErrorString(status);                                  \
+      message += (". "                                                         \
+                  "For more information, enable cuDNN error logging "          \
+                  "by setting CUDNN_LOGERR_DBG=1 and "                         \
+                  "CUDNN_LOGDEST_DBG=stderr in the environment.");             \
+      NVTE_ERROR(message);                                                     \
+    }                                                                          \
+  } while (false)
 
-inline void check_cudnn_(cudnnStatus_t status) {
-    if ( status != CUDNN_STATUS_SUCCESS ) {
-        std::string message;
-        message.reserve(1024);
-        message += "CUDNN Error: ";
-        message += cudnnGetErrorString(status);
-        message += (". "
-                    "For more information, enable cuDNN error logging "
-                    "by setting CUDNN_LOGERR_DBG=1 and "
-                    "CUDNN_LOGDEST_DBG=stderr in the environment.");
-        NVTE_ERROR(message);
-    }
-}
+#define NVTE_CHECK_NVRTC(status)                                               \
+  do {                                                                         \
+    if (status != NVRTC_SUCCESS) {                                             \
+      NVTE_ERROR("NVRTC Error: " + std::string(nvrtcGetErrorString(status)));  \
+    }                                                                          \
+  } while (false)
 
-inline void check_nvrtc_(nvrtcResult status) {
-    if ( status != NVRTC_SUCCESS ) {
-        NVTE_ERROR("NVRTC Error: " + std::string(nvrtcGetErrorString(status)));
-    }
-}
-
-}  // namespace
-
-#define NVTE_CHECK_CUDA(ans) { check_cuda_(ans); }
-
-#define NVTE_CHECK_CUBLAS(ans) { check_cublas_(ans); }
-
-#define NVTE_CHECK_CUDNN(ans) { check_cudnn_(ans); }
-
-#define NVTE_CHECK_NVRTC(ans) { check_nvrtc_(ans); }
-
-#endif  // TRANSFORMER_ENGINE_LOGGING_H_
+#endif // TRANSFORMER_ENGINE_LOGGING_H_
