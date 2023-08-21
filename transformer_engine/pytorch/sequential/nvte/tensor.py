6f98c444769448119c1bd0f8385bd828615c65a8
Jan Bielak <jbielak@nvidia.com>
2023-08-21 13:25:33 +0200

    add repr to nvte.Tensor for debugging
    Signed-off-by: Jan Bielak <jbielak@nvidia.com>

--- transformer_engine/pytorch/sequential/nvte/tensor.py
+++ transformer_engine/pytorch/sequential/nvte/tensor.py
@@ -1,5 +1,6 @@
 import torch
 from ._nvte import Tensor as TensorBase, DType
+from .dtype import dtype_name
 
 
 class Tensor(TensorBase):
@@ -30,3 +31,14 @@ class Tensor(TensorBase):
         self._cached_dtype = super().dtype
         self._cached_shape = tuple(super().shape)
         return self
+
+    def __repr__(self):
+        self.query_shape_and_dtype_()
+        data_repr = repr(self.data)
+        data_repr = data_repr[::-1][data_repr[::-1].find("]") :][::-1] + ")"
+        data_repr = data_repr.replace("\n", "\n" + 11 * " ")
+        return f"""\
+Tensor(
+    data = {data_repr},
+    dtype = {dtype_name(self.dtype)}, amax = {self.amax[0].item()}, scale = {self.scale.item()}, scale_inv = {self.scale_inv.item()})
+"""
