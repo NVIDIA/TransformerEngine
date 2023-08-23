b583419f03db91c1bb9a90f54ee3494965aaa75c
Jan Bielak <jbielak@nvidia.com>
2023-08-23 16:47:46 +0200

    fix for torch compile
    Signed-off-by: Jan Bielak <jbielak@nvidia.com>

--- transformer_engine/pytorch/sequential/utils.py
+++ transformer_engine/pytorch/sequential/utils.py
@@ -47,19 +47,3 @@ def cache(func: Callable[[], T]) -> Callable[[], T]:
         return result
 
     return wrapper
-
-
-@contextmanager
-def set_attribute(obj: object, attr: str, value: Any):
-    """Set an attribute on an object, and reset it to its original value when the context manager exits."""
-    had_value = hasattr(obj, attr)
-    if had_value:
-        old_value = getattr(obj, attr)
-    setattr(obj, attr, value)
-    try:
-        yield
-    finally:
-        if had_value:
-            setattr(obj, attr, old_value)  # type:ignore
-        else:
-            delattr(obj, attr)
