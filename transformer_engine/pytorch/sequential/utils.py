7d061496a2e0970131a31d26400ff33ccea84c80
Jan Bielak <jbielak@nvidia.com>
2023-08-23 16:41:37 +0200

    fix for torch compile
    Signed-off-by: Jan Bielak <jbielak@nvidia.com>

--- transformer_engine/pytorch/sequential/utils.py
+++ transformer_engine/pytorch/sequential/utils.py
@@ -7,42 +7,47 @@ T = TypeVar("T")
 ExcT = TypeVar("ExcT")
 
 
+class __Context:
+    def __init__(
+        self,
+        func: Callable[PS, Generator[T, None, None]],
+        *args: PS.args,
+        **kwargs: PS.kwargs
+    ):
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+
+    def __enter__(self):
+        gen = self.func(*self.args, **self.kwargs)
+        self.gen = gen
+        return next(gen)
+
+    def __exit__(
+        self,
+        exc_type: type[ExcT],
+        exc_value: ExcT,
+        exc_traceback: TracebackType,
+    ):
+        next(self.gen)
+
+
 class contextmanager(Generic[PS, T]):
     def __init__(self, func: Callable[PS, Generator[T, None, None]]):
         self.func = func
 
     def __call__(self, *args: PS.args, **kwargs: PS.kwargs):
-        class Context:
-            def __init__(
-                self,
-                func: Callable[PS, Generator[T, None, None]],
-                *args: PS.args,
-                **kwargs: PS.kwargs
-            ):
-                self.func = func
-                self.args = args
-                self.kwargs = kwargs
+        return __Context(self.func, *args, **kwargs)
 
-            def __enter__(self):
-                gen = self.func(*self.args, **self.kwargs)
-                self.gen = gen
-                return next(gen)
 
-            def __exit__(
-                self,
-                exc_type: type[ExcT],
-                exc_value: ExcT,
-                exc_traceback: TracebackType,
-            ):
-                next(self.gen)
-
-        return Context(self.func, *args, **kwargs)
-
-def cache(func: Callable[[],T])->Callable[[],T]:
+def cache(func: Callable[[], T]) -> Callable[[], T]:
     result = func()
+
     def wrapper():
         return result
+
     return wrapper
+
 
 @contextmanager
 def set_attribute(obj: object, attr: str, value: Any):
