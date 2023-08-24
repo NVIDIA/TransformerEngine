3fe3ca172cf1c2e4b2ce4a713f80930af3c905a6
Jan Bielak <jbielak@nvidia.com>
2023-08-24 15:46:19 +0200

    fix bool use
    Signed-off-by: Jan Bielak <jbielak@nvidia.com>

--- transformer_engine/pytorch/sequential/cppsrc/pybind.cpp
+++ transformer_engine/pytorch/sequential/cppsrc/pybind.cpp
@@ -132,6 +132,9 @@ template <> struct wrapped_arg<float> : trait<double> {
 template <> struct wrapped_arg<size_t> : trait<int64_t> {
   static int64_t unwrap(size_t arg) { return (int64_t)arg; }
 };
+template <> struct wrapped_arg<bool> : trait<int64_t> {
+  static int64_t unwrap(bool arg) { return (int64_t)arg; }
+};
 template <> struct wrapped_arg<NVTETensor> : trait<Tensor> {
   static NVTETensor unwrap(Tensor arg) { return (NVTETensor)arg.pimpl.get(); }
 };
