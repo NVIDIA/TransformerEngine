ae58dc94792f35872171953f0be256889ae4dea8
Jan Bielak <jbielak@nvidia.com>
2023-08-24 18:32:20 +0200

    fix typing
    Signed-off-by: Jan Bielak <jbielak@nvidia.com>

--- transformer_engine/pytorch/sequential/cpp_extensions/real.py
+++ transformer_engine/pytorch/sequential/cpp_extensions/real.py
@@ -1,8 +1,11 @@
 from enum import Enum
 import inspect
-from typing import Any
-import transformer_engine_cuda  # type: ignore
+from typing import Any, Callable
 from ..utils import import_file_as_module, get_return_type
+import torch
+from torch._ops import OpOverloadPacket, _OpNamespace  # type: ignore
+from torch._classes import _ClassNamespace  # type: ignore
+from torch._C import ScriptClass  # type: ignore
 
 
 def inject_real(namespace: dict[str, Any]):
@@ -23,24 +26,24 @@ def inject_real(namespace: dict[str, Any]):
         if issubclass(type_obj, Enum)
     }
 
-    real_functions = to_dict(
-        inspect.getmembers(transformer_engine_cuda, inspect.isfunction)
+    function_ns = torch.ops.transformer_engine_cuda  # type: ignore
+    assert isinstance(function_ns, _OpNamespace)
+    type_ns = torch.classes.transformer_engine_cuda  # type: ignore
+    assert isinstance(type_ns, _ClassNamespace)
+
+    real_function: Callable[[str], OpOverloadPacket] = lambda name: getattr(
+        function_ns, name
     )
-    real_types = to_dict(inspect.getmembers(transformer_engine_cuda, inspect.isclass))
+    real_type: Callable[[str], ScriptClass] = lambda name: getattr(type_ns, name)  # type: ignore
 
     for enum_name in enum_names:
         namespace[enum_name] = stub_types[enum_name]
 
     for class_name in stub_types.keys() - enum_names:
-        stub_type = stub_types[class_name]
-        real_type = real_types[class_name]
-        real_type.__annotations__ = stub_type.__annotations__
-        for attr_name, attr_obj in real_type.__dict__.items():
-            attr_obj.__annotations__ = stub_type.__dict__[attr_name].__annotations__
-        namespace[class_name] = real_type
+        namespace[class_name] = real_type(class_name)
 
     for func_name, func_obj in stub_functions.items():
-        real_func = real_functions[func_name]
+        real_func = real_function(func_name)
         exposed_return_type: type = get_return_type(func_obj)
 
         def wrapper(*args: Any) -> Any:
@@ -50,7 +53,7 @@ def inject_real(namespace: dict[str, Any]):
                     real_args += (arg.value,)
                 else:
                     real_args += (arg,)
-            result = real_func(*real_args)
+            result: Any = real_func(*real_args)
             if issubclass(exposed_return_type, Enum):
                 assert isinstance(result, int)
                 return exposed_return_type(result)  # type: ignore
