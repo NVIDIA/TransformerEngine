db25121f4f34825716a7fe181725c90d5fcb3ac3
Jan Bielak <jbielak@nvidia.com>
2023-08-24 19:51:30 +0200

    format .pyi
    Signed-off-by: Jan Bielak <jbielak@nvidia.com>

--- transformer_engine/pytorch/sequential/cpp_extensions/__init__.pyi
+++ transformer_engine/pytorch/sequential/cpp_extensions/__init__.pyi
@@ -41,14 +41,7 @@ class Tensor:
     amax: torch.Tensor
     scale: torch.Tensor
     scale_inv: torch.Tensor
-    def __init__(
-        self,
-        dtype: DType,
-        data: torch.Tensor,
-        amax: torch.Tensor,
-        scale: torch.Tensor,
-        scale_inv: torch.Tensor,
-    ) -> None: ...
+    def __init__(self, dtype: DType, data: torch.Tensor, amax: torch.Tensor, scale: torch.Tensor, scale_inv: torch.Tensor) -> None: ...
 
 def gelu(input: Tensor, output: Tensor) -> None: ...
 def dgelu(grad: Tensor, input: Tensor, output: Tensor) -> None: ...
@@ -62,238 +55,28 @@ def reglu(input: Tensor, output: Tensor) -> None: ...
 def dreglu(grad: Tensor, input: Tensor, output: Tensor) -> None: ...
 def fp8_quantize(input: Tensor, output: Tensor) -> None: ...
 def fp8_dequantize(input: Tensor, output: Tensor) -> None: ...
-def get_fused_attn_backend(
-    q_dtype: DType,
-    kv_dtype: DType,
-    qkv_layout: QKVLayout,
-    bias_type: BiasType,
-    attn_mask_type: MaskType,
-    dropout: float,
-    max_seqlen_q: int,
-    max_seqlen_kv: int,
-    head_dim: int,
-) -> FusedAttnBackend: ...
-def fused_attn_fwd_qkvpacked(
-    QKV: Tensor,
-    Bias: Tensor,
-    S: Tensor,
-    O: Tensor,
-    Aux_CTX_Tensors: Sequence[Tensor],
-    cu_seqlens: Tensor,
-    rng_state: Tensor,
-    max_seqlen: int,
-    is_training: bool,
-    attn_scale: float,
-    dropout: float,
-    qkv_layout: QKVLayout,
-    bias_type: BiasType,
-    attn_mask_type: MaskType,
-    workspace: Tensor,
-) -> None: ...
-def fused_attn_bwd_qkvpacked(
-    QKV: Tensor,
-    O: Tensor,
-    dO: Tensor,
-    S: Tensor,
-    dP: Tensor,
-    Aux_CTX_Tensors: Sequence[Tensor],
-    dQKV: Tensor,
-    dBias: Tensor,
-    cu_seqlens: Tensor,
-    max_seqlen: int,
-    attn_scale: float,
-    dropout: float,
-    qkv_layout: QKVLayout,
-    bias_type: BiasType,
-    attn_mask_type: MaskType,
-    workspace: Tensor,
-) -> None: ...
-def fused_attn_fwd_kvpacked(
-    Q: Tensor,
-    KV: Tensor,
-    Bias: Tensor,
-    S: Tensor,
-    O: Tensor,
-    Aux_CTX_Tensors: Sequence[Tensor],
-    cu_seqlens_q: Tensor,
-    cu_seqlens_kv: Tensor,
-    rng_state: Tensor,
-    max_seqlen_q: int,
-    max_seqlen_kv: int,
-    is_training: bool,
-    attn_scale: float,
-    dropout: float,
-    qkv_layout: QKVLayout,
-    bias_type: BiasType,
-    attn_mask_type: MaskType,
-    workspace: Tensor,
-) -> None: ...
-def fused_attn_bwd_kvpacked(
-    Q: Tensor,
-    KV: Tensor,
-    O: Tensor,
-    dO: Tensor,
-    S: Tensor,
-    dP: Tensor,
-    Aux_CTX_Tensors: Sequence[Tensor],
-    dQ: Tensor,
-    dKV: Tensor,
-    dBias: Tensor,
-    cu_seqlens_q: Tensor,
-    cu_seqlens_kv: Tensor,
-    max_seqlen_q: int,
-    max_seqlen_kv: int,
-    attn_scale: float,
-    dropout: float,
-    qkv_layout: QKVLayout,
-    bias_type: BiasType,
-    attn_mask_type: MaskType,
-    workspace: Tensor,
-) -> None: ...
-def cublas_gemm(
-    A: Tensor,
-    B: Tensor,
-    D: Tensor,
-    bias: Tensor,
-    pre_gelu_out: Tensor,
-    transa: bool,
-    transb: bool,
-    grad: bool,
-    workspace: Tensor,
-    accumulate: bool,
-    use_split_accumulator: bool,
-    math_sm_count: int,
-) -> None: ...
-def layernorm_fwd(
-    x: Tensor,
-    gamma: Tensor,
-    beta: Tensor,
-    epsilon: float,
-    z: Tensor,
-    mu: Tensor,
-    rsigma: Tensor,
-    multiprocessorCount: int,
-    workspace: Tensor,
-    barrier: Tensor,
-) -> None: ...
-def layernorm1p_fwd(
-    x: Tensor,
-    gamma: Tensor,
-    beta: Tensor,
-    epsilon: float,
-    z: Tensor,
-    mu: Tensor,
-    rsigma: Tensor,
-    multiprocessorCount: int,
-    workspace: Tensor,
-    barrier: Tensor,
-) -> None: ...
-def layernorm_bwd(
-    dz: Tensor,
-    x: Tensor,
-    mu: Tensor,
-    rsigma: Tensor,
-    gamma: Tensor,
-    dx: Tensor,
-    dgamma: Tensor,
-    dbeta: Tensor,
-    dgamma_part: Tensor,
-    dbeta_part: Tensor,
-    multiprocessorCount: int,
-    workspace: Tensor,
-    barrier: Tensor,
-) -> None: ...
-def layernorm1p_bwd(
-    dz: Tensor,
-    x: Tensor,
-    mu: Tensor,
-    rsigma: Tensor,
-    gamma: Tensor,
-    dx: Tensor,
-    dgamma: Tensor,
-    dbeta: Tensor,
-    dgamma_part: Tensor,
-    dbeta_part: Tensor,
-    multiprocessorCount: int,
-    workspace: Tensor,
-    barrier: Tensor,
-) -> None: ...
-def rmsnorm_fwd(
-    x: Tensor,
-    gamma: Tensor,
-    epsilon: float,
-    z: Tensor,
-    rsigma: Tensor,
-    multiprocessorCount: int,
-    workspace: Tensor,
-    barrier: Tensor,
-) -> None: ...
-def rmsnorm_bwd(
-    dz: Tensor,
-    x: Tensor,
-    rsigma: Tensor,
-    gamma: Tensor,
-    dx: Tensor,
-    dgamma: Tensor,
-    dgamma_part: Tensor,
-    multiprocessorCount: int,
-    workspace: Tensor,
-    barrier: Tensor,
-) -> None: ...
-def scaled_softmax_forward(
-    input: Tensor, softmax_results: Tensor, scale_factor: float
-) -> None: ...
-def scaled_softmax_backward(
-    incoming_grads: Tensor,
-    softmax_results: Tensor,
-    output_grads: Tensor,
-    scale_factor: float,
-) -> None: ...
-def scaled_masked_softmax_forward(
-    input: Tensor, mask: Tensor, softmax_results: Tensor, scale_factor: float
-) -> None: ...
-def scaled_masked_softmax_backward(
-    incoming_grads: Tensor,
-    softmax_results: Tensor,
-    output_grads: Tensor,
-    scale_factor: float,
-) -> None: ...
-def scaled_upper_triang_masked_softmax_forward(
-    input: Tensor, softmax_results: Tensor, scale_factor: float
-) -> None: ...
-def scaled_upper_triang_masked_softmax_backward(
-    incoming_grads: Tensor,
-    softmax_results: Tensor,
-    output_grads: Tensor,
-    scale_factor: float,
-) -> None: ...
-def cast_transpose(
-    input: Tensor, cast_output: Tensor, transposed_output: Tensor
-) -> None: ...
+def get_fused_attn_backend(q_dtype: DType, kv_dtype: DType, qkv_layout: QKVLayout, bias_type: BiasType, attn_mask_type: MaskType, dropout: float, max_seqlen_q: int, max_seqlen_kv: int, head_dim: int) -> FusedAttnBackend: ...
+def fused_attn_fwd_qkvpacked(QKV: Tensor, Bias: Tensor, S: Tensor, O: Tensor, Aux_CTX_Tensors: Sequence[Tensor], cu_seqlens: Tensor, rng_state: Tensor, max_seqlen: int, is_training: bool, attn_scale: float, dropout: float, qkv_layout: QKVLayout, bias_type: BiasType, attn_mask_type: MaskType, workspace: Tensor) -> None: ...
+def fused_attn_bwd_qkvpacked(QKV: Tensor, O: Tensor, dO: Tensor, S: Tensor, dP: Tensor, Aux_CTX_Tensors: Sequence[Tensor], dQKV: Tensor, dBias: Tensor, cu_seqlens: Tensor, max_seqlen: int, attn_scale: float, dropout: float, qkv_layout: QKVLayout, bias_type: BiasType, attn_mask_type: MaskType, workspace: Tensor) -> None: ...
+def fused_attn_fwd_kvpacked(Q: Tensor, KV: Tensor, Bias: Tensor, S: Tensor, O: Tensor, Aux_CTX_Tensors: Sequence[Tensor], cu_seqlens_q: Tensor, cu_seqlens_kv: Tensor, rng_state: Tensor, max_seqlen_q: int, max_seqlen_kv: int, is_training: bool, attn_scale: float, dropout: float, qkv_layout: QKVLayout, bias_type: BiasType, attn_mask_type: MaskType, workspace: Tensor) -> None: ...
+def fused_attn_bwd_kvpacked(Q: Tensor, KV: Tensor, O: Tensor, dO: Tensor, S: Tensor, dP: Tensor, Aux_CTX_Tensors: Sequence[Tensor], dQ: Tensor, dKV: Tensor, dBias: Tensor, cu_seqlens_q: Tensor, cu_seqlens_kv: Tensor, max_seqlen_q: int, max_seqlen_kv: int, attn_scale: float, dropout: float, qkv_layout: QKVLayout, bias_type: BiasType, attn_mask_type: MaskType, workspace: Tensor) -> None: ...
+def cublas_gemm(A: Tensor, B: Tensor, D: Tensor, bias: Tensor, pre_gelu_out: Tensor, transa: bool, transb: bool, grad: bool, workspace: Tensor, accumulate: bool, use_split_accumulator: bool, math_sm_count: int) -> None: ...
+def layernorm_fwd(x: Tensor, gamma: Tensor, beta: Tensor, epsilon: float, z: Tensor, mu: Tensor, rsigma: Tensor, multiprocessorCount: int, workspace: Tensor, barrier: Tensor) -> None: ...
+def layernorm1p_fwd(x: Tensor, gamma: Tensor, beta: Tensor, epsilon: float, z: Tensor, mu: Tensor, rsigma: Tensor, multiprocessorCount: int, workspace: Tensor, barrier: Tensor) -> None: ...
+def layernorm_bwd(dz: Tensor, x: Tensor, mu: Tensor, rsigma: Tensor, gamma: Tensor, dx: Tensor, dgamma: Tensor, dbeta: Tensor, dgamma_part: Tensor, dbeta_part: Tensor, multiprocessorCount: int, workspace: Tensor, barrier: Tensor) -> None: ...
+def layernorm1p_bwd(dz: Tensor, x: Tensor, mu: Tensor, rsigma: Tensor, gamma: Tensor, dx: Tensor, dgamma: Tensor, dbeta: Tensor, dgamma_part: Tensor, dbeta_part: Tensor, multiprocessorCount: int, workspace: Tensor, barrier: Tensor) -> None: ...
+def rmsnorm_fwd(x: Tensor, gamma: Tensor, epsilon: float, z: Tensor, rsigma: Tensor, multiprocessorCount: int, workspace: Tensor, barrier: Tensor) -> None: ...
+def rmsnorm_bwd(dz: Tensor, x: Tensor, rsigma: Tensor, gamma: Tensor, dx: Tensor, dgamma: Tensor, dgamma_part: Tensor, multiprocessorCount: int, workspace: Tensor, barrier: Tensor) -> None: ...
+def scaled_softmax_forward(input: Tensor, softmax_results: Tensor, scale_factor: float) -> None: ...
+def scaled_softmax_backward(incoming_grads: Tensor, softmax_results: Tensor, output_grads: Tensor, scale_factor: float) -> None: ...
+def scaled_masked_softmax_forward(input: Tensor, mask: Tensor, softmax_results: Tensor, scale_factor: float) -> None: ...
+def scaled_masked_softmax_backward(incoming_grads: Tensor, softmax_results: Tensor, output_grads: Tensor, scale_factor: float) -> None: ...
+def scaled_upper_triang_masked_softmax_forward(input: Tensor, softmax_results: Tensor, scale_factor: float) -> None: ...
+def scaled_upper_triang_masked_softmax_backward(incoming_grads: Tensor, softmax_results: Tensor, output_grads: Tensor, scale_factor: float) -> None: ...
+def cast_transpose(input: Tensor, cast_output: Tensor, transposed_output: Tensor) -> None: ...
 def transpose(input: Tensor, transposed_output: Tensor) -> None: ...
-def cast_transpose_dbias(
-    input: Tensor,
-    cast_output: Tensor,
-    transposed_output: Tensor,
-    dbias: Tensor,
-    workspace: Tensor,
-) -> None: ...
-def fp8_transpose_dbias(
-    input: Tensor, transposed_output: Tensor, dbias: Tensor, workspace: Tensor
-) -> None: ...
-def cast_transpose_dbias_dgelu(
-    input: Tensor,
-    gelu_input: Tensor,
-    cast_output: Tensor,
-    transposed_output: Tensor,
-    dbias: Tensor,
-    workspace: Tensor,
-) -> None: ...
-def dgeglu_cast_transpose(
-    input: Tensor, geglu_input: Tensor, cast_output: Tensor, transposed_output: Tensor
-) -> None: ...
-def multi_cast_transpose(
-    input_list: Sequence[Tensor],
-    cast_output_list: Sequence[Tensor],
-    transposed_output_list: Sequence[Tensor],
-) -> None: ...
+def cast_transpose_dbias(input: Tensor, cast_output: Tensor, transposed_output: Tensor, dbias: Tensor, workspace: Tensor) -> None: ...
+def fp8_transpose_dbias(input: Tensor, transposed_output: Tensor, dbias: Tensor, workspace: Tensor) -> None: ...
+def cast_transpose_dbias_dgelu(input: Tensor, gelu_input: Tensor, cast_output: Tensor, transposed_output: Tensor, dbias: Tensor, workspace: Tensor) -> None: ...
+def dgeglu_cast_transpose(input: Tensor, geglu_input: Tensor, cast_output: Tensor, transposed_output: Tensor) -> None: ...
+def multi_cast_transpose(input_list: Sequence[Tensor], cast_output_list: Sequence[Tensor], transposed_output_list: Sequence[Tensor]) -> None: ...
