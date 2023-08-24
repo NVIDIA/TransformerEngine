f736f3425f65c6ffa346b43fd2a2d4753e22073c
Jan Bielak <jbielak@nvidia.com>
2023-08-24 19:34:00 +0200

    print tensor
    Signed-off-by: Jan Bielak <jbielak@nvidia.com>

--- transformer_engine/pytorch/sequential/cpp_extensions/__init__.py
+++ transformer_engine/pytorch/sequential/cpp_extensions/__init__.py
@@ -1,9 +1,18 @@
+from typing import Any
 from .real import *
 from . import printing  # only for side effects
 
 # Make tensors printable
 
 raw_type: type = globals().pop("Tensor")
-class Tensor(raw_type): # type: ignore
-    def __repr__(self):
-        return printing.tensor_repr(self) # type: ignore
+
+
+class __TensorImpostor:
+    def __getattribute__(self, __name: str) -> Any:
+        if __name != "__repr__":
+            return getattr(raw_type, __name)
+        else:
+            return printing.tensor_repr  # type: ignore
+
+
+Tensor = __TensorImpostor()
