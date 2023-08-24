38ca18ab9ff9a7a464b2ec2f06f32a423c92cb90
Jan Bielak <jbielak@nvidia.com>
2023-08-24 19:55:21 +0200

    fix load issue
    Signed-off-by: Jan Bielak <jbielak@nvidia.com>

--- transformer_engine/pytorch/sequential/cpp_extensions/__init__.py
+++ transformer_engine/pytorch/sequential/cpp_extensions/__init__.py
@@ -1,4 +1,17 @@
 from typing import Any
 from .real import *
 
-# from . import printing  <- TODO
+from . import printing
+
+raw_tensor = globals().pop("Tensor")
+
+
+class __TensorImpostor:
+    def __getattribute__(self, __name: str) -> Any:
+        if __name == "__repr__":
+            return printing.tensor_repr  # type: ignore
+        else:
+            return getattr(raw_tensor, __name)
+
+
+Tensor = __TensorImpostor()
