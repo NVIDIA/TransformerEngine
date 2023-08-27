82a676f105619aff653e2f33a9ed8f9b2df3e8aa
Jan Bielak <jbielak@nvidia.com>
2023-08-27 17:29:57 +0200

    make cpp extension pure pybind again
    Signed-off-by: Jan Bielak <jbielak@nvidia.com>

--- transformer_engine/pytorch/sequential/cppsrc/pybind.cpp
+++ transformer_engine/pytorch/sequential/cppsrc/pybind.cpp
@@ -5,15 +5,6 @@
  * See LICENSE for license information.
  ************************************************************************/
 
-#include <ATen/ATen.h>
-#include <ATen/Dispatch.h>
-#include <ATen/cuda/CUDAContext.h>
-#include <ATen/cuda/CUDAGeneratorImpl.h>
-#include <ATen/cuda/CUDAGraphsUtils.cuh>
-#include <ATen/cudnn/Handle.h>
-#include <ATen/native/DispatchStub.h>
-#include <c10/cuda/CUDAStream.h>
-#include <c10/macros/Macros.h>
 #include <cstdlib>
 #include <cublasLt.h>
 #include <cuda.h>
@@ -22,9 +13,6 @@
 #include <exception>
 #include <memory>
 #include <stdexcept>
-#include <torch/extension.h>
-#include <torch/script.h>
-#include <torch/torch.h>
 #include <transformer_engine/activation.h>
 #include <transformer_engine/cast.h>
 #include <transformer_engine/fused_attn.h>
@@ -35,6 +23,7 @@
 #include <transformer_engine/transformer_engine.h>
 #include <transformer_engine/transpose.h>
 #include <type_traits>
+#include <utility>
 
 #include "type_list.h"
 
@@ -59,21 +48,51 @@ void cuda_check() {
   }
 }
 
-float *getDataPtr(at::Tensor t) {
-  return reinterpret_cast<float *>(t.data_ptr());
-}
+class Tensor {
+  NVTETensor tensor;
+
+public:
+  Tensor(void *data, const NVTEShape &shape, NVTEDType dtype, float *amax,
+         float *scale, float *scale_inv)
+      : tensor{nvte_tensor_create(data, shape, dtype, amax, scale, scale_inv)} {
+  }
+  Tensor(NVTETensor &&tensor_) : tensor{std::exchange{tensor_, nullptr}} {}
+  Tensor(Tensor &&other) noexcept
+      : tensor{std::exchange(other.tensor, nullptr)} {}
+  Tensor(const Tensor &) = delete;
+  Tensor &operator=(const Tensor &) = delete;
+  Tensor &operator=(Tensor &&) = delete;
+  ~Tensor() {
+    if (tensor)
+      nvte_tensor_destroy(tensor);
+  }
+  operator NVTETensor() const { return tensor; }
+  NVTEDType dtype() const { return nvte_tensor_type(tensor); }
+  NVTEShape shape() const { return nvte_tensor_shape(tensor); }
+  void *data() const { return nvte_tensor_data(tensor); }
+  float *amax() const { return nvte_tensor_amax(tensor); }
+  float *scale() const { return nvte_tensor_scale(tensor); }
+  float *scale_inv() const { return nvte_tensor_scale_inv(tensor); }
+};
 
 // ----------- Wrapper for NVTETensorPack -----------
 struct TensorPack : NVTETensorPack {
-  TensorPack(const std::vector<int64_t> &tensors_) : NVTETensorPack{} {
+  TensorPack(const std::vector<Tensor> &tensors_) : NVTETensorPack{} {
     size = tensors_.size();
     if (size > MAX_SIZE) {
       throw std::runtime_error("TensorPack size exceeds MAX_SIZE");
     }
     for (size_t i = 0; i < size; ++i) {
-      tensors[i] = reinterpret_cast<NVTETensor>(tensors_[i]);
+      tensors[i] = static_cast<NVTETensor>(tensors_[i]);
     }
     nvte_tensor_pack_create(this);
+  }
+  operator std::vector<Tensor>() const {
+    std::vector<Tensor> tensors_(size);
+    for (size_t i = 0; i < size; ++i) {
+      tensors_[i] = static_cast<Tensor>(tensors[i]);
+    }
+    return tensors_;
   }
   operator NVTETensorPack *() { return this; }
   operator const NVTETensorPack *() const { return this; }
@@ -86,24 +105,6 @@ template <typename T> struct exposed_type {
 };
 
 template <typename T> struct wrapped;
-
-#define TO_INT64_T(...)                                                        \
-  template <> struct wrapped<__VA_ARGS__> : exposed_type<int64_t> {            \
-    static __VA_ARGS__ wrap(int64_t arg) { return (__VA_ARGS__)arg; }          \
-    static int64_t unwrap(__VA_ARGS__ arg) { return (int64_t)arg; }            \
-  }
-
-TO_INT64_T(char);
-TO_INT64_T(unsigned char);
-TO_INT64_T(signed char);
-TO_INT64_T(unsigned short);
-TO_INT64_T(signed short);
-TO_INT64_T(unsigned int);
-TO_INT64_T(signed int);
-TO_INT64_T(unsigned long);
-TO_INT64_T(signed long);
-TO_INT64_T(unsigned long long);
-
 template <typename T> struct wrapped : exposed_type<T> {
   static T wrap(T arg) { return arg; }
   static T unwrap(T arg) { return arg; }
@@ -115,58 +116,30 @@ template <> struct wrapped<void> : exposed_type<void> {
   // void type, while conversion
   // should be skipped for void return type.
 };
-template <> struct wrapped<float> : exposed_type<double> {
-  static float wrap(double arg) { return arg; }
-  static double unwrap(float arg) { return arg; }
-};
-template <> struct wrapped<NVTETensor> : exposed_type<int64_t> {
-  static int64_t wrap(NVTETensor arg) { return reinterpret_cast<int64_t>(arg); }
-  static NVTETensor unwrap(int64_t arg) {
-    return reinterpret_cast<NVTETensor>(arg);
-  }
+template <> struct wrapped<NVTETensor> : exposed_type<Tensor> {
+  static Tensor wrap(NVTETensor arg) { return static_cast<Tensor>(arg); }
+  static NVTETensor unwrap(Tensor arg) { return static_cast<NVTETensor>(arg); }
 };
 template <>
-struct wrapped<NVTETensorPack *> : exposed_type<std::vector<int64_t>> {
-  static TensorPack unwrap(const std::vector<int64_t> &arg) {
+struct wrapped<NVTETensorPack *> : exposed_type<std::vector<Tensor>> {
+  static TensorPack unwrap(const std::vector<Tensor> &arg) {
     return TensorPack(arg);
   }
 };
 template <>
-struct wrapped<const NVTETensorPack *> : exposed_type<std::vector<int64_t>> {
-  static TensorPack unwrap(const std::vector<int64_t> &arg) {
+struct wrapped<const NVTETensorPack *> : exposed_type<std::vector<Tensor>> {
+  static TensorPack unwrap(const std::vector<Tensor> &arg) {
     return TensorPack(arg);
   }
 };
-template <> struct wrapped<NVTEDType> : exposed_type<int64_t> {
-  static int64_t wrap(NVTEDType arg) { return int64_t(arg); }
-  static NVTEDType unwrap(int64_t arg) { return NVTEDType(arg); }
-};
-template <> struct wrapped<NVTE_Fused_Attn_Backend> : exposed_type<int64_t> {
-  static int64_t wrap(NVTE_Fused_Attn_Backend arg) { return int64_t(arg); }
-  static NVTE_Fused_Attn_Backend unwrap(int64_t arg) {
-    return NVTE_Fused_Attn_Backend(arg);
+template <> struct wrapped<NVTEShape> : exposed_type<std::vector<size_t>> {
+  static std::vector<size_t> wrap(NVTEShape arg) {
+    return std::vector<size_t>(arg.data, arg.data + arg.ndim);
   }
-};
-template <> struct wrapped<NVTE_QKV_Layout> : exposed_type<int64_t> {
-  static int64_t wrap(NVTE_QKV_Layout arg) { return int64_t(arg); }
-  static NVTE_QKV_Layout unwrap(int64_t arg) { return NVTE_QKV_Layout(arg); }
-};
-template <> struct wrapped<NVTE_Bias_Type> : exposed_type<int64_t> {
-  static int64_t wrap(NVTE_Bias_Type arg) { return int64_t(arg); }
-  static NVTE_Bias_Type unwrap(int64_t arg) { return NVTE_Bias_Type(arg); }
-};
-template <> struct wrapped<NVTE_Mask_Type> : exposed_type<int64_t> {
-  static int64_t wrap(NVTE_Mask_Type arg) { return int64_t(arg); }
-  static NVTE_Mask_Type unwrap(int64_t arg) { return NVTE_Mask_Type(arg); }
-};
-template <> struct wrapped<NVTEShape> : exposed_type<std::vector<int64_t>> {
-  static std::vector<int64_t> wrap(NVTEShape arg) {
-    return std::vector<int64_t>(arg.data, arg.data + arg.ndim);
-  }
-  static NVTEShape unwrap(const std::vector<int64_t> &arg) {
+  static NVTEShape unwrap(const std::vector<size_t> &arg) {
     NVTEShape shape{};
     shape.ndim = arg.size();
-    shape.data = (size_t *)arg.data();
+    shape.data = arg.data();
     return shape;
   }
 };
@@ -177,22 +150,22 @@ struct at_scope_exit {
   ~at_scope_exit() { ptr(); }
 };
 
+// Makes the cuda stream argument always be the last argument
 template <typename Ret, typename... PrefixArgs, typename... SuffixArgs,
           typename... Args>
-constexpr auto
-remove_cuda_stream_arg_helper(Ret(func)(Args...), type_list<PrefixArgs...>,
-                              type_list<SuffixArgs...>) noexcept {
+constexpr auto cuda_stream_arg_helper(Ret(func)(Args...),
+                                      type_list<PrefixArgs...>,
+                                      type_list<SuffixArgs...>) noexcept {
   return [func](wrapped_t<PrefixArgs>... prefixArgs,
-                wrapped_t<SuffixArgs>... suffixArgs) -> wrapped_t<Ret> {
+                wrapped_t<SuffixArgs>... suffixArgs,
+                cudaStream_t stream) -> wrapped_t<Ret> {
     at_scope_exit _{cuda_check};
     if constexpr (!std::is_same_v<Ret, void>) {
       return wrapped<Ret>::wrap(
-          func(wrapped<PrefixArgs>::unwrap(prefixArgs)...,
-               at::cuda::getCurrentCUDAStream(),
+          func(wrapped<PrefixArgs>::unwrap(prefixArgs)..., stream,
                wrapped<SuffixArgs>::unwrap(suffixArgs)...));
     } else {
-      return func(wrapped<PrefixArgs>::unwrap(prefixArgs)...,
-                  at::cuda::getCurrentCUDAStream(),
+      return func(wrapped<PrefixArgs>::unwrap(prefixArgs)..., stream,
                   wrapped<SuffixArgs>::unwrap(suffixArgs)...);
     }
   };
@@ -205,7 +178,7 @@ constexpr auto wrap(Ret(func)(Args...)) noexcept {
     constexpr size_t stream_arg_idx = tl::template find<cudaStream_t>;
     using prefix = typename tl::template pop_back<tl::size - stream_arg_idx>;
     using suffix = typename tl::template pop_front<stream_arg_idx + 1>;
-    return remove_cuda_stream_arg_helper(func, prefix(), suffix());
+    return cuda_stream_arg_helper(func, prefix(), suffix());
   } else {
     return [func](wrapped_t<Args>... args) -> wrapped_t<Ret> {
       at_scope_exit _{cuda_check};
@@ -219,35 +192,63 @@ constexpr auto wrap(Ret(func)(Args...)) noexcept {
 }
 
 // Manual wrapper around nvte_multi_cast_transpose
-void multi_cast_transpose(const std::vector<int64_t> &inputs,
-                          const std::vector<int64_t> &cast_outs,
-                          const std::vector<int64_t> &transposed_outs) {
+void multi_cast_transpose(const std::vector<Tensor> &inputs,
+                          const std::vector<Tensor> &cast_outs,
+                          const std::vector<Tensor> &transposed_outs,
+                          cudaStream_t stream) {
   auto inputs_ = *reinterpret_cast<const std::vector<NVTETensor> *>(&inputs);
   auto cast_outs_ =
       *reinterpret_cast<const std::vector<NVTETensor> *>(&cast_outs);
   auto transposed_outs_ =
       *reinterpret_cast<const std::vector<NVTETensor> *>(&transposed_outs);
   nvte_multi_cast_transpose(inputs_.size(), inputs_.data(), cast_outs_.data(),
-                            transposed_outs_.data(),
-                            at::cuda::getCurrentCUDAStream());
+                            transposed_outs_.data(), stream);
 
   cuda_check();
 }
 
 // ----------- Registration of torch.ops -----------
-TORCH_LIBRARY(transformer_engine_cuda, m) {
-  m.def("create_tensor",
-        wrap(+[](NVTEDType dtype, const std::vector<int64_t> &shape,
-                 at::Tensor data, at::Tensor amax, at::Tensor scale,
-                 at::Tensor scale_inv) -> NVTETensor {
-          return nvte_create_tensor(
-              getDataPtr(data),
-              NVTEShape{(size_t *)(shape.data()), shape.size()}, dtype,
-              getDataPtr(amax), getDataPtr(scale), getDataPtr(scale_inv));
-        }));
-  m.def("get_tensor_dtype", wrap(nvte_tensor_type));
-  m.def("get_tensor_shape", wrap(nvte_tensor_shape));
-  m.def("destroy_tensor", wrap(nvte_destroy_tensor));
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+  py::enum_<NVTEDType>(m, "DType", py::module_local())
+      .value("Byte", kNVTEByte)
+      .value("Int32", kNVTEInt32)
+      .value("Int64", kNVTEInt64)
+      .value("Float32", kNVTEFloat32)
+      .value("Float16", kNVTEFloat16)
+      .value("BFloat16", kNVTEBFloat16)
+      .value("Float8E4M3", kNVTEFloat8E4M3)
+      .value("Float8E5M2", kNVTEFloat8E5M2);
+
+  py::enum_<NVTE_Fused_Attn_Backend>(m, "FusedAttnBackend", py::module_local())
+      .value("No_Backend", NVTE_No_Backend)
+      .value("F16_max512_seqlen", NVTE_F16_max512_seqlen)
+      .value("F16_arbitrary_seqlen", NVTE_F16_arbitrary_seqlen)
+      .value("FP8", NVTE_FP8);
+
+  py::enum_<NVTE_QKV_Layout>(m, "QKVLayout", py::module_local())
+      .value("NOT_INTERLEAVED", NVTE_NOT_INTERLEAVED)
+      .value("QKV_INTERLEAVED", NVTE_QKV_INTERLEAVED)
+      .value("KV_INTERLEAVED", NVTE_KV_INTERLEAVED);
+
+  py::enum_<NVTE_Bias_Type>(m, "BiasType", py::module_local())
+      .value("NO_BIAS", NVTE_NO_BIAS)
+      .value("PRE_SCALE_BIAS", NVTE_PRE_SCALE_BIAS)
+      .value("POST_SCALE_BIAS", NVTE_POST_SCALE_BIAS);
+
+  py::enum_<NVTE_Mask_Type>(m, "MaskType", py::module_local())
+      .value("NO_MASK", NVTE_NO_MASK)
+      .value("PADDING_MASK", NVTE_PADDING_MASK)
+      .value("CAUSAL_MASK", NVTE_CAUSAL_MASK);
+
+  py::class_<Tensor>(m, "Tensor", py::module_local())
+      .def(py::init<void *, const NVTEShape &, NVTEDType, float *, float *,
+                    float *>())
+      .def_property_readonly("dtype", &Tensor::dtype)
+      .def_property_readonly("shape", &Tensor::shape)
+      .def_property_readonly("data", &Tensor::data)
+      .def_property_readonly("amax", &Tensor::amax)
+      .def_property_readonly("scale", &Tensor::scale)
+      .def_property_readonly("scale_inv", &Tensor::scale_inv);
 
   m.def("gelu", wrap(nvte_gelu));
   m.def("dgelu", wrap(nvte_dgelu));
