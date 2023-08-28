387199d1cf9fe39db2f93b1bf3d8a4033976a2a0
Jan Bielak <jbielak@nvidia.com>
2023-08-28 16:00:09 +0200

    fix for dynamo
    Signed-off-by: Jan Bielak <jbielak@nvidia.com>

--- transformer_engine/pytorch/sequential/nvte/dtype.py
+++ transformer_engine/pytorch/sequential/nvte/dtype.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from .. import cpp_extensions as _nvte
 
-from ..cpp_extensions import te_to_torch_dtype, torch_to_te_dtype, dtype_name, bit_width
+from ..cpp_extensions import
 
 
 def is_fp8(t: _nvte.Tensor | _nvte.DType):
@@ -9,4 +9,4 @@ def is_fp8(t: _nvte.Tensor | _nvte.DType):
         dtype = t
     else:
         dtype = t.dtype
-    return dtype == _nvte.DType.Float8E4M3 or dtype == _nvte.DType.Float8E5M2
+    return dtype is _nvte.DType.Float8E4M3 or dtype is _nvte.DType.Float8E5M2
