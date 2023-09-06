f00b556b1db6d5a389bd798fd14b4054daad377e
Kirthi Shankar Sivamani <ksivamani@nvidia.com>
2023-09-06 10:18:50 -0700

    add plots
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>

--- tests/pytorch/distributed/print_logs.py
+++ tests/pytorch/distributed/print_logs.py
@@ -4,11 +4,15 @@
 
 import os
 import re
+import glob
 from prettytable import PrettyTable
+from matplotlib import pyplot as plt
 
 
 te_path = os.getenv("TE_PATH", "/opt/transformerengine")
 mlm_log_dir = os.path.join(te_path, "ci_logs")
+te_ci_log_dir = "/data/transformer_engine_ci_logs"
+te_ci_plot_dir = os.path.join(te_ci_log_dir, "plots")
 
 
 convergence_pattern = (
@@ -21,8 +25,7 @@ perf_pattern = "elapsed time per iteration \(ms\): ([\d.]*)"
 
 
 def get_output_file():
-    te_ci_log_dir = "/data/transformer_engine_ci_logs"
-    fname = f"te_pytorch_distributed_ci_{os.getenv('CI_PIPELINE_ID', 'unknown_id')}.txt"
+    fname = f"{os.getenv('CI_PIPELINE_ID', 'unknown_id')}.txt"
     return os.path.join(te_ci_log_dir, fname)
 
 
@@ -43,8 +46,9 @@ def get_run_metrics(filename):
     return loss, ppl, avg_step_time
 
 
-def main():
-    experiments = []
+def print_run_logs():
+    tables = []
+    raw_logs = []
     for model_config in os.listdir(mlm_log_dir):
         model_config_dir = os.path.join(mlm_log_dir, model_config)
         table = PrettyTable()
@@ -53,16 +57,62 @@ def main():
         for exp in os.listdir(model_config_dir):
             filename = os.path.join(model_config_dir, exp)
             loss, ppl, time_per_step = get_run_metrics(filename)
-            table.add_row([exp[:-4], loss, ppl, time_per_step])
-        experiments.append(table)
-
+            exp_name = exp[:-4]
+            table.add_row([exp_name, loss, ppl, time_per_step])
+            raw_logs.append(f"{model_config} {exp_name} {loss} {ppl} {time_per_step}\n")
+        tables.append(table)
 
     with open(get_output_file(), "w") as f:
-        for table in experiments:
-            f.write(str(table))
-            f.write("\n")
-            print(table)
+        for raw_log in raw_logs:
+            f.write(raw_log)
+    for table in tables:
+        print(table)
+
+
+def save_plot(title, legend, data, filename):
+    x = list(range(1, len(data[0]) + 1))
+    plt.figure()
+    for label, y in zip(legend, data):
+        plt.plot(x, y, "-o", label=label)
+    plt.title(title)
+    plt.legend()
+    plt.savefig(os.path.join(te_ci_plot_dir, filename))
+
+
+def perf_and_loss_plots():
+    files = glob.glob(os.path.join(te_ci_log_dir, "*.txt"))
+    files.sort(key=os.path.getctime)
+    data = {}
+    for filename in files:
+        with open(filename) as file:
+            for line in file:
+                line = line.strip()
+                model_config, exp_name, loss, _, time_per_step = line.split(" ")
+                if model_config not in data:
+                    data[model_config] = {}
+                if exp_name not in data[model_config]:
+                    data[model_config][exp_name] = {"loss": [], "perf": []}
+                data[model_config][exp_name]["loss"].append(float(loss))
+                data[model_config][exp_name]["perf"].append(float(time_per_step))
+
+    for model_config, experiments in data.items():
+        lm_loss_data = []
+        lm_perf_data = []
+        legend = []
+        for exp_name, lm_data in experiments.items():
+            legend.append(exp_name)
+            lm_loss_data.append(lm_data["loss"])
+            lm_perf_data.append(lm_data["perf"])
+        save_plot(
+            model_config + " LM-Loss", legend,
+            lm_loss_data, model_config + "_loss.png",
+        )
+        save_plot(
+            model_config + " Time per iteration (ms)",
+            legend, lm_perf_data, model_config + "_perf.png",
+        )
 
 
 if __name__ == "__main__":
-    main()
+    print_run_logs()
+    perf_and_loss_plots()
