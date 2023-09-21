fix formula
Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>

transformer_engine/jax/fp8.py
@@ -310,7 +310,7 @@ class FP8Helper:
                 amax = fp8_meta_arrays[fp8_amax_idx][..., 0:1]
             scale = fp8_meta_arrays[fp8_scale_idx]
 
-            sf = fp8_max / amax
+            sf = (fp8_max / amax) / (2 ** FP8Helper.MARGIN)
             sf = jnp.where(amax > 0.0, sf, scale)
             sf = jnp.where(jnp.isfinite(amax), sf, scale)
             fp8_meta_arrays[fp8_scale_idx] = scale
@@ -424,8 +424,7 @@ def update_fp8_metas(state: Collection) -> Collection:
 
     .. code-block:: python
 
-        exp = floor(log2(fp8_max / amax)) - margin
-        sf = round(power(2, abs(exp)))
+        sf = (fp8_max / amax) / (2 ^ margin)
         sf = sf if amax > 0.0, else original_scale
         sf = sf if isfinite(amax), else original_scale)
         updated_scale = 1/sf if exp < 0, else sf

transformer_engine/pytorch/fp8.py
@@ -538,7 +538,7 @@ def _default_sf_compute(
     margin: int,
 ) -> torch.Tensor:
     """Default function to convert amax to scaling factor."""
-    sf = fp8_max / amax
+    sf = (fp8_max / amax) / (2 ** margin)
     sf = torch.where(amax > 0.0, sf, scale)
     sf = torch.where(torch.isfinite(amax), sf, scale)
     return sf

transformer_engine/tensorflow/fp8.py
@@ -157,7 +157,7 @@ def get_fp8_recipe():
 
 def _default_sf_compute(amax, scale, fp8_max, margin):
     """Default function to convert amax to scaling factor."""
-    sf = fp8_max / amax
+    sf = (fp8_max / amax) / (2 ** margin)
     sf = tf.where(amax > 0.0, sf, scale)
     sf = tf.where(tf.math.is_finite(amax), sf, scale)
     return sf