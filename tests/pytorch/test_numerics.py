8e757a450780474e0d027558f945e8e9060775f4
Kirthi Shankar Sivamani <ksivamani@nvidia.com>
2023-10-12 13:34:13 -0700

    [PyTorch] RNG state support for model parallelism (#473)
    * Add class for RNG state tracker.
    
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>
    
    * Fix docs for checkpoint
    
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>
    
    * docs
    
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>
    
    ---------
    
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>

--- tests/pytorch/test_numerics.py
+++ tests/pytorch/test_numerics.py
@@ -4,7 +4,6 @@
 
 import math
 import os
-import contextlib
 from typing import List, Optional
 import pytest
 import copy
@@ -12,8 +11,6 @@ import copy
 import torch
 import torch.nn as nn
 from torch.nn import Parameter
-from torch import _C
-from torch.cuda import _lazy_call, device as device_ctx_manager
 
 from transformer_engine.pytorch.utils import (
     init_method_normal,
@@ -25,9 +22,10 @@ from transformer_engine.pytorch import (
     MultiheadAttention, RMSNorm, TransformerLayer
 )
 from transformer_engine.pytorch.distributed import checkpoint as te_checkpoint
+from transformer_engine.pytorch.distributed import _set_cuda_rng_state, CudaRNGStatesTracker
+
 
 seed = 1234
-rng_str = "rng_state"
 torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)
 # Record initial RNG state from script run.
@@ -91,119 +89,14 @@ def assert_allclose(l1: List[torch.Tensor], l2: List[torch.Tensor], atol: float)
             raise AssertionError(msg)
 
 
-def _set_cuda_rng_state(new_state, device=-1):
-    """Sets the random number generator state of the current GPU.
-
-    Argumentss:
-        new_state (torch.ByteTensor): The desired state
-    This function is adapted from PyTorch repo (torch.cuda.set_rng_state)
-    with a single change: the input state is not cloned. Cloning caused
-    major performance issues for +4 GPU cases.
-    """
-    if hasattr(_C, "_cuda_setRNGState") and callable(_C._cuda_setRNGState):
-        # older PyTorch
-        def cb():
-            with device_ctx_manager(device):
-                _C._cuda_setRNGState(new_state)
-
-    else:
-        # newer PyTorch
-        if device == -1:
-            device = torch.device("cuda")
-        elif isinstance(device, str):
-            device = torch.device(device)
-        elif isinstance(device, int):
-            device = torch.device("cuda", device)
-
-        def cb():
-            idx = device.index
-            if idx is None:
-                idx = torch.cuda.current_device()
-            default_generator = torch.cuda.default_generators[idx]
-            default_generator.set_state(new_state)
-
-    _lazy_call(cb)
-
-
 def reset_rng_states() -> None:
     # revert back to initial RNG state.
     torch.set_rng_state(_cpu_rng_state)
     _set_cuda_rng_state(_cuda_rng_state)
 
 
-class CudaRNGStatesTracker:
-    """Tracker for the cuda RNG states.
-
-    Using the `add` method, a cuda rng state is initialized based on
-    the input `seed` and is assigned to `name`. Later, by forking the
-    rng state, we can perform operations and return to our starting
-    cuda state.
-    """
-
-    def __init__(self):
-        # Map from a string name to the cuda rng state.
-        self.states_ = {}
-        # Seeds are just for book keeping and ensure no seed is set twice.
-        self.seeds_ = set()
-
-    def reset(self):
-        """Set to the initial state (no tracker)."""
-        self.states_ = {}
-        self.seeds_ = set()
-
-    def get_states(self):
-        """Get rng states. Copy the dictionary so we have direct
-        pointers to the states, not just a pointer to the dictionary."""
-        states = {}
-        for name in self.states_:
-            states[name] = self.states_[name]
-        return states
-
-    def set_states(self, states):
-        """Set the rng states. For efficiency purposes, we do not check
-        the size of seed for compatibility."""
-        self.states_ = states
-
-    def add(self, name, seed):
-        """Track the rng state."""
-        # Check seed is not already used.
-        if seed in self.seeds_:
-            raise Exception("seed {} already exists".format(seed))
-        self.seeds_.add(seed)
-        # Check that state is not already defined.
-        if name in self.states_:
-            raise Exception("cuda rng state {} already exists".format(name))
-        # Get the current rng state.
-        orig_rng_state = torch.cuda.get_rng_state()
-        # Set the new state and store it.
-        torch.cuda.manual_seed(seed)
-        self.states_[name] = torch.cuda.get_rng_state()
-        # Reset rng state to what it was.
-        _set_cuda_rng_state(orig_rng_state)
-
-    @contextlib.contextmanager
-    def fork(self, name=rng_str):
-        """Fork the cuda rng state, perform operations, and exit with
-        the original state."""
-        # Check if we have added the state
-        if name not in self.states_:
-            raise Exception("cuda rng state {} is not added".format(name))
-        # Store current rng state.
-        orig_cuda_rng_state = torch.cuda.get_rng_state()
-        # Set rng state to the desired one
-        _set_cuda_rng_state(self.states_[name])
-        # Do the stuff we wanted to do.
-        try:
-            yield
-        finally:
-            # Update the current rng state for later use.
-            self.states_[name] = torch.cuda.get_rng_state()
-            # And set the state to the original state we started with.
-            _set_cuda_rng_state(orig_cuda_rng_state)
-
-
 _DUMMY_CUDA_RNG_STATE_TRACKER = CudaRNGStatesTracker()
-_DUMMY_CUDA_RNG_STATE_TRACKER.add(rng_str, seed)
+_DUMMY_CUDA_RNG_STATE_TRACKER.add("model-parallel-rng", seed)
 
 
 def get_dummy_cuda_rng_tracker():
