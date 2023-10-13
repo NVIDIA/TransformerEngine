670e390897dbf0f055491e5de80918d4f8314826
Tim Moon <tmoon@nvidia.com>
2023-10-13 11:59:02 -0700

    Explicitly specify that PyTorch QA tests use PyTorch
    Signed-off-by: Tim Moon <tmoon@nvidia.com>

--- qa/L0_pytorch_lint/test.sh
+++ qa/L0_pytorch_lint/test.sh
@@ -9,7 +9,7 @@ set -e
 pip install cpplint==1.6.0 pylint==2.13.5
 if [ -z "${PYTHON_ONLY}" ]
 then
-  cp $TE_PATH/qa/L0_lint/CPPLINT.cfg $TE_PATH
+  cp $TE_PATH/qa/L0_pytorch_lint/CPPLINT.cfg $TE_PATH
   cd $TE_PATH
   echo "Checking common API headers"
   cpplint --root transformer_engine/common/include --recursive transformer_engine/common/include
@@ -19,7 +19,7 @@ then
 fi
 if [ -z "${CPP_ONLY}" ]
 then
-  cp $TE_PATH/qa/L0_lint/pylintrc $TE_PATH
+  cp $TE_PATH/qa/L0_pytorch_lint/pylintrc $TE_PATH
   cd $TE_PATH
   echo "Checking Python files"
   pylint --recursive=y transformer_engine/common transformer_engine/pytorch
