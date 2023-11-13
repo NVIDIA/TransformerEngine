1a45c7c62ffebb4d5884405b2f83b7399b8c35bc
Ming Huang <mingh@nvidia.com>
2023-11-13 10:27:51 +0000

    Adding license to test_distributed_*
    Signed-off-by: Ming Huang <mingh@nvidia.com>

--- tests/jax/distributed_test_base.py
+++ tests/jax/distributed_test_base.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2022-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # See LICENSE for license information.
+
 import jax
 from jax.experimental.pjit import pjit, _UNSPECIFIED
 
