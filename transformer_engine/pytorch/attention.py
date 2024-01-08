bb759adc2a53c57cc387f531c580a16149e3d4ac
Tim Moon <4406448+timmoon10@users.noreply.github.com>
2024-01-08 11:40:20 -0800

    [PyTorch] Refactor parameter splitting in Linear and LayerNormLinear (#590)
    * Refactor parameter split in Linear module
    
    Remove module state from noop_cat. Support arbitrary names in parameter split. Handle tensor parallelism.
    
    Signed-off-by: Tim Moon <tmoon@nvidia.com>
    
    * Make noop_cat a standalone operation
    
    Signed-off-by: Tim Moon <tmoon@nvidia.com>
    
    * Update parameter splits in LayerNormLinear
    
    Signed-off-by: Tim Moon <tmoon@nvidia.com>
    
    * Debug case without bias
    
    Fix pylint complaints.
    
    Signed-off-by: Tim Moon <tmoon@nvidia.com>
    
    * Remove unused import
    
    Signed-off-by: Tim Moon <tmoon@nvidia.com>
    
    ---------
    
    Signed-off-by: Tim Moon <tmoon@nvidia.com>

--- transformer_engine/pytorch/attention.py
+++ transformer_engine/pytorch/attention.py
@@ -3,6 +3,7 @@
 # See LICENSE for license information.
 
 """Attention."""
+import collections
 import os
 import warnings
 import math
@@ -2705,9 +2706,13 @@ class MultiheadAttention(torch.nn.Module):
         qkv_parallel_mode = "column" if set_parallel_mode else None
 
         if self.attention_type == "self":
-            parameters_split = {"query_": hidden_size,
-                                "key_": self.hidden_size_kv,
-                                "value_": self.hidden_size_kv} if not fuse_qkv_params else None
+            parameters_split = None
+            if not fuse_qkv_params:
+                parameters_split = collections.OrderedDict([
+                    ("query", hidden_size),
+                    ("key", self.hidden_size_kv),
+                    ("value", self.hidden_size_kv),
+                ])
             if self.input_layernorm:
                 self.layernorm_qkv = LayerNormLinear(
                     hidden_size,
@@ -2749,7 +2754,7 @@ class MultiheadAttention(torch.nn.Module):
                     bias=bias,
                     return_bias=False,
                     parallel_mode=qkv_parallel_mode,
-                    parameters_split=("query_",) if not fuse_qkv_params else None,
+                    parameters_split=("query",) if not fuse_qkv_params else None,
                     return_layernorm_output=return_layernorm_output,
                     zero_centered_gamma=zero_centered_gamma,
                     ub_bulk_wgrad=ub_bulk_wgrad,
@@ -2777,7 +2782,7 @@ class MultiheadAttention(torch.nn.Module):
                 bias=bias,
                 return_bias=False,
                 parallel_mode=qkv_parallel_mode,
-                parameters_split=("key_", "value_") if not fuse_qkv_params else None,
+                parameters_split=("key", "value") if not fuse_qkv_params else None,
                 **common_gemm_kwargs,
             )
 
