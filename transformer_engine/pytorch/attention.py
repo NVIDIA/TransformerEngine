5456494b09660311b9f933e2c3a38597dd3f6067
Markus Schnoes <markus.schnoes@gmx.de>
2024-01-18 14:31:18 +0100

    [PyTorch] fix forward attention_type in MultiheadAttention
    Signed-off-by: Markus Schnoes <markus.schnoes@gmx.de>

--- transformer_engine/pytorch/attention.py
+++ transformer_engine/pytorch/attention.py
@@ -3050,6 +3050,7 @@ class MultiheadAttention(torch.nn.Module):
             sequence_parallel=sequence_parallel,
             tp_group=tp_group,
             layer_number=self.layer_number,
+            attention_type=self.attention_type,
         )
 
         # Linear
