6682b18da6b7be4e70ed34f8831fe5ed9d766c5a
Selvaraj Anandaraj <selvaraja@login-eos01.eos.clusters.nvidia.com>
2024-01-25 00:56:09 -0800

    Fixed offloading for attention activations
    Signed-off-by: Selvaraj Anandaraj <selvaraja@login-eos01.eos.clusters.nvidia.com>

--- transformer_engine/pytorch/attention.py
+++ transformer_engine/pytorch/attention.py
@@ -1864,7 +1864,7 @@ class FusedAttnFunc(torch.autograd.Function):
 
         from .cpu_offload import CPUOffloadEnabled
         if CPUOffloadEnabled:
-            tensor_list = [q, k, v, out, cu_seqlens_q, cu_seqlens_kv]
+            tensor_list = [v, out, cu_seqlens_q, cu_seqlens_kv]
             for tensor in tensor_list:
                 if tensor is not None:
                     tensor.activation_offloading = True
@@ -2748,6 +2748,9 @@ class DotProductAttention(torch.nn.Module):
 
         assert (not context_parallel), \
             "Context parallelism is only implemented with Flash Attention and Fused Attention!"
+
+        assert (not CPUOffloadEnabled), \
+            "CPU Offloading is only implemented with Flash Attention and Fused Attention!"
 
         if _NVTE_DEBUG:
             print("[DotProductAttention]: using unfused DPA")
