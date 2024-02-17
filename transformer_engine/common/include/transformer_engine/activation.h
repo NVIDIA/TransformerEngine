a6ecd1bdf47fb1133f24d69306c1103a39bea869
Alp Dener <adener@nvidia.com>
2024-02-17 03:05:36 +0000

    indent fix
    Signed-off-by: Alp Dener <adener@nvidia.com>

--- transformer_engine/common/include/transformer_engine/activation.h
+++ transformer_engine/common/include/transformer_engine/activation.h
@@ -47,8 +47,8 @@ void nvte_dgelu(const NVTETensor grad,
  *  \param[in]     stream    CUDA stream used for the operation.
  */
 void nvte_geglu(const NVTETensor input,
-                      NVTETensor output,
-                      cudaStream_t stream);
+                NVTETensor output,
+                cudaStream_t stream);
 
 /*! \brief Compute GeGLU gradient.
  *  \param[in]     grad      Incoming gradient of shape [N, H].
@@ -113,8 +113,8 @@ void nvte_dswiglu(const NVTETensor grad,
  *  \param[in]     stream    CUDA stream used for the operation.
  */
 void nvte_reglu(const NVTETensor input,
-                 NVTETensor output,
-                 cudaStream_t stream);
+                NVTETensor output,
+                cudaStream_t stream);
 
 /*! \brief Compute ReGLU gradient.
  *  \param[in]     grad      Incoming gradient of shape [N, H].
@@ -123,9 +123,9 @@ void nvte_reglu(const NVTETensor input,
  *  \param[in]     stream    CUDA stream used for the operation.
  */
 void nvte_dreglu(const NVTETensor grad,
-                  const NVTETensor input,
-                  NVTETensor output,
-                  cudaStream_t stream);
+                 const NVTETensor input,
+                 NVTETensor output,
+                 cudaStream_t stream);
 
 /*! \brief Compute QuickGELU activation of the input.
  *
@@ -134,8 +134,8 @@ void nvte_dreglu(const NVTETensor grad,
  *  \param[in]     stream    CUDA stream used for the operation.
  */
 void nvte_qgelu(const NVTETensor input,
-               NVTETensor output,
-               cudaStream_t stream);
+                NVTETensor output,
+                cudaStream_t stream);
 
 /*! \brief Compute QuickGELU activation gradient.
  *
@@ -145,9 +145,9 @@ void nvte_qgelu(const NVTETensor input,
  *  \param[in]     stream    CUDA stream used for the operation.
  */
 void nvte_dqgelu(const NVTETensor grad,
-                const NVTETensor input,
-                NVTETensor output,
-                cudaStream_t stream);
+                 const NVTETensor input,
+                 NVTETensor output,
+                 cudaStream_t stream);
 
 #ifdef __cplusplus
 }  // extern "C"
