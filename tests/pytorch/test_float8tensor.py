1cd94388a96972542fae5957c30fc624c387de88
Kirthi Shankar Sivamani <ksivamani@nvidia.com>
2024-03-22 23:40:46 -0700

    fix float8tensor test
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>

--- tests/pytorch/test_float8tensor.py
+++ tests/pytorch/test_float8tensor.py
@@ -258,11 +258,9 @@ class TestFloat8Tensor:
             torch.testing.assert_close(x_fp8, x_ref, **tols)
 
     @pytest.mark.parametrize("dims", [[33, 41], [5, 7, 11]])
-    @pytest.mark.parametrize("transpose_dims", [(0, 1), (-2, -1), (0, 0)])
     def test_transpose(
         self,
         dims: DimsType,
-        transpose_dims: Tuple[int, int],
         fp8_dtype: tex.DType = tex.DType.kFloat8E4M3,
         scale: float = 0.5,
         dtype: torch.dtype = torch.float32,
@@ -280,19 +278,14 @@ class TestFloat8Tensor:
         x = x_fp8.from_float8()
 
         # Perform transpose
-        x_fp8_t = x_fp8._transpose(*transpose_dims)
-        x_t = x.transpose(*transpose_dims)
+        x_fp8_t = x_fp8._transpose()
+        x_t = x.transpose()
         if x_fp8_t.dtype == torch.uint8:
             x_fp8_t = Float8Tensor.make_like(x_fp8, data=x_fp8_t)
 
         # Check results
         tols = dict(rtol=0, atol=0)
         torch.testing.assert_close(x_fp8_t, x_t, **tols)
-
-        # Make sure we are not trivially passing the test
-        if transpose_dims[0] != transpose_dims[1]:
-            with pytest.raises(AssertionError):
-                torch.testing.assert_close(x_fp8_t, x, **tols)
 
     def test_serialization(
         self,
