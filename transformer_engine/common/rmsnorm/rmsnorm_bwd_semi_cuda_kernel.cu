48d54789440fdb51a7b9d6ec7fa63287a4e1a53d
Tim Moon <4406448+timmoon10@users.noreply.github.com>
2024-04-04 11:54:01 -0700

    Compile tuned RMSNorm kernels for hidden size 8192 (#747)
    Signed-off-by: Tim Moon <tmoon@nvidia.com>
    Signed-off-by: Pawel Gadzinski <pgadzinski@nvidia.com>

--- transformer_engine/common/rmsnorm/rmsnorm_bwd_semi_cuda_kernel.cu
+++ transformer_engine/common/rmsnorm/rmsnorm_bwd_semi_cuda_kernel.cu
@@ -201,6 +201,10 @@ REGISTER_BWD_TUNED_LAUNCHER(4096, fp32, fp32, fp32, fp32, 1, 1, 4, 16, 4);
 REGISTER_BWD_TUNED_LAUNCHER(4096, fp16, fp16, fp16, fp32, 1, 1, 4, 16, 4);
 REGISTER_BWD_TUNED_LAUNCHER(4096, bf16, bf16, bf16, fp32, 1, 1, 4, 16, 4);
 
+REGISTER_BWD_TUNED_LAUNCHER(8192, fp32, fp32, fp32, fp32, 1, 1, 4, 16, 4);
+REGISTER_BWD_TUNED_LAUNCHER(8192, fp16, fp16, fp16, fp32, 1, 1, 4, 16, 4);
+REGISTER_BWD_TUNED_LAUNCHER(8192, bf16, bf16, bf16, fp32, 1, 1, 4, 16, 4);
+
 // Create rmsnorm general launch function and register. Macro signature:
 //  HIDDEN_SIZE, WTYPE, ITYPE, OTYPE, CTYPE, ...
 //                             WARPS_M, WARPS_N, BYTES_PER_LDG, BYTES_PER_LDG_FINAL
