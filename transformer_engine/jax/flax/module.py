Fixing the wrong shape of axes to LoRA weights.
Signed-off-by: Ming Huang <mingh@nvidia.com>

transformer_engine/jax/flax/module.py
@@ -480,20 +480,22 @@ class DenseGeneral(TransformerEngineBase):
                                    self.low_rank_adaptation_dim)
             lora_a_kernel_init_shape = (kernel_param_shape[0], *features[:-1],
                                         self.low_rank_adaptation_dim)
+            lora_a_kernel_axes = (None,) * len(lora_a_kernel_init_shape)
             lora_a_kernel = nn_partitioning.param_with_axes('lora_a_kernel',
                                                             self.kernel_init,
                                                             lora_a_kernel_init_shape,
                                                             jnp.float32,
-                                                            axes=None)
+                                                            axes=lora_a_kernel_axes)
             lora_a_kernel = jnp.reshape(lora_a_kernel, lora_a_kernel_shape)
             lora_a_kernel = lora_a_kernel.astype(self.dtype)
 
             lora_b_kernel_shape = (*features[:-1], self.low_rank_adaptation_dim, features[-1])
+            lora_b_kernel_axes = (None,) * len(lora_b_kernel_shape)
             lora_b_kernel = nn_partitioning.param_with_axes('lora_b_kernel',
                                                             nn.initializers.zeros,
                                                             lora_b_kernel_shape,
                                                             jnp.float32,
-                                                            axes=None)
+                                                            axes=lora_b_kernel_axes)
             lora_b_kernel = lora_b_kernel.astype(self.dtype)
 
             y += _apply_low_rank_adaptation(inputs, axis, features, lora_a_kernel, lora_b_kernel,
@@ -726,20 +728,22 @@ class LayerNormDenseGeneral(TransformerEngineBase):
                                    self.low_rank_adaptation_dim)
             lora_a_kernel_init_shape = (kernel_param_shape[0], *features[:-1],
                                         self.low_rank_adaptation_dim)
+            lora_a_kernel_axes = (None,) * len(lora_a_kernel_init_shape)
             lora_a_kernel = nn_partitioning.param_with_axes('lora_a_kernel',
                                                             self.kernel_init,
                                                             lora_a_kernel_init_shape,
                                                             jnp.float32,
-                                                            axes=None)
+                                                            axes=lora_a_kernel_axes)
             lora_a_kernel = jnp.reshape(lora_a_kernel, lora_a_kernel_shape)
             lora_a_kernel = lora_a_kernel.astype(self.dtype)
 
             lora_b_kernel_shape = (*features[:-1], self.low_rank_adaptation_dim, features[-1])
+            lora_b_kernel_axes = (None,) * len(lora_b_kernel_shape)
             lora_b_kernel = nn_partitioning.param_with_axes('lora_b_kernel',
                                                             nn.initializers.zeros,
                                                             lora_b_kernel_shape,
                                                             jnp.float32,
-                                                            axes=None)
+                                                            axes=lora_b_kernel_axes)
             lora_b_kernel = lora_b_kernel.astype(self.dtype)
 
             z += _apply_low_rank_adaptation(y, axis, features, lora_a_kernel, lora_b_kernel,
@@ -1111,23 +1115,25 @@ class LayerNormMLP(TransformerEngineBase):
                 wi_lora_a_kernel_shape = (*kernel_1_shape[:len(axis)], num_activations,
                                           self.low_rank_adaptation_dim)
                 wi_lora_a_kernel_init_shape = (kernel_1_each_shape[0], self.low_rank_adaptation_dim)
+                wi_lora_a_kernel_axes = (None,) * len(wi_lora_a_kernel_init_shape)
                 wi_lora_a_kernel = nn_partitioning.param_with_axes('wi_lora_a_kernel',
                                                                    kernel_1_init,
                                                                    num_activations,
                                                                    -2,
                                                                    wi_lora_a_kernel_init_shape,
                                                                    jnp.float32,
-                                                                   axes=None)
+                                                                   axes=wi_lora_a_kernel_axes)
                 wi_lora_a_kernel = jnp.reshape(wi_lora_a_kernel, wi_lora_a_kernel_shape)
                 wi_lora_a_kernel = wi_lora_a_kernel.astype(self.dtype)
 
                 wi_lora_b_kernel_shape = (num_activations, self.low_rank_adaptation_dim,
                                           self.intermediate_dim)
+                wi_lora_b_kernel_axes = (None,) * len(wi_lora_b_kernel_shape)
                 wi_lora_b_kernel = nn_partitioning.param_with_axes('wi_lora_b_kernel',
                                                                    nn.initializers.zeros,
                                                                    wi_lora_b_kernel_shape,
                                                                    jnp.float32,
-                                                                   axes=None)
+                                                                   axes=wi_lora_b_kernel_axes)
                 wi_lora_b_kernel = wi_lora_b_kernel.astype(self.dtype)
 
                 x += _apply_low_rank_adaptation(y, axis, intermediate_dim, wi_lora_a_kernel,
@@ -1178,19 +1184,21 @@ class LayerNormMLP(TransformerEngineBase):
 
             if self.enable_low_rank_adaptation:
                 wo_lora_a_kernel_shape = (self.intermediate_dim, self.low_rank_adaptation_dim)
+                wo_lora_a_kernel_axes = (None,) * len(wo_lora_a_kernel_shape)
                 wo_lora_a_kernel = nn_partitioning.param_with_axes('wo_lora_a_kernel',
                                                                    self.kernel_init,
                                                                    wo_lora_a_kernel_shape,
                                                                    jnp.float32,
-                                                                   axes=None)
+                                                                   axes=wo_lora_a_kernel_axes)
                 wo_lora_a_kernel = wo_lora_a_kernel.astype(self.dtype)
 
                 wo_lora_b_kernel_shape = (self.low_rank_adaptation_dim, hidden_size)
+                wo_lora_b_kernel_axes = (None,) * len(wo_lora_b_kernel_shape)
                 wo_lora_b_kernel = nn_partitioning.param_with_axes('wo_lora_b_kernel',
                                                                    nn.initializers.zeros,
                                                                    wo_lora_b_kernel_shape,
                                                                    jnp.float32,
-                                                                   axes=None)
+                                                                   axes=wo_lora_b_kernel_axes)
                 wo_lora_b_kernel = wo_lora_b_kernel.astype(self.dtype)
 
                 out += _apply_low_rank_adaptation(z, axis, hidden_size_tuple, wo_lora_a_kernel,