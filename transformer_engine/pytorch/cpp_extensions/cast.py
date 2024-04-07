d79d661ac6b5cc5e5a3baa9d877c83ad48c49810
Dennis Liu <denliu@nvidia.com>
2024-04-07 00:26:09 -0700

    Fix unittest.
    Signed-off-by: Dennis Liu <denliu@nvidia.com>

--- transformer_engine/pytorch/cpp_extensions/cast.py
+++ transformer_engine/pytorch/cpp_extensions/cast.py
@@ -33,16 +33,15 @@ def cast_to_fp8(
                 otype
             )
         return None
-    if inp.nelement() > 0:
-        return torch.ops.tex_ts.cast_to_fp8_ts(
-            inp,
-            fp8_meta_tensor.scale,
-            fp8_meta_tensor.amax_history,
-            fp8_meta_tensor.scale_inv,
-            fp8_tensor,
-            otype,
-        )
-    return inp
+
+    return torch.ops.tex_ts.cast_to_fp8_ts(
+        inp,
+        fp8_meta_tensor.scale,
+        fp8_meta_tensor.amax_history,
+        fp8_meta_tensor.scale_inv,
+        fp8_tensor,
+        otype,
+    )
 
 def cast_from_fp8(
     inp: torch.Tensor,
