39c2498c826ea514f69b40f746c23c8f22391e8a
Kirthi Shankar Sivamani <ksivamani@nvidia.com>
2024-04-15 21:50:04 -0700

    Use torch function as a class method
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>

--- transformer_engine/pytorch/float8_tensor.py
+++ transformer_engine/pytorch/float8_tensor.py
@@ -263,7 +263,7 @@ class _ViewFunc(torch.autograd.Function):
     @staticmethod
     def backward(ctx,
         grad: torch.Tensor,
-    ) -> Tuple[[torch.Tensor, None], ...]:
+    ) -> Tuple[Union[torch.Tensor, None], ...]:
 
         if isinstance(grad, Float8Tensor):
             dgrad = Float8Tensor.make_like(
@@ -853,5 +853,8 @@ class Float8Tensor(torch.Tensor):
     _transpose_invalid = property(**_make_fp8_attr_property_funcs("transpose_invalid"))
     _scale_inv = property(**_make_fp8_attr_property_funcs("scale_inv"))
 
-    # Do not force the Float8Tensor type on the returned tensor
-    __torch_function__ = torch._C._disabled_torch_function_impl
+    @classmethod
+    def __torch_function__(cls, func, types, args=(), kwargs=None):
+        if kwargs is None:
+            kwargs = {}
+        return torch._C._disabled_torch_function_impl(func, types, args, kwargs)
