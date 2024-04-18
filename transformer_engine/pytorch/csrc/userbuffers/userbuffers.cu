e22821042560b6a4c0149cbaaecca6a8f3592fbb
Pavel Shamis (Pasha) <pasharesearch@gmail.com>
2024-04-18 08:58:54 -0700

    Adding macro to provide an option to enable/disable CE deadlock detector
    Since CE deadlock detector launch and additional increment kernel it
    may introduce an overhead to the communication flow. Adding in
    option to disable it.
    
    Signed-off-by: Pavel Shamis (Pasha) <pasharesearch@gmail.com>
    Signed-off-by: Pasha (Pavel) Shamis <pasharesearch@gmail.com>

--- transformer_engine/pytorch/csrc/userbuffers/userbuffers.cu
+++ transformer_engine/pytorch/csrc/userbuffers/userbuffers.cu
@@ -2033,6 +2033,15 @@ __global__ void kuserbuffers_inc(int *id) {
   atomicAdd(id, 1);
 }
 
+#if CE_DEADLOCK_DETECTOR
+#define LAUNCH_CE_CHECK_INC(stream, id)                               \
+do {                                                                  \
+  kuserbuffers_inc<<<1, 1, 0, stream>>>(reinterpret_cast<int *>(id)); \
+} while (0)
+#else
+#define LAUNCH_CE_CHECK_INC(stream, id)
+#endif
+
 __global__ void kuserbuffers_dummy(void) {}
 
 __global__ void __launch_bounds__(MAX_THREADS)
@@ -2111,8 +2120,12 @@ __global__ void __launch_bounds__(MAX_THREADS)
   }
 }
 
+#if CE_DEADLOCK_DETECTOR
 #define CHECK_CE(ce_start, ce_end) ((ce_start) != nullptr && (ce_end) != nullptr && \
                                     *(ce_start) != *(ce_end))
+#else
+#define CHECK_CE(ce_start, ce_end) (false)
+#endif
 
 __global__ void kuserbuffers_pushrecv(int myrank, int peer, int nvrank, int nvpeer, int *recv_id,
                                       int *flagptr, int adder, uint64_t ub_timeout,
@@ -2392,9 +2405,9 @@ void userbuffers_send(const int srchandler, const size_t srcoffset, const int ds
     void *dstptr = reinterpret_cast<char *>(comm->peer_ptr[dsthandler][peerlocal]) + dstoffset;
 
     if (comm->use_ce) {
-      kuserbuffers_inc<<<1, 1, 0, stream>>>(reinterpret_cast<int *>(ce_send_start_ptr));
+      LAUNCH_CE_CHECK_INC(stream, (reinterpret_cast<int *>(ce_send_start_ptr)));
       CUDACHECK(cudaMemcpyAsync(dstptr, srcptr, bytes, cudaMemcpyDeviceToDevice, stream));
-      kuserbuffers_inc<<<1, 1, 0, stream>>>(reinterpret_cast<int *>(ce_send_end_ptr));
+      LAUNCH_CE_CHECK_INC(stream, (reinterpret_cast<int *>(ce_send_end_ptr)));
     }
     SETUP_LAUNCH_CONFIG(signalonly ? 1 : comm->sms, signalonly ? 1 : 1024, stream);
     int *arg1 = &comm->send_id[peer], *arg2 = reinterpret_cast<int *>(flagptr);
@@ -2424,9 +2437,9 @@ void userbuffers_sendrecv(const int srchandler, const int dsthandler, const size
                       + send_offset;
 
   if (comm->use_ce) {
-    kuserbuffers_inc<<<1, 1, 0, stream>>>(reinterpret_cast<int *>(ce_send_start_ptr));
+    LAUNCH_CE_CHECK_INC(stream, (reinterpret_cast<int *>(ce_send_start_ptr)));
     CUDACHECK(cudaMemcpyAsync(send_dstptr, send_srcptr, bytes, cudaMemcpyDeviceToDevice, stream));
-    kuserbuffers_inc<<<1, 1, 0, stream>>>(reinterpret_cast<int *>(ce_send_end_ptr));
+    LAUNCH_CE_CHECK_INC(stream, (reinterpret_cast<int *>(ce_send_end_ptr)));
   }
   SETUP_LAUNCH_CONFIG(signalonly ? 1 : comm->sms, signalonly ? 1 : 1024, stream);
 
@@ -2479,9 +2492,9 @@ void userbuffers_sendrecv_atomic(const int srchandler, const int dsthandler,
   void *send_dstptr = reinterpret_cast<char *>(comm->peer_ptr[dsthandler][send_peerlocal])
                       + send_offset;
   if (comm->use_ce) {
-    kuserbuffers_inc<<<1, 1, 0, stream>>>(reinterpret_cast<int *>(ce_send_start_ptr));
+    LAUNCH_CE_CHECK_INC(stream, (reinterpret_cast<int *>(ce_send_start_ptr)));
     CUDACHECK(cudaMemcpyAsync(send_dstptr, send_srcptr, bytes, cudaMemcpyDeviceToDevice, stream));
-    kuserbuffers_inc<<<1, 1, 0, stream>>>(reinterpret_cast<int *>(ce_send_end_ptr));
+    LAUNCH_CE_CHECK_INC(stream, (reinterpret_cast<int *>(ce_send_end_ptr)));
   }
   SETUP_LAUNCH_CONFIG(signalonly ? 1 : comm->sms, signalonly ? 1 : 1024, stream);
 
