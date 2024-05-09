b003ed99e00cf17aaf8a019e4d6e7f7e93d77f59
Tim Moon <tmoon@nvidia.com>
2024-05-09 18:18:21 +0000

    Import frameworks lazily
    Signed-off-by: Tim Moon <tmoon@nvidia.com>

--- transformer_engine/__init__.py
+++ transformer_engine/__init__.py
@@ -3,15 +3,32 @@
 # See LICENSE for license information.
 
 """Top level package"""
+import importlib.util
+import sys
+from types import ModuleType
+
 from ._version import __version__
 from . import common
 
-try:
-    from . import pytorch
-except ImportError as e:
-    pass
+def _lazy_import(name: str) -> ModuleType:
+    """Construct a module that is imported the first time it is used"""
+    spec = importlib.util.find_spec(name)
+    loader = importlib.util.LazyLoader(spec.loader)
+    spec.loader = loader
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[name] = module
+    loader.exec_module(module)
+    return module
 
-try:
-    from . import jax
-except ImportError as e:
-    pass
+# Lazily import frameworks
+pytorch: ModuleType = _lazy_import("transformer_engine.pytorch")
+jax: ModuleType = _lazy_import("transformer_engine.jax")
+paddle: ModuleType = _lazy_import("transformer_engine.paddle")
+
+__all__ = [
+    "__version__",
+    "common",
+    "jax",
+    "paddle",
+    "pytorch",
+]
