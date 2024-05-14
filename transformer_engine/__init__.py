7df4623b13b0a159b3e0586cc4411357d7b1cc2c
Tim Moon <tmoon@nvidia.com>
2024-05-14 00:41:00 +0000

    Do not attempt importing submodules if framework is not available
    Signed-off-by: Tim Moon <tmoon@nvidia.com>

--- transformer_engine/__init__.py
+++ transformer_engine/__init__.py
@@ -20,26 +20,38 @@ def _lazy_import(name: str) -> ModuleType:
     loader.exec_module(module)
     return module
 
+# See which frameworks are available
+try:
+    import torch
+    _FOUND_PYTORCH = True
+except ImportError:
+    _FOUND_PYTORCH = False
+try:
+    import jax
+    _FOUND_JAX = True
+except ImportError:
+    _FOUND_JAX = False
+try:
+    import paddle
+    _FOUND_PADDLE = True
+except ImportError:
+    _FOUND_PADDLE = False
+
 # Import framework submodules
 # Note: Load module lazily if import fails. This way a useful import
 # error will be thrown if the user attempts to access the module.
-try:
-    from . import pytorch
-except ImportError:
-    pytorch = _lazy_import("transformer_engine.pytorch")
-try:
-    from . import jax
-except ImportError:
-    jax = _lazy_import("transformer_engine.jax")
-try:
-    from . import paddle
-except ImportError:
-    paddle = _lazy_import("transformer_engine.paddle")
-
-__all__ = [
-    "__version__",
-    "common",
-    "jax",
-    "paddle",
-    "pytorch",
-]
+if _FOUND_PYTORCH:
+    try:
+        from . import pytorch
+    except ImportError:
+        pytorch = _lazy_import("transformer_engine.pytorch")
+if _FOUND_JAX:
+    try:
+        from . import jax
+    except ImportError:
+        jax = _lazy_import("transformer_engine.jax")
+if _FOUND_PADDLE:
+    try:
+        from . import paddle
+    except ImportError:
+        paddle = _lazy_import("transformer_engine.paddle")
