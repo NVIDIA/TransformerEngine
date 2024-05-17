e55cd6ca72afaa5603d8e360bbf6153d95efb22c
Phuong Nguyen <phuonguyen@nvidia.com>
2024-05-16 17:14:38 -0700

    added unit test for gemm with unaligned inputs
    Signed-off-by: Phuong Nguyen <phuonguyen@nvidia.com>

--- tests/pytorch/test_sanity.py
+++ tests/pytorch/test_sanity.py
@@ -29,6 +29,10 @@ from transformer_engine.pytorch import (
     get_cpu_offload_context,
 )
 from transformer_engine.common import recipe
+import transformer_engine_extensions as tex
+from transformer_engine.pytorch.cpp_extensions import gemm, fp8_gemm, gelu, cast_to_fp8, cast_from_fp8
+from transformer_engine.pytorch.module.base import get_workspace
+from test_onnx_export import create_meta
 
 # Only run FP8 tests on H100.
 fp8_available, reason_for_no_fp8 = FP8GlobalStateManager.is_fp8_available()
@@ -924,3 +928,60 @@ def test_model_multiple_cast():
 
     y2 = m(a)
     assert y2.dtype == torch.float16
+
+
+@pytest.mark.parametrize("N", [32])
+@pytest.mark.parametrize("offset", [1, 3, 5])
+@pytest.mark.parametrize("datatype", param_types)
+def test_sanity_gemm_with_unalignment(N, offset, datatype):
+    scratchpad = torch.randn(N*N + 2*offset, device="cuda", dtype=datatype)
+    inp = torch.reshape(scratchpad[offset:-offset], (N, N))
+    weight = torch.reshape(scratchpad[offset*2:], (N, N))
+
+    _, _, _ = gemm(
+        A=weight,
+        B=inp,
+        dtype=datatype,
+        workspace=get_workspace())
+    torch.cuda.synchronize()
+
+
+@pytest.mark.parametrize("N", [32])
+@pytest.mark.parametrize("datatype", [torch.float16, torch.bfloat16])
+def test_sanity_fp8_gemm_with_unalignment(N, datatype):
+    offset = 4
+    scratchpad = torch.randn(N*N + 2*offset, device="cuda", dtype=datatype)
+
+    fp8_tensor_inp = tex.FP8FwdTensors.GEMM1_INPUT
+    fp8_tensor_weight = tex.FP8FwdTensors.GEMM1_WEIGHT
+
+    nb_inp_scales, nb_weight_scales = 1, N
+    scale_factor = 1.
+    meta_inp = create_meta(scale_factor, nb_inp_scales)
+    meta_weight = create_meta(scale_factor, nb_weight_scales)
+    inp_type = tex.DType.kFloat8E4M3
+    weights_type = tex.DType.kFloat8E4M3
+    outp_type = datatype
+
+    scratchpad_fp8 = cast_to_fp8(
+            scratchpad,
+            meta_weight,
+            fp8_tensor_inp,
+            inp_type)
+    inp_fp8 = torch.reshape(scratchpad_fp8[offset:-offset], (N, N))
+    weight_fp8 = torch.reshape(scratchpad_fp8[offset*2:], (N, N))
+    _, _ = fp8_gemm(
+            weight_fp8,
+            meta_weight.scale_inv,
+            fp8_tensor_weight,
+            inp_type,
+            inp_fp8,
+            meta_inp.scale_inv,
+            fp8_tensor_inp,
+            weights_type,
+            outp_type,
+            get_workspace(),
+            bias=None,
+            use_bias=False,
+            use_split_accumulator=False)
+    torch.cuda.synchronize()
