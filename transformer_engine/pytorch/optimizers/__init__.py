e8a91b9d0f747b5b14a497818401d82cd64fbcfb
Xin Yao <xiny@nvidia.com>
2024-06-11 00:13:02 -0700

    expose multi_tensor_* kernels
    Signed-off-by: Xin Yao <xiny@nvidia.com>

--- transformer_engine/pytorch/optimizers/__init__.py
+++ transformer_engine/pytorch/optimizers/__init__.py
@@ -6,3 +6,12 @@
 from .fused_adam import FusedAdam
 from .fused_sgd import FusedSGD
 from .multi_tensor_apply import MultiTensorApply, multi_tensor_applier
+from transformer_engine_torch import (
+    multi_tensor_scale,
+    multi_tensor_l2norm,
+    multi_tensor_unscale_l2norm,
+    multi_tensor_adam,
+    multi_tensor_adam_capturable,
+    multi_tensor_adam_capturable_master,
+    multi_tensor_sgd,
+)
