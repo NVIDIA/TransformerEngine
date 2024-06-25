f71be15a5b3da228783dd09a74b4d5c2dbbb00ca
Jiang Shao <jiangs@nvidia.com>
2024-06-25 19:28:47 +0000

    Move permutation.py out of module dir

--- transformer_engine/pytorch/__init__.py
+++ transformer_engine/pytorch/__init__.py
@@ -36,12 +36,12 @@ from transformer_engine.pytorch.module import LayerNormLinear
 from transformer_engine.pytorch.module import Linear
 from transformer_engine.pytorch.module import LayerNormMLP
 from transformer_engine.pytorch.module import LayerNorm
-from transformer_engine.pytorch.module import Permute, Unpermute
 from transformer_engine.pytorch.module import RMSNorm
 from transformer_engine.pytorch.module import GroupedLinear
 from transformer_engine.pytorch.attention import DotProductAttention
 from transformer_engine.pytorch.attention import InferenceParams
 from transformer_engine.pytorch.attention import MultiheadAttention
+from transformer_engine.pytorch.permutation import Permute, Unpermute
 from transformer_engine.pytorch.transformer import TransformerLayer
 from transformer_engine.pytorch.fp8 import fp8_autocast
 from transformer_engine.pytorch.fp8 import fp8_model_init
