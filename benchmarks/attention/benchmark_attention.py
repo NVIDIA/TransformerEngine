22f551b8968b72a4cbbf683a75b99f815aa05595
Charlene Yang <8636796+cyanguwa@users.noreply.github.com>
2024-07-16 15:42:07 -0700

    WIP: add benchmarks
    Signed-off-by: Charlene Yang <8636796+cyanguwa@users.noreply.github.com>

--- benchmarks/attention/benchmark_attention.py
+++ benchmarks/attention/benchmark_attention.py
@@ -9,18 +9,17 @@ import numpy as np
 import torch
 import nvtx
 import transformer_engine
+sys.path.append("../../")
 from tests.pytorch.fused_attn.test_fused_attn import (
     ModelConfig,
-    _is_flash_attention_supported,
-    _is_fused_attention_supported,
-    _is_unfused_attention_supported,
+    _get_attention_backends,
     _run_dot_product_attention,
 )
 
 pd.set_option("display.precision", 4)
 
 # data type
-dtype = torch.bfloat16
+dtype = torch.float16
 # number of iterations after 3 warmup iterations
 num_iters = 3
 # checkpointing
@@ -29,8 +28,6 @@ ckpt_attn = False
 workspace_opt = True
 # QKV memory layout
 qkv_layout = "bshd_bshd_bshd"
-# sliding window attention
-swa = False
 # padding between sequences for qkv_format=thd
 pad_between_seqs = False
 # training mode
@@ -42,6 +39,8 @@ model_configs = {
     "test_1": ModelConfig(2, 16, 16, 128, 2048, 2048, 0.0, "causal", "no_bias"),  # longer seq, mask
     "test_2": ModelConfig(2, 16, 16, 128, 2048, 2048, 0.0, "causal", "post_scale_bias"),  # bias
     "test_3": ModelConfig(2, 32, 4, 128, 8192, 8192, 0.0, "causal", "no_bias"),  # GQA
+    "test_4": ModelConfig(2, 32, 4, 128, 16384, 16384, 0.0, "causal", "no_bias"),  # GQA
+    "test_5": ModelConfig(2, 16, 16, 128, 16384, 16384, 0.0, "causal", "no_bias"),  # MHA
 }
 
 
@@ -64,7 +63,6 @@ def benchmark_dot_product_attention(model, fused_attn_supported, flash_attn_supp
                 ckpt_attn,
                 qkv_layout,
                 workspace_opt,
-                swa,
                 pad_between_seqs,
                 is_training,
             )
@@ -76,14 +74,13 @@ def benchmark_dot_product_attention(model, fused_attn_supported, flash_attn_supp
                 ckpt_attn,
                 qkv_layout,
                 workspace_opt,
-                swa,
                 pad_between_seqs,
                 is_training,
             )
-        if fused_attn_supported and flash_attn_supported:
-            torch.testing.assert_close(fused_attn_fwd, flash_attn_fwd, **tols)
-            for i, _ in enumerate(flash_attn_bwd):
-                torch.testing.assert_close(fused_attn_bwd[i], flash_attn_bwd[i], **tols)
+        #if fused_attn_supported and flash_attn_supported:
+        #    torch.testing.assert_close(fused_attn_fwd, flash_attn_fwd, **tols)
+        #    for i, _ in enumerate(flash_attn_bwd):
+        #        torch.testing.assert_close(fused_attn_bwd[i], flash_attn_bwd[i], **tols)
 
     torch.cuda.cudart().cudaProfilerStart()
     torch.cuda.synchronize()
@@ -97,7 +94,6 @@ def benchmark_dot_product_attention(model, fused_attn_supported, flash_attn_supp
                 ckpt_attn,
                 qkv_layout,
                 workspace_opt,
-                swa,
                 pad_between_seqs,
                 is_training,
             )
@@ -115,7 +111,6 @@ def benchmark_dot_product_attention(model, fused_attn_supported, flash_attn_supp
                 ckpt_attn,
                 qkv_layout,
                 workspace_opt,
-                swa,
                 pad_between_seqs,
                 is_training,
             )
@@ -205,13 +200,12 @@ def main():
     )
     for model in model_configs.keys():
         config = model_configs[model]
-        fused_attn_supported, fused_attn_backend = _is_fused_attention_supported(
+        available_backends, fused_attn_backends = _get_attention_backends(
             config,
-            dtype,
+            qkv_dtype=dtype,
             qkv_layout=qkv_layout,
         )
-        fused_attn_supported = fused_attn_supported and not swa
-        flash_attn_supported = _is_flash_attention_supported(config)
+        flash_attn_supported, fused_attn_supported, unfused_attn_supported = available_backends
         print(
             f'Running {model} with {"cuDNN attention" if fused_attn_supported else ""}'
             f'{" and flash-attention" if flash_attn_supported else ""}...'
