eecef4c1c45af6f698c76e9c1a669e6bdb359794
Kirthi Shankar Sivamani <ksivamani@nvidia.com>
2024-07-16 13:15:20 -0700

    Add aarch
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>

--- build_tools/wheel_utils/build_wheels.sh
+++ build_tools/wheel_utils/build_wheels.sh
@@ -2,6 +2,9 @@
 #
 # See LICENSE for license information.
 
+PLATFORM=${1:-manylinux_2_28_x86_64}
+MAKE_FW_SDIST=${2:-true}
+
 export NVTE_RELEASE_BUILD=1
 export TARGET_BRANCH=${TARGET_BRANCH:-wheels}
 
@@ -9,7 +12,7 @@ git config --global --add safe.directory /TransformerEngine
 cd /TransformerEngine
 git checkout $TARGET_BRANCH
 git submodule update --init --recursive && \
-/opt/python/cp38-cp38/bin/python setup.py bdist_wheel --verbose --python-tag=py3 --plat-name=manylinux_2_28_x86_64
+/opt/python/cp310-cp310/bin/python setup.py bdist_wheel --verbose --python-tag=py3 --plat-name=$PLATFORM
 
 # Set target name for build distribution.
 mkdir /wheelhouse
@@ -18,15 +21,18 @@ IFS='-' read -ra whl_parts <<< "$whl_name"
 whl_name_target="${whl_parts[0]}-${whl_parts[1]}-py3-none-${whl_parts[4]}"
 mv dist/"$whl_name" /wheelhouse/"$whl_name_target"
 
-# PyTorch sdist
-cd transformer_engine/pytorch
-/opt/python/cp38-cp38/bin/pip install torch
-/opt/python/cp38-cp38/bin/python setup.py sdist
-cp dist/* /wheelhouse/
 
-# JAX sdist
-cd ../jax
-/opt/python/cp38-cp38/bin/pip install jax jaxlib
-/opt/python/cp38-cp38/bin/python setup.py sdist
-cp dist/* /wheelhouse/
+if $MAKE_FW_SDIST ; then
+	# PyTorch sdist
+	cd transformer_engine/pytorch
+	/opt/python/cp310-cp310/bin/pip install torch
+	/opt/python/cp310-cp310/bin/python setup.py sdist
+	cp dist/* /wheelhouse/
+
+	# JAX sdist
+	cd ../jax
+	/opt/python/cp310-cp310/bin/pip install jax jaxlib
+	/opt/python/cp310-cp310/bin/python setup.py sdist
+	cp dist/* /wheelhouse/
+fi
 
--- build_tools/wheel_utils/launch.sh
+++ build_tools/wheel_utils/launch.sh
@@ -2,8 +2,13 @@
 #
 # See LICENSE for license information.
 
-docker build -t "wheel" -f build_tools/wheel_utils/Dockerfile .
-docker run --runtime=nvidia --gpus=all --ipc=host "wheel"
-rm -rf wheelhouse
-docker cp $(docker ps -aq | head -1):/wheelhouse .
+docker build -t "x86_wheel" -f build_tools/wheel_utils/Dockerfile.x86 .
+docker run --runtime=nvidia --gpus=all --ipc=host "x86_wheel"
+rm -rf x86_wheelhouse
+docker cp $(docker ps -aq | head -1):/wheelhouse x86_wheelhouse
+
+docker build -t "aarch_wheel" -f build_tools/wheel_utils/Dockerfile.aarch .
+docker run --runtime=nvidia --gpus=all --ipc=host "aarch_wheel"
+rm -rf aarch_wheelhouse
+docker cp $(docker ps -aq | head -1):/wheelhouse/ aarch_wheelhouse
 
