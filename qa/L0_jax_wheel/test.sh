48f3d48e7d0420d78c5298382f8faa146a8cbf4b
Kirthi Shankar Sivamani <ksivamani@nvidia.com>
2024-07-23 18:32:43 -0700

    Add sanity wheel test
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>

---
+++ qa/L0_jax_wheel/test.sh
@@ -0,0 +1,19 @@
+# Copyright (c) 2022-2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# See LICENSE for license information.
+
+set -e
+
+: "${TE_PATH:=/opt/transformerengine}"
+
+cd $TE_PATH
+pip uninstall -y transformer-engine
+export NVTE_RELEASE_BUILD=1
+python setup.py bdist_wheel
+cd transformer_engine/jax
+python setup.py sdist
+
+export NVTE_RELEASE_BUILD=0
+pip install dist/*
+cd $TE_PATH
+pip install dist/*
