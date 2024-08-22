a33537448c58a5474a246f57ea3ba00932ec472c
NVJiangShao <91270701+StudyingShao@users.noreply.github.com>
2024-08-23 00:43:59 +0800

    [MoE][Common/PyTorch] Add permutation (#936)
    * Add permutation functions
    
    * Add permutation ops
    
    * Remove the dependency on cutlass
    
    * Move permutation.py out of module dir
    
    * Rewrite the unit test and enable skipping if FP8 is unavailable
    
    * Rename exposed C++ API and reorder its parameters + take NVTETensor as inputs
    
    * Use Float8Tensor for FP8 input
    
    * Move dtype to ctx
    
    ---------
    
    Signed-off-by: Jiang Shao <jiangs@nvidia.com>
    Co-authored-by: Qi Zhang <qizhang@nvidia.com>
    Co-authored-by: Phuong Nguyen <36155692+phu0ngng@users.noreply.github.com>

---
+++ transformer_engine/common/include/transformer_engine/permutation.h
@@ -0,0 +1,21 @@
+/*************************************************************************
+ * Copyright (c) 2022-2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ *
+ * See LICENSE for license information.
+ ************************************************************************/
+
+#ifndef TRANSFORMER_ENGINE_PERMUTATION_H_
+#define TRANSFORMER_ENGINE_PERMUTATION_H_
+
+#include "transformer_engine.h"
+
+void nvte_permute(const NVTETensor input, NVTETensor output, const NVTETensor sorted_row_id,
+                  NVTETensor row_id_map, const NVTETensor prob, NVTETensor prob_grad,
+                  const NVTETensor input_fwd, const int num_rows, const int topK,
+                  const int num_cols, const int num_out_tokens, cudaStream_t stream = nullptr);
+
+void nvte_unpermute(const NVTETensor input, NVTETensor output, NVTETensor row_id_map,
+                    const NVTETensor prob, const int num_rows, const int topK, const int num_cols,
+                    cudaStream_t stream = nullptr);
+
+#endif  // TRANSFORMER_ENGINE_PERMUTATION_H_
