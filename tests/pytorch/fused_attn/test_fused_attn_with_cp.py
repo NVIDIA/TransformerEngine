a2ebe4785a50c52d64d28c4db6dbf332b9d3c372
Xiaowei Ren <xren@nvidia.com>
2024-08-30 20:19:34 -0700

    add more A2A unit test
    Signed-off-by: Xiaowei Ren <xren@nvidia.com>

--- tests/pytorch/fused_attn/test_fused_attn_with_cp.py
+++ tests/pytorch/fused_attn/test_fused_attn_with_cp.py
@@ -22,10 +22,16 @@ model_configs_flash_attn = {
     "cp_1_2": ModelConfig(
         2, 12, 12, 128, 4096, 4096, 0.0, "causal", "no_bias", window_size=(512, 0)
     ),  # MHA
+    "cp_1_3": ModelConfig(
+        2, 12, 12, 128, 4096, 4096, 0.0, "no_mask", "no_bias", window_size=(512, 0)
+    ),  # MHA
     "cp_2_0": ModelConfig(2, 12, 1, 128, 4096, 4096, 0.0, "causal", "no_bias"),  # GQA
     "cp_2_1": ModelConfig(2, 12, 1, 128, 4096, 4096, 0.0, "no_mask", "no_bias"),  # GQA
     "cp_2_2": ModelConfig(
         2, 12, 1, 128, 4096, 4096, 0.0, "causal", "no_bias", window_size=(512, 0)
+    ),  # GQA
+    "cp_2_3": ModelConfig(
+        2, 12, 1, 128, 4096, 4096, 0.0, "no_mask", "no_bias", window_size=(512, 0)
     ),  # GQA
 }
 
