215db88dcd294ef4ddbbb635cd51956a35fc1e4f
Ruibin Cheung <beinggod@foxmail.com>
2024-09-06 02:03:24 +0800

    [PyTorch] Implement Fp8 padding and unpadding module (#1129)
    * [TE/PyTorch][MoE] Add FP8 padding and unpadding module 
    
     1. Add multi-tensor padding kernel for FP8 with padding size = 16.
     2. Add FP8Padding and Fp8Unpadding module
     3. Add Padded GroupedLinear unit tests
    
    ---------
    
    Signed-off-by: beinggod <zhangruibin@01.ai>
    Co-authored-by: Phuong Nguyen <36155692+phu0ngng@users.noreply.github.com>

--- transformer_engine/pytorch/csrc/extensions/pybind.cpp
+++ transformer_engine/pytorch/csrc/extensions/pybind.cpp
@@ -152,7 +152,8 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("fused_amax_and_scale_update_after_reduction", &fused_amax_and_scale_update_after_reduction,
         "Update amax history and FP8 scale/scale_inv after reduction",
         py::call_guard<py::gil_scoped_release>());
-
+  m.def("fused_multi_row_padding", &fused_multi_row_padding, "Fused Multi-tensor padding",
+        py::call_guard<py::gil_scoped_release>());
   // fused apply rope
   m.def("fused_rope_forward", &fused_rope_forward, "Fused Apply RoPE FWD",
         py::call_guard<py::gil_scoped_release>());
