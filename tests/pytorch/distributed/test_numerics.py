d289c4e60f2560aa68fb4b1d65e17c2399fe4907
Pawel Gadzinski <pgadzinski@nvidia.com>
2024-09-21 10:40:25 -0700

    Tests for distributed
    Signed-off-by: Pawel Gadzinski <pgadzinski@nvidia.com>

---
+++ tests/pytorch/distributed/test_numerics.py
@@ -0,0 +1,85 @@
+# Copyright (c) 2022-2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# See LICENSE for license information.
+
+import os
+import subprocess
+from pathlib import Path
+
+import pytest
+import torch
+from transformer_engine.pytorch.fp8 import FP8GlobalStateManager
+
+"""
+    Distributed numerics tests
+
+    These tests test the numerical corectness of the TransformerEngine layers.
+    Tests are parametrized by the layer and fp8 precision.
+    One test consists of running multiple configurations from file run_numerics.py
+    Such design is due to the fact the initialization of one test is long
+    - 2 processes need to start and load torch and TE. Multiple configurations
+    are run in one test - this reduces the initialization overhead.
+
+"""
+
+
+
+if torch.cuda.device_count() < 2:
+    pytest.skip("Distributed training needs at least 2 GPUs.")
+
+fp8_available, reason_for_no_fp8 = FP8GlobalStateManager.is_fp8_available()
+
+TEST_ROOT = Path(__file__).parent.resolve()
+NUM_PROCS: int = min(torch.cuda.device_count(), 4)
+LAUNCH_CMD = ["torchrun", f"--nproc_per_node={NUM_PROCS}"]
+
+def _run_test(layer, fp8):
+    test_path = TEST_ROOT / "run_numerics.py"
+    test_cmd = LAUNCH_CMD + [
+        str(test_path),
+        f"--layer-type={layer}"
+    ]
+
+    if fp8:
+        test_cmd += ["--fp8=true"]
+
+    result = subprocess.run(test_cmd, env=os.environ, capture_output=True, check=False)
+    if (
+        result.returncode != 0
+        or "NUMERICAL CHECK FAILED" in result.stderr.decode()
+        or "NUMERICAL CHECK PASSED" not in result.stdout.decode()
+    ):
+        raise AssertionError(result.stderr.decode())
+
+
+all_boolean = [True, False]
+
+@pytest.mark.parametrize("fp8", all_boolean)
+def test_linear(fp8):
+    if fp8 and not fp8_available:
+        pytest.skip(reason_for_no_fp8)
+    _run_test("linear", fp8)
+
+@pytest.mark.parametrize("fp8", all_boolean)
+def test_layernorm(fp8):
+    if fp8 and not fp8_available:
+        pytest.skip(reason_for_no_fp8)
+    _run_test("layernorm", fp8)
+
+@pytest.mark.parametrize("fp8", all_boolean)
+def test_layernorm_linear(fp8):
+    if fp8 and not fp8_available:
+        pytest.skip(reason_for_no_fp8)
+    _run_test("layernorm_linear", fp8)
+
+@pytest.mark.parametrize("fp8", all_boolean)
+def test_layernorm_mlp(fp8):
+    if fp8 and not fp8_available:
+        pytest.skip(reason_for_no_fp8)
+    _run_test("layernorm_mlp", fp8)
+
+@pytest.mark.parametrize("fp8", all_boolean)
+def test_transformer_layer(fp8):
+    if fp8 and not fp8_available:
+        pytest.skip(reason_for_no_fp8)
+    _run_test("transformer_layer", fp8)
