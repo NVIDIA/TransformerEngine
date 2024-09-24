821d4590c54aadb2d6eb21ec49a27c124526f53f
Sangkug Lym <slym@nvidia.com>
2024-09-24 10:42:07 -0700

    Add default value
    Signed-off-by: Sangkug Lym <slym@nvidia.com>

--- build_tools/pytorch.py
+++ build_tools/pytorch.py
@@ -88,7 +88,7 @@ def setup_pytorch_extension(
     # Libraries
     library_dirs = []
     libraries = []
-    if bool(int(os.getenv("NVTE_UB_WITH_MPI"))):
+    if bool(int(os.getenv("NVTE_UB_WITH_MPI", 0))):
         assert (
             os.getenv("MPI_HOME") is not None
         ), "MPI_HOME must be set when compiling with NVTE_UB_WITH_MPI=1"
