985386fc4a35e4135d7f4002d59810a679cececf
Tim Moon <4406448+timmoon10@users.noreply.github.com>
2024-10-08 15:19:58 -0700

    Tweak comments
    Review suggestion from @ptrendx
    
    Signed-off-by: Tim Moon <4406448+timmoon10@users.noreply.github.com>

--- tests/pytorch/test_float8tensor.py
+++ tests/pytorch/test_float8tensor.py
@@ -368,7 +368,7 @@ class TestFloat8Tensor:
         assert x.dtype == y.dtype
         assert x.device == y.device
 
-        # Copy from plain tensor
+        # Set data to plain tensor
         x0 = torch.zeros((3, 2), dtype=torch.float16, device=x.device)
         x.data = x0
         assert isinstance(x, Float8Tensor)
@@ -381,7 +381,7 @@ class TestFloat8Tensor:
         assert x.dtype == y.dtype
         assert x.device == y.device
 
-        # Copy from Float8Tensor
+        # Set data to Float8Tensor
         x0 = Float8Tensor.to_float8(torch.zeros((4, 3, 1), dtype=torch.float32))
         x.data = x0
         assert isinstance(x, Float8Tensor)
