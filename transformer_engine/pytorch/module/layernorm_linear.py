1d0759efc597ee6572a4850f39c1a6bf2203e65c
Robin Zhang <robinz@nvidia.com>
2024-10-10 20:07:18 -0700

    Revert self.fp8
    Signed-off-by: Robin Zhang <robinz@nvidia.com>

--- transformer_engine/pytorch/module/layernorm_linear.py
+++ transformer_engine/pytorch/module/layernorm_linear.py
@@ -1217,7 +1217,7 @@ class LayerNormLinear(TransformerEngineBaseModule):
                 self.apply_bias and not self.gemm_bias_unfused_add,
                 self.eps,
                 is_first_microbatch,
-                False if self.fp8 is None else self.fp8,
+                self.fp8,
                 self.fp8_calibration,
                 self.fp8_meta,
                 self.fuse_wgrad_accumulation,
