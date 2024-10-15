35582bd025610b08522c320941961bcdeef87e6b
Tim Moon <tmoon@nvidia.com>
2024-10-15 14:27:15 -0700

    Upgrade to Pylint 3.3.1
    Signed-off-by: Tim Moon <tmoon@nvidia.com>

--- qa/L0_pytorch_lint/test.sh
+++ qa/L0_pytorch_lint/test.sh
@@ -6,19 +6,19 @@ set -e
 
 : "${TE_PATH:=/opt/transformerengine}"
 
-pip install cpplint==1.6.0 pylint==2.13.5
+pip install cpplint==1.6.0 pylint==3.3.1
 if [ -z "${PYTHON_ONLY}" ]
 then
   cd $TE_PATH
   echo "Checking common API headers"
-  cpplint --root transformer_engine/common/include --recursive transformer_engine/common/include
+  python -m cpplint --root transformer_engine/common/include --recursive transformer_engine/common/include
   echo "Checking C++ files"
-  cpplint --recursive --exclude=transformer_engine/common/include --exclude=transformer_engine/build_tools/build transformer_engine/common
-  cpplint --recursive transformer_engine/pytorch
+  python -m cpplint --recursive --exclude=transformer_engine/common/include --exclude=transformer_engine/build_tools/build transformer_engine/common
+  python -m cpplint --recursive transformer_engine/pytorch
 fi
 if [ -z "${CPP_ONLY}" ]
 then
   cd $TE_PATH
   echo "Checking Python files"
-  pylint --recursive=y transformer_engine/common transformer_engine/pytorch
+  python -m pylint --recursive=y transformer_engine/common transformer_engine/pytorch
 fi
