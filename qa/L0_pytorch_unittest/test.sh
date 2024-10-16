f6b766bdf28aea6fee17416bdf8ee4039963cf54
Tim Moon <4406448+timmoon10@users.noreply.github.com>
2024-10-15 17:34:15 -0700

    [PyTorch] Build custom ORT ops before running ONNX export tests (#1252)
    * Build custom ORT ops before running ONNX tests
    
    Signed-off-by: Tim Moon <tmoon@nvidia.com>
    
    * Remove ONNX from context parallelism tests
    
    Signed-off-by: Tim Moon <tmoon@nvidia.com>
    
    * Export ONNX ops that do compute in FP32
    
    Matches internal impl of TE kernels.
    
    Signed-off-by: Tim Moon <tmoon@nvidia.com>
    
    * Add build script for custom ORT ops
    
    Signed-off-by: Tim Moon <tmoon@nvidia.com>
    
    ---------
    
    Signed-off-by: Tim Moon <tmoon@nvidia.com>

--- qa/L0_pytorch_unittest/test.sh
+++ qa/L0_pytorch_unittest/test.sh
@@ -6,7 +6,7 @@ set -e
 
 : ${TE_PATH:=/opt/transformerengine}
 
-pip install pytest==8.2.1 onnxruntime==1.13.1
+pip install pytest==8.2.1
 pytest -v -s $TE_PATH/tests/pytorch/test_sanity.py
 pytest -v -s $TE_PATH/tests/pytorch/test_recipe.py
 pytest -v -s $TE_PATH/tests/pytorch/test_deferred_init.py
@@ -15,7 +15,6 @@ PYTORCH_JIT=0 NVTE_TORCH_COMPILE=0 NVTE_ALLOW_NONDETERMINISTIC_ALGO=0 pytest -v 
 pytest -v -s $TE_PATH/tests/pytorch/test_jit.py
 NVTE_TORCH_COMPILE=0 NVTE_DEBUG=1 NVTE_DEBUG_LEVEL=1 pytest -o log_cli=true --log-cli-level=INFO -v -s $TE_PATH/tests/pytorch/fused_attn/test_fused_attn.py
 pytest -v -s $TE_PATH/tests/pytorch/test_fused_rope.py
-NVTE_TORCH_COMPILE=0 pytest -v -s $TE_PATH/tests/pytorch/test_onnx_export.py
 pytest -v -s $TE_PATH/tests/pytorch/test_float8tensor.py
 pytest -v -s $TE_PATH/tests/pytorch/test_torch_save_load.py
 pytest -v -s $TE_PATH/tests/pytorch/test_gqa.py
@@ -24,3 +23,9 @@ pytest -v -s $TE_PATH/tests/pytorch/test_multi_tensor.py
 pytest -v -s $TE_PATH/tests/pytorch/test_fusible_ops.py
 pytest -v -s $TE_PATH/tests/pytorch/test_fusible_ops_distributed.py
 pytest -v -s $TE_PATH/tests/pytorch/test_permutation.py
+
+# Build custom ONNX extensions for ONNX export test
+pip install onnxruntime==1.19.2
+export CUSTOM_ORT_OPS_PATH=$TE_PATH/tests/pytorch/custom_ort_ops
+bash $CUSTOM_ORT_OPS_PATH/build.sh
+NVTE_TORCH_COMPILE=0 pytest -v -s $TE_PATH/tests/pytorch/test_onnx_export.py
