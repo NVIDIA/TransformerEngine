41fe1e505bd5de32c61980182f2916c3e2a3de00
Tim Moon <4406448+timmoon10@users.noreply.github.com>
2024-10-17 18:57:13 -0700

    [PyTorch] Reorganize L1 tests (#1255)
    * Reorganize PyTorch L1 tests
    
    Signed-off-by: Tim Moon <tmoon@nvidia.com>
    
    * Move ONNX tests to L1
    
    Signed-off-by: Tim Moon <tmoon@nvidia.com>
    
    * Move FA version test to L3
    
    Signed-off-by: Tim Moon <tmoon@nvidia.com>
    
    * Limit parallel build jobs in FA version test
    
    Signed-off-by: Tim Moon <tmoon@nvidia.com>
    
    ---------
    
    Signed-off-by: Tim Moon <tmoon@nvidia.com>

--- qa/L0_pytorch_unittest/test.sh
+++ qa/L0_pytorch_unittest/test.sh
@@ -21,11 +21,4 @@ pytest -v -s $TE_PATH/tests/pytorch/test_gqa.py
 pytest -v -s $TE_PATH/tests/pytorch/test_fused_optimizer.py
 pytest -v -s $TE_PATH/tests/pytorch/test_multi_tensor.py
 pytest -v -s $TE_PATH/tests/pytorch/test_fusible_ops.py
-pytest -v -s $TE_PATH/tests/pytorch/test_fusible_ops_distributed.py
 pytest -v -s $TE_PATH/tests/pytorch/test_permutation.py
-
-# Build custom ONNX extensions for ONNX export test
-pip install onnxruntime==1.19.2
-export CUSTOM_ORT_OPS_PATH=$TE_PATH/tests/pytorch/custom_ort_ops
-bash $CUSTOM_ORT_OPS_PATH/build.sh
-NVTE_TORCH_COMPILE=0 pytest -v -s $TE_PATH/tests/pytorch/test_onnx_export.py
