4c60400ebc8832edd1e8ccf433228b533cc4e68d
Charlene Yang <8636796+cyanguwa@users.noreply.github.com>
2024-10-18 21:40:01 -0700

    update FE for T3HD/TH3D
    Signed-off-by: Charlene Yang <8636796+cyanguwa@users.noreply.github.com>

--- tests/pytorch/fused_attn/test_fused_attn.py
+++ tests/pytorch/fused_attn/test_fused_attn.py
@@ -644,8 +644,6 @@ model_configs_layout_thd = {
 @pytest.mark.parametrize("qkv_layout", qkv_layouts_thd)
 def test_dpa_qkv_layout_thd(dtype, model_configs, model, qkv_layout):
     """Test DotProductAttention module with different QKV layouts"""
-    if qkv_layout in ["t3hd", "th3d"]:
-        pytest.skip("Skip t3hd and th3d before cuDNN frontend change")
     pad_between_seqs = True
     test_dot_product_attention(
         dtype, model_configs, model, False, True, qkv_layout, False, pad_between_seqs
