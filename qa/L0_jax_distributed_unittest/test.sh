eb08d672f48ab4d8e3344ecb5c406209de566029
Phuong Nguyen <phuonguyen@nvidia.com>
2024-11-13 17:21:38 -0800

    added L0 jax distributed tests with pax
    Signed-off-by: Phuong Nguyen <phuonguyen@nvidia.com>

---
+++ qa/L0_jax_distributed_unittest/test.sh
@@ -0,0 +1,19 @@
+# Copyright (c) 2022-2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# See LICENSE for license information.
+
+set -xe
+
+## 4 GPU Tests
+
+# DP 2 TP 2 
+bash test-pax.sh --data-parallel 2 --tensor-parallel 2 --enable-te --multiprocess
+
+# PP 2 FSDP 2
+bash test-pax.sh --fsdp 2 --pipeline-parallel 2 --enable-te --multiprocess
+
+# PP 2 FSDP 2 + FP 8
+ENABLE_FP8=1 bash test-pax.sh --fsdp 2 --pipeline-parallel 2 --enable-te --multiprocess
+
+# PP 2 FSDP 2 + FP 8 + single-process
+ENABLE_FP8=1 bash test-pax.sh --fsdp 2 --pipeline-parallel 2 --enable-te
