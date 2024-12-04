7f3efccd17a6af218871c1e83f46d74f45936234
Phuong Nguyen <phuonguyen@nvidia.com>
2024-12-04 11:41:41 -0800

    clean up
    Signed-off-by: Phuong Nguyen <phuonguyen@nvidia.com>

--- qa/L0_jax_distributed_unittest/test.sh
+++ qa/L0_jax_distributed_unittest/test.sh
@@ -6,8 +6,6 @@ set -xe
 
 pip install -r $TE_PATH/examples/jax/encoder/requirements.txt
 
-pytest -c $TE_PATH/tests/jax/pytest.ini -v $TE_PATH/examples/jax/mnist
-
 # Make encoder tests to have run-to-run deterministic to have the stable CI results
 export XLA_FLAGS="${XLA_FLAGS} --xla_gpu_deterministic_ops"
 pytest -c $TE_PATH/tests/jax/pytest.ini -v $TE_PATH/examples/jax/encoder --ignore=$TE_PATH/examples/jax/encoder/test_single_gpu_encoder.py
