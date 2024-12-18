84895feceb6c225f87c251783620bbf194a4ac27
Phuong Nguyen <phuonguyen@nvidia.com>
2024-12-18 05:50:15 -0800

    Move test distributed encoder to L0 distributed testsuit
    Signed-off-by: Phuong Nguyen <phuonguyen@nvidia.com>
    Co-authored-by: Reese Wang <rewang@nvidia.com>

---
+++ qa/L0_jax_distributed_unittest/test.sh
@@ -0,0 +1,16 @@
+# Copyright (c) 2022-2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# See LICENSE for license information.
+
+set -xe
+
+: ${TE_PATH:=/opt/transformerengine}
+
+pip install -r $TE_PATH/examples/jax/encoder/requirements.txt
+
+# Make encoder tests to have run-to-run deterministic to have the stable CI results
+export XLA_FLAGS="${XLA_FLAGS} --xla_gpu_deterministic_ops"
+pytest -c $TE_PATH/tests/jax/pytest.ini -v $TE_PATH/examples/jax/encoder/test_multigpu_encoder.py
+pytest -c $TE_PATH/tests/jax/pytest.ini -v $TE_PATH/examples/jax/encoder/test_model_parallel_encoder.py
+pytest -c $TE_PATH/tests/jax/pytest.ini -v $TE_PATH/examples/jax/encoder/test_multiprocessing_encoder.py
+
