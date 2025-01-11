a65ad37e622ad89837b15520b9f2b6c7232d3423
Phuong Nguyen <phuonguyen@nvidia.com>
2025-01-10 16:53:31 -0800

    [JAX] Test_multiprocessing_encoder with process spawn in bash (#1394)
    * add test_multiprocessing_encoder with processing spawning in bash
    
    ---------
    
    Signed-off-by: Phuong Nguyen <phuonguyen@nvidia.com>

--- examples/jax/encoder/common.py
+++ examples/jax/encoder/common.py
@@ -12,3 +12,10 @@ def is_bf16_supported():
     """Return if BF16 has hardware supported"""
     gpu_arch = get_device_compute_capability(0)
     return gpu_arch >= 80
+
+
+@lru_cache
+def is_fp8_supported():
+    """Return if FP8 has hardware supported"""
+    gpu_arch = get_device_compute_capability(0)
+    return gpu_arch >= 90
