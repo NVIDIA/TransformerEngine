474d8870301b2fd9e7bde35bc280155c5e88ce2b
Johnny <johnnynuca14@gmail.com>
2025-01-21 20:23:56 +0100

    initial support blackwell b100/200 & rtx50

--- transformer_engine/jax/fp4.py
+++ transformer_engine/jax/fp4.py
@@ -2,7 +2,7 @@
 #
 # See LICENSE for license information.
 """
-Helper module for fp8 meta management
+Helper module for fp4 meta management
 """
 from contextlib import contextmanager
 from enum import Enum
@@ -12,7 +12,7 @@ from typing import Dict, List, Optional, Tuple, Union
 import jax
 import jax.numpy as jnp
 from flax.core.frozen_dict import FrozenDict
-from flax.linen import fp8_ops
+from flax.linen import fp4_ops
 
 from transformer_engine.transformer_engine_jax import DType
 from transformer_engine.transformer_engine_jax import get_cublasLt_version
@@ -24,42 +24,36 @@ from transformer_engine.common.recipe import DelayedScaling, Format
 from transformer_engine.jax.sharding import global_shard_guard
 from transformer_engine.jax.sharding import MeshResource
 
-_is_fp8_available = None
-_reason_for_no_fp8 = ""
+_is_fp4_available = None
+_reason_for_no_fp4 = ""
 Collection = Union[Dict, FrozenDict]
 
-def _check_fp8_support(gpu_id) -> Tuple[bool, str]:
-    """Return if fp8 support is available"""
+def _check_fp4_support(gpu_id) -> Tuple[bool, str]:
+    """Return if fp4 support is available"""
     gpu_arch = get_device_compute_capability(gpu_id)
-    if gpu_arch >= 90:  # hopper and above
+    if gpu_arch >= 100:  # hopper and above
         return True, ""
-    if gpu_arch < 89:  # pre-ada
-        return False, "Device compute capability 8.9 or higher required for FP8 execution."
-    if get_cublasLt_version() < 120103:
-        return False, "CublasLt version 12.1.3.x or higher required for FP8 execution on Ada."
-    if get_cuda_version() < 12010:
-        return False, "Cuda version 12.1 or higher required for FP8 execution on Ada."
+    if gpu_arch < 100:  # pre-ada
+        return False, "Device compute capability 8.9 or lower not supported."
     return True, ""
 
-
-def is_fp8_available(gpu_id=None) -> Tuple[bool, str]:
-    """Return if fp8 support is available"""
+def is_fp4_available(gpu_id=None) -> Tuple[bool, str]:
+    """Return if fp4 support is available"""
     if gpu_id is not None:
-        return _check_fp8_support(gpu_id)
+        return _check_fp4_support(gpu_id)
 
-    global _is_fp8_available, _reason_for_no_fp8
-    if _is_fp8_available is None:
-        _is_fp8_available = True
+    global _is_fp4_available, _reason_for_no_fp4
+    if _is_fp4_available is None:
+        _is_fp4_available = True
         # JAX doesn't provide the local GPU id.
         for local_gpu_id in range(len(jax.local_devices())):
-            ret, msg = _check_fp8_support(local_gpu_id)
+            ret, msg = _check_fp4_support(local_gpu_id)
             if ret is False:
-                _is_fp8_available = ret
-                _reason_for_no_fp8 = msg
+                _is_fp4_available = ret
+                _reason_for_no_fp4 = msg
             break
 
-    return _is_fp8_available, _reason_for_no_fp8
-
+    return _is_fp4_available, _reason_for_no_fp4
 
 def _format2dtypes(format_: Format):
     if format_ == Format.E4M3:
@@ -75,14 +69,14 @@ def _format2dtypes(format_: Format):
 # This is typically used in Pipeline Parallelism + "MiconBatching > 1",
 # which is implemented via nn.scan. Without this custom dtype, nn.scan
 # would sum gradients from all micro-batches, and this is not the expected
-# behavior for FP8 meta. Instead, the summation of FP8 meta gradients should
+# behavior for fp4 meta. Instead, the summation of fp4 meta gradients should
 # be "MAX".
-FlaxFloatMeta32 = fp8_ops.fm32
+FlaxFloatMeta32 = fp4_ops.fm32
 
 
-class FP8MetaPackage:
+class fp4MetaPackage:
     """
-    A container that contains all required meta data for FP8
+    A container that contains all required meta data for fp4
     """
 
     NUM_OF_META: int = 3
@@ -100,15 +94,15 @@ class FP8MetaPackage:
         grad_scale: jnp.ndarray,
     ) -> None:
 
-        self._amax_list = [None] * FP8MetaPackage.NUM_OF_META
-        self._scale_list = [None] * FP8MetaPackage.NUM_OF_META
+        self._amax_list = [None] * fp4MetaPackage.NUM_OF_META
+        self._scale_list = [None] * fp4MetaPackage.NUM_OF_META
 
-        self._amax_list[FP8MetaPackage.INPUT_IDX] = input_amax
-        self._scale_list[FP8MetaPackage.INPUT_IDX] = input_scale
-        self._amax_list[FP8MetaPackage.WEIGHT_IDX] = weight_amax
-        self._scale_list[FP8MetaPackage.WEIGHT_IDX] = weight_scale
-        self._amax_list[FP8MetaPackage.GRAD_IDX] = grad_amax
-        self._scale_list[FP8MetaPackage.GRAD_IDX] = grad_scale
+        self._amax_list[fp4MetaPackage.INPUT_IDX] = input_amax
+        self._scale_list[fp4MetaPackage.INPUT_IDX] = input_scale
+        self._amax_list[fp4MetaPackage.WEIGHT_IDX] = weight_amax
+        self._scale_list[fp4MetaPackage.WEIGHT_IDX] = weight_scale
+        self._amax_list[fp4MetaPackage.GRAD_IDX] = grad_amax
+        self._scale_list[fp4MetaPackage.GRAD_IDX] = grad_scale
 
     @property
     def amax_list(self) -> List[jnp.ndarray]:
@@ -129,20 +123,20 @@ class FP8MetaPackage:
         """
         Update the amax history list
         """
-        updated_amax_list = [FP8Helper.update_amax_history(amax) for amax in amax_list]
+        updated_amax_list = [fp4Helper.update_amax_history(amax) for amax in amax_list]
         return updated_amax_list
 
     @staticmethod
-    def update_fp8_scale(
-        amax_list: List[jnp.ndarray], scale_list: List[jnp.ndarray], fp8_dtype_list: List[DType]
+    def update_fp4_scale(
+        amax_list: List[jnp.ndarray], scale_list: List[jnp.ndarray], fp4_dtype_list: List[DType]
     ) -> Tuple[List[jnp.ndarray], List[jnp.ndarray]]:
         """
         Get update scale and scale_inv list
         """
         update_scale_list = []
         update_scale_inv_list = []
-        for amax, scale, fp8_dtype in zip(amax_list, scale_list, fp8_dtype_list):
-            upadted_scale, updated_scale_inv = FP8Helper.update_fp8_scale(amax, scale, fp8_dtype)
+        for amax, scale, fp4_dtype in zip(amax_list, scale_list, fp4_dtype_list):
+            upadted_scale, updated_scale_inv = fp4Helper.update_fp4_scale(amax, scale, fp4_dtype)
             update_scale_list.append(upadted_scale)
             update_scale_inv_list.append(updated_scale_inv)
         return update_scale_list, update_scale_inv_list
@@ -155,66 +149,66 @@ class AmaxComputeAlgo(Enum):
     MOST_RECENT = "most_recent"
 
 
-NVTE_FP8_COLLECTION_NAME = "fp8_metas"
+NVTE_fp4_COLLECTION_NAME = "fp4_metas"
 
 
-class FP8Helper:
+class fp4Helper:
     """
-    FP8 helper to manage the FP8 meta
+    fp4 helper to manage the fp4 meta
     """
 
     INITIALIZED = False
     MARGIN: float = 0.0
-    FP8_FORMAT: Format = Format.HYBRID
+    fp4_FORMAT: Format = Format.HYBRID
     FWD_DTYPE: DType = _format2dtypes(Format.HYBRID)[0]
     BWD_DTYPE: DType = _format2dtypes(Format.HYBRID)[1]
     AMAX_HISTORY_LEN: int = 1024
     AMAX_COMPUTE_ALGO: AmaxComputeAlgo = AmaxComputeAlgo.MAX
-    FP8_COLLECTION_NAME: str = NVTE_FP8_COLLECTION_NAME
-    FP8_AMAX_NAME: str = "amax"
-    FP8_SCALE_NAME: str = "scale"
-    FP8_2X_ACC_FPROP: bool = False
-    FP8_2X_ACC_DGRAD: bool = True
-    FP8_2X_ACC_WGRAD: bool = True
+    fp4_COLLECTION_NAME: str = NVTE_fp4_COLLECTION_NAME
+    fp4_AMAX_NAME: str = "amax"
+    fp4_SCALE_NAME: str = "scale"
+    fp4_2X_ACC_FPROP: bool = False
+    fp4_2X_ACC_DGRAD: bool = True
+    fp4_2X_ACC_WGRAD: bool = True
 
     @staticmethod
-    def is_fp8_enabled():
+    def is_fp4_enabled():
         """
-        Indicate if fp8 training is enable or not.
+        Indicate if fp4 training is enable or not.
         """
-        return FP8Helper.INITIALIZED
+        return fp4Helper.INITIALIZED
 
     @staticmethod
     def initialize(
         margin: float = 0.0,
-        fp8_format: Format = Format.HYBRID,
+        fp4_format: Format = Format.HYBRID,
         amax_history_len: int = 1,
         amax_compute_algo: AmaxComputeAlgo = AmaxComputeAlgo.MAX,
     ) -> None:
         """
-        Initialize the FP8 meta
+        Initialize the fp4 meta
         """
-        FP8Helper.INITIALIZED = True
-        FP8Helper.MARGIN = margin
-        FP8Helper.FP8_FORMAT = fp8_format
-        FP8Helper.FWD_DTYPE, FP8Helper.BWD_DTYPE = _format2dtypes(FP8Helper.FP8_FORMAT)
-        FP8Helper.AMAX_HISTORY_LEN = amax_history_len
-        FP8Helper.AMAX_COMPUTE_ALGO = amax_compute_algo
-        FP8Helper.FP8_2X_ACC_FPROP = False
-        FP8Helper.FP8_2X_ACC_DGRAD = True
-        FP8Helper.FP8_2X_ACC_WGRAD = True
+        fp4Helper.INITIALIZED = True
+        fp4Helper.MARGIN = margin
+        fp4Helper.fp4_FORMAT = fp4_format
+        fp4Helper.FWD_DTYPE, fp4Helper.BWD_DTYPE = _format2dtypes(fp4Helper.fp4_FORMAT)
+        fp4Helper.AMAX_HISTORY_LEN = amax_history_len
+        fp4Helper.AMAX_COMPUTE_ALGO = amax_compute_algo
+        fp4Helper.fp4_2X_ACC_FPROP = False
+        fp4Helper.fp4_2X_ACC_DGRAD = True
+        fp4Helper.fp4_2X_ACC_WGRAD = True
 
     @staticmethod
     def finalize() -> None:
         """
-        FP8 helper finalize
+        fp4 helper finalize
         """
-        FP8Helper.INITIALIZED = False
-        FP8Helper.MARGIN = 0.0
-        FP8Helper.FP8_FORMAT = Format.HYBRID
-        FP8Helper.FWD_DTYPE, FP8Helper.BWD_DTYPE = _format2dtypes(FP8Helper.FP8_FORMAT)
-        FP8Helper.AMAX_HISTORY_LEN = 1024
-        FP8Helper.AMAX_COMPUTE_ALGO = AmaxComputeAlgo.MAX
+        fp4Helper.INITIALIZED = False
+        fp4Helper.MARGIN = 0.0
+        fp4Helper.fp4_FORMAT = Format.HYBRID
+        fp4Helper.FWD_DTYPE, fp4Helper.BWD_DTYPE = _format2dtypes(fp4Helper.fp4_FORMAT)
+        fp4Helper.AMAX_HISTORY_LEN = 1024
+        fp4Helper.AMAX_COMPUTE_ALGO = AmaxComputeAlgo.MAX
 
     @staticmethod
     def update_collections(new: Collection, original: Collection) -> Collection:
@@ -233,7 +227,7 @@ class FP8Helper:
         return new_coll
 
     @staticmethod
-    def generate_fp8_meta_dtype_converter_pair(*args):
+    def generate_fp4_meta_dtype_converter_pair(*args):
         """
         Generate a pair of conversion fun in-between fm32 and fp32.
         """
@@ -280,18 +274,18 @@ class FP8Helper:
 
     @staticmethod
     @partial(jax.jit, static_argnums=(2,))
-    def update_fp8_scale(amax: jnp.ndarray, scale: jnp.ndarray, fp8_dtype: DType) -> jnp.ndarray:
+    def update_fp4_scale(amax: jnp.ndarray, scale: jnp.ndarray, fp4_dtype: DType) -> jnp.ndarray:
         """
-        Calculate fp8 scale and scale_inv based on given amax.
+        Calculate fp4 scale and scale_inv based on given amax.
         """
-        fp8_max = jnp.astype(jnp.finfo(fp8_dtype).max, jnp.float32)
+        fp4_max = jnp.astype(jnp.finfo(fp4_dtype).max, jnp.float32)
 
-        if FP8Helper.AMAX_COMPUTE_ALGO is AmaxComputeAlgo.MAX:
+        if fp4Helper.AMAX_COMPUTE_ALGO is AmaxComputeAlgo.MAX:
             amax = jnp.max(amax, axis=-1, keepdims=True)
         else:
             amax = amax[0:1]
 
-        sf = (fp8_max / amax) / (2**FP8Helper.MARGIN)
+        sf = (fp4_max / amax) / (2**fp4Helper.MARGIN)
         sf = jnp.where(amax > 0.0, sf, scale)
         sf = jnp.where(jnp.isfinite(amax), sf, scale)
         scale = sf
@@ -301,13 +295,13 @@ class FP8Helper:
 
 
 @contextmanager
-def fp8_autocast(
+def fp4_autocast(
     enabled: bool = False,
-    fp8_recipe: Optional[DelayedScaling] = None,
+    fp4_recipe: Optional[DelayedScaling] = None,
     mesh_resource: Optional[MeshResource] = None,
 ) -> None:
     r"""
-    Context manager for FP8 usage.
+    Context manager for fp4 usage.
 
     .. code-block:: python
 
@@ -319,7 +313,7 @@ def fp8_autocast(
         with maps.Mesh(devices, (dp_mesh_axis_name, tp_mesh_axis_name)):
             mesh_resource=MeshResource(dp_mesh_axis_name, tp_mesh_axis_name)
 
-            with fp8_autocast(enabled=True, mesh_resource=mesh_resource):
+            with fp4_autocast(enabled=True, mesh_resource=mesh_resource):
                 rules = extend_logical_axis_rules(tuple())
                 transformer = TransformerLayer()
 
@@ -327,7 +321,7 @@ def fp8_autocast(
                     pjit(transformer.init, ...)(...)
 
     .. note::
-        We only support :attr:`margin`, :attr:`fp8_format`, :attr:`amax_history_len`,
+        We only support :attr:`margin`, :attr:`fp4_format`, :attr:`amax_history_len`,
         and :attr:`amax_compute_algo` (with value 'max' and 'most_recent') in
         recipe.DelayedScaling currently. Other parameters in recipe.DelayedScaling
         will trigger an assertion.
@@ -335,30 +329,30 @@ def fp8_autocast(
     Parameters
     ----------
     enabled: bool, default = False
-        Whether or not to enable fp8
-    fp8_recipe: recipe.DelayedScaling, default = None
-        Recipe used for FP8 training.
+        Whether or not to enable fp4
+    fp4_recipe: recipe.DelayedScaling, default = None
+        Recipe used for fp4 training.
     mesh_resource: MeshResource, default = None
         Specify the mesh axes for data and tensor parallelism to shard along.
         If set to None, then no data or tensor parallelism will be used.
 
     """
-    if fp8_recipe is None:
-        fp8_recipe = DelayedScaling()
+    if fp4_recipe is None:
+        fp4_recipe = DelayedScaling()
 
-    assert fp8_recipe.amax_compute_algo in [
+    assert fp4_recipe.amax_compute_algo in [
         "max",
         "most_recent",
     ], "DelayedScaling amax_compute_algo only supports max and most_recent with TE/JAX."
     assert (
-        fp8_recipe.scaling_factor_compute_algo is None
+        fp4_recipe.scaling_factor_compute_algo is None
     ), "DelayedScaling scaling_factor_compute_algo isn't supported by TE/JAX."
-    assert fp8_recipe.override_linear_precision == (
+    assert fp4_recipe.override_linear_precision == (
         False,
         False,
         False,
     ), "DelayedScaling override_linear_precision isn't supported by TE/JAX."
-    assert fp8_recipe.reduce_amax, "DelayedScaling reduce_amax should be enabled for TE/JAX."
+    assert fp4_recipe.reduce_amax, "DelayedScaling reduce_amax should be enabled for TE/JAX."
 
     if mesh_resource is None:
         mesh_resource = MeshResource()
@@ -366,22 +360,22 @@ def fp8_autocast(
     try:
         with global_shard_guard(mesh_resource):
             if enabled:
-                fp8_available, reason_for_no_fp8 = is_fp8_available()
-                assert fp8_available, reason_for_no_fp8
+                fp4_available, reason_for_no_fp4 = is_fp4_available()
+                assert fp4_available, reason_for_no_fp4
 
                 amax_compute_algo = AmaxComputeAlgo.MOST_RECENT
-                if fp8_recipe.amax_compute_algo == "max":
+                if fp4_recipe.amax_compute_algo == "max":
                     amax_compute_algo = AmaxComputeAlgo.MAX
 
-                FP8Helper.initialize(
-                    margin=fp8_recipe.margin,
-                    fp8_format=fp8_recipe.fp8_format,
-                    amax_history_len=fp8_recipe.amax_history_len,
+                fp4Helper.initialize(
+                    margin=fp4_recipe.margin,
+                    fp4_format=fp4_recipe.fp4_format,
+                    amax_history_len=fp4_recipe.amax_history_len,
                     amax_compute_algo=amax_compute_algo,
                 )
             yield
     finally:
-        FP8Helper.finalize()
+        fp4Helper.finalize()
 
 
 # Function Wrappers
@@ -403,29 +397,29 @@ def update_collections(new: Collection, original: Collection) -> FrozenDict:
     outputs : Collection
         The updated collection.
     """
-    return FP8Helper.update_collections(new, original)
+    return fp4Helper.update_collections(new, original)
 
 
 def get_delayed_scaling():
     r"""
-    Obtain an instance of  DelayedScaling which is set via fp8_autocast.
+    Obtain an instance of  DelayedScaling which is set via fp4_autocast.
 
     .. note::
-        We only store :attr:`margin`, :attr:`fp8_format`, :attr:`amax_history_len`
-        , and :attr:`amax_compute_algo` via fp8_autocast. Other parameters in
+        We only store :attr:`margin`, :attr:`fp4_format`, :attr:`amax_history_len`
+        , and :attr:`amax_compute_algo` via fp4_autocast. Other parameters in
         recipe.DelayedScaling would be returned as the default values.
 
     Returns
     -------
     delay_scaling : DelayedScaling
-        an instance of  DelayedScaling which is set via fp8_autocast.
+        an instance of  DelayedScaling which is set via fp4_autocast.
     """
     amax_compute_algo = (
-        "max" if FP8Helper.AMAX_COMPUTE_ALGO is AmaxComputeAlgo.MAX else "most_recent"
+        "max" if fp4Helper.AMAX_COMPUTE_ALGO is AmaxComputeAlgo.MAX else "most_recent"
     )
     return DelayedScaling(
-        margin=int(FP8Helper.MARGIN),
-        fp8_format=FP8Helper.FP8_FORMAT,
-        amax_history_len=FP8Helper.AMAX_HISTORY_LEN,
+        margin=int(fp4Helper.MARGIN),
+        fp4_format=fp4Helper.fp4_FORMAT,
+        amax_history_len=fp4Helper.AMAX_HISTORY_LEN,
         amax_compute_algo=amax_compute_algo,
     )
