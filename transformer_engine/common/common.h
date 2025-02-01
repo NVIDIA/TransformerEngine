0887022ba30f3a008ee1b14491f37d06a23bfcc2
Oleg Goncharov <64355998+Oleg-Goncharov@users.noreply.github.com>
2025-02-01 01:13:32 +0100

    [common] Generalized MXFP8 fused kernels w.r.t. input tensor dimensions (#1437)
    * Generalized MXFP8 fused kernels w.r.t. input tensor dimensions
    
    Signed-off-by: Oleg Goncharov <ogoncharov@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update transformer_engine/common/common.cu
    
    Co-authored-by: Tim Moon <4406448+timmoon10@users.noreply.github.com>
    Signed-off-by: Oleg Goncharov <64355998+Oleg-Goncharov@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Removed unnecessary test scenarios
    
    Signed-off-by: Oleg Goncharov <64355998+Oleg-Goncharov@users.noreply.github.com>
    
    * Reverted the previous commit as it generated a compilation error (caused by to string conversion)
    
    Signed-off-by: Oleg Goncharov <64355998+Oleg-Goncharov@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update transformer_engine/common/common.cu
    
    Co-authored-by: Tim Moon <4406448+timmoon10@users.noreply.github.com>
    Signed-off-by: Oleg Goncharov <64355998+Oleg-Goncharov@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update test_cast_mxfp8.cu
    
    Signed-off-by: Oleg Goncharov <64355998+Oleg-Goncharov@users.noreply.github.com>
    
    * Fixed the bug with partial dbias writes in trimmed chunks
    
    Signed-off-by: Oleg Goncharov <ogoncharov@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Generalized MXFP8 dequantize kernel
    
    Signed-off-by: Oleg Goncharov <ogoncharov@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Signed-off-by: Oleg Goncharov <ogoncharov@nvidia.com>
    Signed-off-by: Oleg Goncharov <64355998+Oleg-Goncharov@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Tim Moon <4406448+timmoon10@users.noreply.github.com>

--- transformer_engine/common/common.h
+++ transformer_engine/common/common.h
@@ -420,6 +420,12 @@ struct is_fp8<fp8e4m3> : std::true_type {};
 template <>
 struct is_fp8<fp8e5m2> : std::true_type {};
 
+// [128,4] rowwise and [4,128] colwise alignment requirements for the tensor with scaling factors
+constexpr size_t scale_tensor_alignment_X_rowwise = 4;
+constexpr size_t scale_tensor_alignment_Y_rowwise = 128;
+constexpr size_t scale_tensor_alignment_X_colwise = 128;
+constexpr size_t scale_tensor_alignment_Y_colwise = 4;
+
 size_t typeToSize(const DType type);
 
 void CheckNoopTensor(const Tensor &t, const std::string &name);
