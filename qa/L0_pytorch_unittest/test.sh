529eeb6aa98e051ad501636041c32df63aae5d7a
Selvaraj Anandaraj <selvaraja@cw-dfw-cs-001-login-01.cm.cluster>
2025-02-19 00:17:39 -0800

    Added to test list
    Signed-off-by: Selvaraj Anandaraj <selvaraja@cw-dfw-cs-001-login-01.cm.cluster>

--- qa/L0_pytorch_unittest/test.sh
+++ qa/L0_pytorch_unittest/test.sh
@@ -20,4 +20,5 @@ pytest -v -s $TE_PATH/tests/pytorch/test_fused_optimizer.py
 pytest -v -s $TE_PATH/tests/pytorch/test_multi_tensor.py
 pytest -v -s $TE_PATH/tests/pytorch/test_fusible_ops.py
 pytest -v -s $TE_PATH/tests/pytorch/test_permutation.py
+pytest -v -s $TE_PATH/tests/pytorch/test_parallel_cross_entropy.py
 NVTE_TORCH_COMPILE=0 NVTE_DEBUG=1 NVTE_DEBUG_LEVEL=1 pytest -o log_cli=true --log-cli-level=INFO -v -s $TE_PATH/tests/pytorch/fused_attn/test_fused_attn.py
