f0d7ba39874d7099d4000837ae803aeffe25030a
Pawel Gadzinski <pgadzinski@nvidia.com>
2025-02-21 18:15:35 +0000

    fix
    Signed-off-by: Pawel Gadzinski <pgadzinski@nvidia.com>

--- qa/L1_pytorch_distributed_unittest/test.sh
+++ qa/L1_pytorch_distributed_unittest/test.sh
@@ -8,21 +8,13 @@ set -e
 
 pip install pytest==8.2.1
 
-error_occurred=0
+FAIL=0  
 
-run_test() {
-    "$@"
-    local status=$?
-    if [ $status -ne 0 ]; then
-        error_occurred=1
-    fi
-}
-
-run_test pytest -v -s $TE_PATH/tests/pytorch/distributed/test_numerics.py
-run_test pytest -v -s $TE_PATH/tests/pytorch/distributed/test_fusible_ops.py
-run_test pytest -v -s $TE_PATH/tests/pytorch/distributed/test_torch_fsdp2.py
-run_test pytest -v -s $TE_PATH/tests/pytorch/distributed/test_comm_gemm_overlap.py
+pytest -v -s $TE_PATH/tests/pytorch/distributed/test_numerics.py || FAIL=1
+pytest -v -s $TE_PATH/tests/pytorch/distributed/test_fusible_ops.py || FAIL=1
+pytest -v -s $TE_PATH/tests/pytorch/distributed/test_torch_fsdp2.py || FAIL=1
+pytest -v -s $TE_PATH/tests/pytorch/distributed/test_comm_gemm_overlap.py || FAIL=1
 # pytest -v -s $TE_PATH/tests/pytorch/distributed/test_fusible_ops_with_userbuffers.py  ### TODO Debug UB support with te.Sequential
-run_test pytest -v -s $TE_PATH/tests/pytorch/fused_attn/test_fused_attn_with_cp.py
+pytest -v -s $TE_PATH/tests/pytorch/fused_attn/test_fused_attn_with_cp.py || FAIL=1
 
-exit $error_occurred
+exit $FAIL
