d805d351e316580ef9f7b9916dc539e2881f2c48
Pawel Gadzinski <pgadzinski@nvidia.com>
2025-02-21 17:51:15 +0000

    non-exit tests
    Signed-off-by: Pawel Gadzinski <pgadzinski@nvidia.com>

--- qa/L0_pytorch_unittest/test.sh
+++ qa/L0_pytorch_unittest/test.sh
@@ -7,17 +7,30 @@ set -e
 : ${TE_PATH:=/opt/transformerengine}
 
 pip install pytest==8.2.1
-pytest -v -s $TE_PATH/tests/pytorch/test_sanity.py
-pytest -v -s $TE_PATH/tests/pytorch/test_recipe.py
-pytest -v -s $TE_PATH/tests/pytorch/test_deferred_init.py
-PYTORCH_JIT=0 NVTE_TORCH_COMPILE=0 NVTE_ALLOW_NONDETERMINISTIC_ALGO=0 pytest -v -s $TE_PATH/tests/pytorch/test_numerics.py
-NVTE_CUDNN_MXFP8_NORM=0 PYTORCH_JIT=0 NVTE_TORCH_COMPILE=0 NVTE_ALLOW_NONDETERMINISTIC_ALGO=0 pytest -v -s $TE_PATH/tests/pytorch/test_cuda_graphs.py
-pytest -v -s $TE_PATH/tests/pytorch/test_jit.py
-pytest -v -s $TE_PATH/tests/pytorch/test_fused_rope.py
-pytest -v -s $TE_PATH/tests/pytorch/test_float8tensor.py
-pytest -v -s $TE_PATH/tests/pytorch/test_gqa.py
-pytest -v -s $TE_PATH/tests/pytorch/test_fused_optimizer.py
-pytest -v -s $TE_PATH/tests/pytorch/test_multi_tensor.py
-pytest -v -s $TE_PATH/tests/pytorch/test_fusible_ops.py
-pytest -v -s $TE_PATH/tests/pytorch/test_permutation.py
-NVTE_TORCH_COMPILE=0 NVTE_DEBUG=1 NVTE_DEBUG_LEVEL=1 pytest -o log_cli=true --log-cli-level=INFO -v -s $TE_PATH/tests/pytorch/fused_attn/test_fused_attn.py
+
+error_occurred=0
+
+run_test() {
+    "$@"
+    local status=$?
+    if [ $status -ne 0 ]; then
+        error_occurred=1
+    fi
+}
+
+run_test pytest -v -s $TE_PATH/tests/pytorch/test_sanity.py
+run_test pytest -v -s $TE_PATH/tests/pytorch/test_recipe.py
+run_test pytest -v -s $TE_PATH/tests/pytorch/test_deferred_init.py
+run_test env PYTORCH_JIT=0 NVTE_TORCH_COMPILE=0 NVTE_ALLOW_NONDETERMINISTIC_ALGO=0 pytest -v -s $TE_PATH/tests/pytorch/test_numerics.py
+run_test env NVTE_CUDNN_MXFP8_NORM=0 PYTORCH_JIT=0 NVTE_TORCH_COMPILE=0 NVTE_ALLOW_NONDETERMINISTIC_ALGO=0 pytest -v -s $TE_PATH/tests/pytorch/test_cuda_graphs.py
+run_test pytest -v -s $TE_PATH/tests/pytorch/test_jit.py
+run_test pytest -v -s $TE_PATH/tests/pytorch/test_fused_rope.py
+run_test pytest -v -s $TE_PATH/tests/pytorch/test_float8tensor.py
+run_test pytest -v -s $TE_PATH/tests/pytorch/test_gqa.py
+run_test pytest -v -s $TE_PATH/tests/pytorch/test_fused_optimizer.py
+run_test pytest -v -s $TE_PATH/tests/pytorch/test_multi_tensor.py
+run_test pytest -v -s $TE_PATH/tests/pytorch/test_fusible_ops.py
+run_test pytest -v -s $TE_PATH/tests/pytorch/test_permutation.py
+run_test env NVTE_TORCH_COMPILE=0 NVTE_DEBUG=1 NVTE_DEBUG_LEVEL=1 pytest -o log_cli=true --log-cli-level=INFO -v -s $TE_PATH/tests/pytorch/fused_attn/test_fused_attn.py
+
+exit $error_occurred
--- qa/L1_pytorch_distributed_unittest/test.sh
+++ qa/L1_pytorch_distributed_unittest/test.sh
@@ -7,9 +7,22 @@ set -e
 : ${TE_PATH:=/opt/transformerengine}
 
 pip install pytest==8.2.1
-pytest -v -s $TE_PATH/tests/pytorch/distributed/test_numerics.py
-pytest -v -s $TE_PATH/tests/pytorch/distributed/test_fusible_ops.py
-pytest -v -s $TE_PATH/tests/pytorch/distributed/test_torch_fsdp2.py
-pytest -v -s $TE_PATH/tests/pytorch/distributed/test_comm_gemm_overlap.py
+
+error_occurred=0
+
+run_test() {
+    "$@"
+    local status=$?
+    if [ $status -ne 0 ]; then
+        error_occurred=1
+    fi
+}
+
+run_test pytest -v -s $TE_PATH/tests/pytorch/distributed/test_numerics.py
+run_test pytest -v -s $TE_PATH/tests/pytorch/distributed/test_fusible_ops.py
+run_test pytest -v -s $TE_PATH/tests/pytorch/distributed/test_torch_fsdp2.py
+run_test pytest -v -s $TE_PATH/tests/pytorch/distributed/test_comm_gemm_overlap.py
 # pytest -v -s $TE_PATH/tests/pytorch/distributed/test_fusible_ops_with_userbuffers.py  ### TODO Debug UB support with te.Sequential
-pytest -v -s $TE_PATH/tests/pytorch/fused_attn/test_fused_attn_with_cp.py
+run_test pytest -v -s $TE_PATH/tests/pytorch/fused_attn/test_fused_attn_with_cp.py
+
+exit $error_occurred
