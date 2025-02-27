0c3aa4354dc79bf2b28928fb89bbe7389eaee0b6
Sudhakar Singh <sudhakars@nvidia.com>
2025-02-27 14:30:33 -0800

    fix lint
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>

--- transformer_engine/pytorch/tensor/mxfp8_tensor.py
+++ transformer_engine/pytorch/tensor/mxfp8_tensor.py
@@ -285,7 +285,7 @@ class MXFP8Tensor(MXFP8TensorBase, QuantizedTensor):
         self._rowwise_data = torch.Tensor() if self._rowwise_data is not None else None
         self._columnwise_data = torch.Tensor() if self._columnwise_data is not None else None
 
-    def prepare_for_saving(self) -> Tuple[list[Optional[torch.Tensor]], Float8TensorBase]:
+    def prepare_for_saving(self) -> Tuple[list[Optional[torch.Tensor]], MXFP8TensorBase]:
         """Prepare the tensor base for saving for backward
 
         After calling this, the tensor instance does not hold any
