a8fdd09ecb1aefaea4a69c573bb9638d98d4b39f
Nicolas Castet <ncastet@nvidia.com>
2025-03-03 14:06:09 -0600

    Add initial copyright date
    Signed-off-by: Nicolas Castet <ncastet@nvidia.com>

--- transformer_engine/common/util/cuda_nvml.cpp
+++ transformer_engine/common/util/cuda_nvml.cpp
@@ -1,5 +1,5 @@
 /*************************************************************************
- * Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * Copyright (c) 2022-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  *
  * See LICENSE for license information.
  ************************************************************************/
