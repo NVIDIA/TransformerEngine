547d8dd865b60c9e080e1cfdabadbc72a2a73706
Sérgio Agostinho <159524688+sagostinho-nvidia@users.noreply.github.com>
2025-03-05 19:06:23 +0100

    Don't touch nor send messages to the root logger. (#1380)
    ---------
    
    Signed-off-by: Sérgio Agostinho <sagostinho@nvidia.com>

--- transformer_engine/jax/__init__.py
+++ transformer_engine/jax/__init__.py
@@ -12,6 +12,8 @@ from importlib.metadata import version
 from transformer_engine.common import get_te_path, is_package_installed
 from transformer_engine.common import _get_sys_extension
 
+_logger = logging.getLogger(__name__)
+
 
 def _load_library():
     """Load shared library with Transformer Engine C extensions"""
@@ -36,7 +38,7 @@ def _load_library():
 
     if is_package_installed("transformer-engine-cu12"):
         if not is_package_installed(module_name):
-            logging.info(
+            _logger.info(
                 "Could not find package %s. Install transformer-engine using 'pip"
                 " install transformer-engine[jax]==VERSION'",
                 module_name,
