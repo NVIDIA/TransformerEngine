d09d7657313a6c81d1ede79304a3ffa4a7adf5a3
Evgeny Tsykunov <e.tsykunov@gmail.com>
2025-03-05 18:37:46 +0000

    set OMP_NUM_THREADS
    Signed-off-by: Evgeny Tsykunov <e.tsykunov@gmail.com>
    Signed-off-by: Evgeny Tsykunov <etsykunov@nvidia.com>

--- qa/L0_cppunittest/test.sh
+++ qa/L0_cppunittest/test.sh
@@ -9,7 +9,12 @@ set -e
 TE_LIB_PATH=`pip show transformer-engine | grep Location | cut -d ' ' -f 2`
 export LD_LIBRARY_PATH=$TE_LIB_PATH:$LD_LIBRARY_PATH
 
+# Set parallelization parameters
+NUM_PHYSICAL_CORES=16
+NUM_PARALLEL_JOBS=4
+export OMP_NUM_THREADS=$(NUM_PHYSICAL_CORES / NUM_PARALLEL_JOBS)
+
 cd $TE_PATH/tests/cpp
 cmake -GNinja -Bbuild .
 cmake --build build
-ctest --test-dir build -j4
+ctest --test-dir build -j$NUM_PARALLEL_JOBS
