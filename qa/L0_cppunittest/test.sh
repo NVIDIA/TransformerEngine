30c773963f8ca0263bc7129505c69d06a5c44421
Evgeny Tsykunov <etsykunov@nvidia.com>
2025-03-10 20:23:21 +0000

    Debug omp num threads
    Signed-off-by: Evgeny Tsykunov <etsykunov@nvidia.com>

--- qa/L0_cppunittest/test.sh
+++ qa/L0_cppunittest/test.sh
@@ -5,7 +5,7 @@
 set -e
 
 # Find TE
-: ${TE_PATH:=/opt/transformerengine}
+: ${TE_PATH:=/workspace/TransformerEngine}
 TE_LIB_PATH=`pip show transformer-engine | grep Location | cut -d ' ' -f 2`
 export LD_LIBRARY_PATH=$TE_LIB_PATH:$LD_LIBRARY_PATH
 
@@ -17,4 +17,4 @@ cd $TE_PATH/tests/cpp
 cmake -GNinja -Bbuild .
 cmake --build build
 export OMP_NUM_THREADS=$((NUM_PHYSICAL_CORES / NUM_PARALLEL_JOBS))
-ctest --test-dir build -j$NUM_PARALLEL_JOBS
+ctest --test-dir build -j$NUM_PARALLEL_JOBS -V
