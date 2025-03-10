a3e267f637ea461d4cc8acf732e0dee907e82cdb
Jianbing Dong <jianbingd@nvidia.com>
2025-03-10 05:39:37 -0700

    verified correctness linear_cross_entropy
    Signed-off-by: Jianbing Dong <jianbingd@nvidia.com>

--- transformer_engine/pytorch/linear_cross_entropy.py
+++ transformer_engine/pytorch/linear_cross_entropy.py
@@ -10,11 +10,13 @@ Fuse cross entropy with linear layer.
 import typing
 import torch
 from transformer_engine.pytorch.triton import linear_cross_entropy_with_token_entropy as linear_cross_entropy_with_token_entropy_kernels
+from transformer_engine.pytorch.triton import linear_cross_entropy as linear_cross_entropy_kernels
+
 
 class LinearCrossEntropyWithTokenEntropy(torch.autograd.Function):
     """
-    This class implements a custom autograd function for linear (matmul) and cross entropy, whose equivalent
-    logic in PyTorch is:
+    This class implements a custom autograd function for linear (matmul) and cross entropy, as well as entropy per token,
+    whose equivalent logic in PyTorch is:
         ```python
         def torch_entropy(hidden, weight, labels):
             logits = torch.matmul(hidden, weight)
@@ -33,7 +35,7 @@ class LinearCrossEntropyWithTokenEntropy(torch.autograd.Function):
                 labels: torch.Tensor,
                 reduction: typing.Optional[str] = "mean") -> typing.List[torch.Tensor]:
         """
-        The forward pass of the Linear Cross Entropy.
+        The forward pass of the Linear Cross Entropy with Token Entropy.
         Args:
             hidden (torch.Tensor): The input tensor of shape (num_tokens, hidden_size).
             weight (torch.Tensor): The weight tensor of shape (hidden_size, vocab_size).
@@ -83,4 +85,72 @@ class LinearCrossEntropyWithTokenEntropy(torch.autograd.Function):
 
 linear_cross_entropy_with_token_entropy = LinearCrossEntropyWithTokenEntropy.apply
 
-__all__ = ["linear_cross_entropy_with_token_entropy", "LinearCrossEntropyWithTokenEntropy"]
+
+class LinearCrossEntropy(torch.autograd.Function):
+    """
+    This class implements a custom autograd function for linear and cross entropy, whose equivalent logic in PyTorch is:
+        ```python
+        def torch_entropy(hidden, weight, labels):
+            logits = torch.matmul(hidden, weight)
+            logprobs = torch.nn.functional.cross_entropy(logits, labels)
+            return logprobs
+        ```
+    """
+    @staticmethod
+    def forward(ctx,
+                hidden: torch.Tensor,
+                weight: torch.Tensor,
+                labels: torch.Tensor,
+                reduction: typing.Optional[str] = "mean") -> torch.Tensor:
+        """
+        The forward pass of the Linear Cross Entropy.
+        Args:
+            hidden (torch.Tensor): The input tensor of shape (num_tokens, hidden_size).
+            weight (torch.Tensor): The weight tensor of shape (hidden_size, vocab_size).
+            labels (torch.Tensor): The labels tensor of shape (num_tokens,).
+            reduction (str, optional): The reduction method. Defaults to "mean", and can be
+                one of "none", "sum", "mean".
+        Returns:
+            logprobs (torch.Tensor): The cross entropy.
+        """
+        with torch.cuda.nvtx.range("LinearCrossEntropy-forward"):
+            REDUCTION = linear_cross_entropy_with_token_entropy_kernels.get_entropy_reduction_enum_number(reduction.lower())
+
+            logprobs, _maximum, _maximum_indices, _acc =\
+                linear_cross_entropy_kernels.efficient_entropy_forward(hidden, weight, labels, REDUCTION)
+
+            ctx.save_for_backward(hidden, weight, labels, _maximum, _maximum_indices, _acc)
+            ctx.REDUCTION = REDUCTION
+
+        return logprobs
+
+    @staticmethod
+    def backward(ctx,
+                 dlogprobs: torch.Tensor) -> typing.List[torch.Tensor]:
+        """
+        The backward pass of the Linear Cross Entropy.
+        Args:
+            dlogprobs (torch.Tensor): The gradient of the cross entropy.
+        Returns:
+            dhidden (torch.Tensor): The gradient of the hidden.
+            dweight (torch.Tensor): The gradient of the weight.
+        """
+        with torch.cuda.nvtx.range("LinearCrossEntropy-backward"):
+            (hidden, weight, labels, _maximum, _maximum_indices, _acc) = ctx.saved_tensors
+            REDUCTION = ctx.REDUCTION
+
+            d_hidden, d_weight = linear_cross_entropy_kernels.efficient_entropy_backward(
+                dlogprobs, 
+                hidden, weight, labels,
+                _maximum, _maximum_indices, _acc,
+                REDUCTION
+            )
+        return d_hidden, d_weight, None, None
+    
+
+linear_cross_entropy = LinearCrossEntropy.apply
+
+__all__ = ["linear_cross_entropy_with_token_entropy", 
+           "LinearCrossEntropyWithTokenEntropy",
+           "linear_cross_entropy",
+           "LinearCrossEntropy"]
