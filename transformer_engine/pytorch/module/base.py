1ded1fba6a9e17a1b1fe4452c3f0e59f314ef356
Xin Yao <xiny@nvidia.com>
2025-03-13 19:15:09 -0700

    merge main
    Signed-off-by: Xin Yao <xiny@nvidia.com>

--- transformer_engine/pytorch/module/base.py
+++ transformer_engine/pytorch/module/base.py
@@ -35,7 +35,6 @@ from ..constants import dist_group_type
 from ..tensor import QuantizedTensor, Quantizer
 from ..tensor._internal.float8_tensor_base import Float8TensorBase
 from ..tensor._internal.mxfp8_tensor_base import MXFP8TensorBase
-from ..tensor.float8_tensor import Float8CurrentScalingQuantizer
 from ..utils import torch_get_autocast_gpu_dtype
 
 __all__ = ["initialize_ub", "destroy_ub"]
