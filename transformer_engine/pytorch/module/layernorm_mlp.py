fece5706a95dd37132b9f4600bac8f20636f0882
Li Tao <lit@nvidia.com>
2025-03-14 00:16:06 -0700

    fall back to get_buffer in layernormmlp
    Signed-off-by: Li Tao <lit@nvidia.com>

--- transformer_engine/pytorch/module/layernorm_mlp.py
+++ transformer_engine/pytorch/module/layernorm_mlp.py
@@ -277,9 +277,9 @@ class _LayerNormMLP(torch.autograd.Function):
         # So the output of normalization is in high precision, and we need to quantize it to FP8 and put in the buffer.
         if ub_overlap_ag and isinstance(fc1_input_quantizer, Float8CurrentScalingQuantizer):
             ub_obj_lnout = get_ub("fc1_fprop")
-            ln_out_local = fc1_input_quantizer.quantize(ln_out)
-            ub_obj_lnout.copy_into_buffer(ln_out_local, fc1_input_quantizer, local_chunk=True)
-            ln_out = ln_out_local
+            ln_out_local = ln_out
+            ln_out = ub_obj_lnout.get_buffer(fc1_input_quantizer, local_chunk=True)
+            fc1_input_quantizer.quantize(ln_out_local, out=ln_out)
 
         # Prepare GEMM input
         # Note: Cast to expected dtype and perform tensor-parallel communication
