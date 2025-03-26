c9b57d497dbd44603a2867d4198cdb746db2c02d
Vladimir Cherepanov <vcherepanov@nvidia.com>
2025-03-26 20:50:32 +0000

    Test fixure

--- tests/cpp/comm_gemm/test_comm_gemm.cu
+++ tests/cpp/comm_gemm/test_comm_gemm.cu
@@ -21,11 +21,32 @@ int main(int argc, char* argv[]) {
   return ret;
 }
 
-TEST(TestCommGemm, Rank) {
-  int rank = -1;
-  CHECK_MPI(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
-  EXPECT_EQ(rank, 0);
-  auto* ctx = nvte_comm_gemm_ctx_create();
-  nvte_comm_gemm(ctx);
-  nvte_comm_gemm_ctx_destroy(ctx);
+class CommGemmTest : public ::testing::Test {
+ protected:
+  CommGemmTest() {
+    int nranks{};
+    CHECK_MPI(MPI_Comm_size(MPI_COMM_WORLD, &nranks));
+    int rank{};
+    CHECK_MPI(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
+    int local_device = rank;
+    ctx_ = nvte_comm_gemm_ctx_create(nranks, rank, local_device);
+  }
+  ~CommGemmTest() { nvte_comm_gemm_ctx_destroy(ctx_); }
+
+  CommGemmCtx* ctx_;
+};
+
+TEST_F(CommGemmTest, Rank) {
+  NVTETensor a{};
+  NVTETensor b{};
+  NVTETensor d{};
+  NVTETensor bias{};
+  NVTETensor pre_gelu_out{};
+  bool transa = false;
+  bool transb = false;
+  bool grad = false;
+  bool accumulate = false;
+  int comm_sm_count = 0;
+  nvte_comm_gemm(ctx_, a, b, d, bias, pre_gelu_out, transa, transb, grad, accumulate,
+                 comm_sm_count);
 }
