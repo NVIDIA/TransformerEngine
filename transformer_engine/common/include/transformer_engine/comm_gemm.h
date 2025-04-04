77b2f4897a25fdc72acb4b110892ce0c0ac788f5
Vladimir Cherepanov <vcherepanov@nvidia.com>
2025-04-04 01:04:49 +0000

    Saving...
    Signed-off-by: Vladimir Cherepanov <vcherepanov@nvidia.com>

--- transformer_engine/common/include/transformer_engine/comm_gemm.h
+++ transformer_engine/common/include/transformer_engine/comm_gemm.h
@@ -7,6 +7,8 @@
 #ifndef TRANSFORMER_ENGINE_COMMON_COMM_GEMM_H_
 #define TRANSFORMER_ENGINE_COMMON_COMM_GEMM_H_
 
+#include <stdint.h>
+
 #include "transformer_engine.h"
 
 #ifdef __cplusplus
@@ -18,9 +20,10 @@ typedef struct CommGemmCtx CommGemmCtx;
 CommGemmCtx* nvte_comm_gemm_ctx_create(int nranks, int rank, int local_device);
 void nvte_comm_gemm_ctx_destroy(CommGemmCtx* ctx) noexcept;
 
-void nvte_comm_gemm(CommGemmCtx* ctx, const NVTETensor a, const NVTETensor b, NVTETensor d,
-                    const NVTETensor bias, NVTETensor pre_gelu_out, bool transa, bool transb,
-                    bool grad, bool accumulate, int comm_sm_count);
+void nvte_comm_gemm(CommGemmCtx* ctx, int64_t m, int64_t n, int64_t k, const NVTETensor a,
+                    const NVTETensor b, const NVTETensor d, const NVTETensor bias,
+                    const NVTETensor pre_act_out, bool transa, bool transb, bool grad,
+                    bool accumulate, int comm_sm_count);
 
 #ifdef __cplusplus
 }  // extern "C"
