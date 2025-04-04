ac221c582a97e6485c9b4151f5a7bc6024dc7048
Phuong Nguyen <phuonguyen@nvidia.com>
2025-04-04 07:44:48 -0700

    sharding scale_inv for mxfp8
    Signed-off-by: Phuong Nguyen <phuonguyen@nvidia.com>

--- transformer_engine/jax/quantize/tensor.py
+++ transformer_engine/jax/quantize/tensor.py
@@ -236,10 +236,16 @@ class ScaledTensor1x(ScaledTensor):
 
         data = with_sharding_constraint_by_logical_axes(self.data, axis_names)
 
+        if self.scaling_mode == ScalingMode.NVTE_MXFP8_1D_SCALING:
+            # TODO(Phuong): Handle padding !?
+            scale_inv = with_sharding_constraint_by_logical_axes(self.scale_inv, axis_names)
+        else:
+            scale_inv = self.scale_inv
+
         # TODO(Phuong): constaint padded scale_inv?
         return ScaledTensor1x(
             data=data,
-            scale_inv=self.scale_inv,
+            scale_inv=scale_inv,
             scaling_mode=self.scaling_mode,
             dq_dtype=self.dq_dtype,
             _dq_func=self._dq_func,
