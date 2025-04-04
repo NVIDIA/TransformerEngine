1a32f429c4639690e6a307c92ecbf2eea07c80eb
Vladimir Cherepanov <vcherepanov@nvidia.com>
2025-04-04 01:04:49 +0000

    Saving...

--- tests/cpp/comm_gemm/test_comm_gemm.cu
+++ tests/cpp/comm_gemm/test_comm_gemm.cu
@@ -1,6 +1,13 @@
 #include <gtest/gtest.h>
 #include <mpi.h>
 #include <transformer_engine/comm_gemm.h>
+#include <transformer_engine/gemm.h>
+
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+
+#include "../test_common.h"
 
 #define CHECK_MPI(expr)                                              \
   do {                                                               \
@@ -21,32 +28,64 @@ int main(int argc, char* argv[]) {
   return ret;
 }
 
-class CommGemmTest : public ::testing::Test {
+struct TestDims {
+  size_t m;
+  size_t n;
+  size_t k;
+};
+
+class CommGemmTest : public ::testing::TestWithParam<TestDims> {
  protected:
   CommGemmTest() {
     int nranks{};
     CHECK_MPI(MPI_Comm_size(MPI_COMM_WORLD, &nranks));
-    int rank{};
-    CHECK_MPI(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
-    int local_device = rank;
-    ctx_ = nvte_comm_gemm_ctx_create(nranks, rank, local_device);
+    CHECK_MPI(MPI_Comm_rank(MPI_COMM_WORLD, &rank_));
+    int local_device = rank_;
+    ctx_ = nvte_comm_gemm_ctx_create(nranks, rank_, local_device);
   }
   ~CommGemmTest() { nvte_comm_gemm_ctx_destroy(ctx_); }
 
-  CommGemmCtx* ctx_;
+  CommGemmCtx* ctx_{};
+  int rank_;
 };
 
-TEST_F(CommGemmTest, Rank) {
-  NVTETensor a{};
-  NVTETensor b{};
-  NVTETensor d{};
-  NVTETensor bias{};
-  NVTETensor pre_gelu_out{};
+TEST_P(CommGemmTest, GEMM) {
+  auto [m, n, k] = GetParam();
+  auto dtype = transformer_engine::DType::kFloat32;
+  test::Tensor a("a", {k, m}, dtype);
+  test::Tensor b("b", {n, k}, dtype);
+  test::Tensor d("d", {n, m}, dtype);
+  test::Tensor bias;
+  test::Tensor pre_act_out;
+  // test::Tensor bias("bias", {m, n}, dtype);
+  // test::Tensor pre_act_out("pre_act_out", {m, n}, dtype);
+  test::Tensor workspace("workspace", {32 << 20}, transformer_engine::DType::kByte);
   bool transa = false;
   bool transb = false;
   bool grad = false;
   bool accumulate = false;
-  int comm_sm_count = 0;
-  nvte_comm_gemm(ctx_, a, b, d, bias, pre_gelu_out, transa, transb, grad, accumulate,
-                 comm_sm_count);
+  cudaStream_t stream{};
+  NVTE_CHECK_CUDA(cudaStreamCreate(&stream));
+  auto pa = a.rowwise_cpu_dptr<float>();
+  std::iota(pa, pa + m * k, 0);
+  a.from_cpu();
+  auto pb = b.rowwise_cpu_dptr<float>();
+  std::iota(pb, pb + n * k, 10);
+  b.from_cpu();
+  nvte_cublas_gemm(a.data(), b.data(), d.data(), bias.data(), pre_act_out.data(), transa, transb,
+                   grad, workspace.data(), accumulate, false /* use_split_accumulator */,
+                   0 /* math_sm_count */, stream);
+  NVTE_CHECK_CUDA(cudaStreamSynchronize(stream));
+  NVTE_CHECK_CUDA(cudaStreamDestroy(stream));
+  d.to_cpu();
+  auto pd = d.rowwise_cpu_dptr<float>();
+  if (rank_ == 0) {
+    for (size_t i = 0; i < m * n; ++i) {
+      std::cerr << i << ": " << pd[i] << "\n";
+    }
+  }
+  nvte_comm_gemm(ctx_, m, n, k, a.data(), b.data(), d.data(), bias.data(), pre_act_out.data(),
+                 transa, transb, grad, accumulate, 0 /* comm_sm_count */);
 }
+
+INSTANTIATE_TEST_SUITE_P(CommGemmShapes, CommGemmTest, testing::Values(TestDims{2, 3, 4}));
