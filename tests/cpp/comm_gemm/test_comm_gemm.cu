da9e1f50f8af800b28ac883fdf4ab9df0d565481
Vladimir Cherepanov <vcherepanov@nvidia.com>
2025-04-07 22:01:37 +0000

    Saving...
    Signed-off-by: Vladimir Cherepanov <vcherepanov@nvidia.com>

--- tests/cpp/comm_gemm/test_comm_gemm.cu
+++ tests/cpp/comm_gemm/test_comm_gemm.cu
@@ -9,6 +9,7 @@
 #include <iterator>
 #include <numeric>
 #include <random>
+#include <sstream>
 
 #include "common.h"
 
@@ -44,22 +45,42 @@ class CommGemmTest : public ::testing::TestWithParam<Params> {
     CHECK_MPI(MPI_Comm_size(MPI_COMM_WORLD, &nranks_));
     CHECK_MPI(MPI_Comm_rank(MPI_COMM_WORLD, &rank_));
     int local_device = rank_;
-    ctx_ = nvte_comm_gemm_ctx_create(nranks_, rank_, local_device);
     NVTE_CHECK_CUDA(cudaSetDevice(rank_));
+    ctx_ = nvte_comm_gemm_ctx_create(nranks_, rank_, local_device);
   }
   ~CommGemmTest() { nvte_comm_gemm_ctx_destroy(ctx_); }
 
   template <typename T>
-  transformer_engine::Tensor MakeDeviceTensor(size_t m, size_t n, T fill_value,
-                                              transformer_engine::DType dtype) {
-    std::vector<T> values(m * n, fill_value);
+  transformer_engine::Tensor MakeDeviceTensor(size_t m, size_t n, transformer_engine::DType dtype,
+                                              cudaStream_t stream) {
     void* dptr{};
-    NVTE_CHECK_CUDA(cudaMalloc(&dptr, values.size() * sizeof values[0]));
-    NVTE_CHECK_CUDA(
-        cudaMemcpy(dptr, values.data(), values.size() * sizeof values[0], cudaMemcpyDefault));
+    NVTE_CHECK_CUDA(cudaMallocAsync(&dptr, m * n * sizeof(T), stream));
+    NVTE_CHECK_CUDA(cudaMemsetAsync(dptr, 0, m * n * sizeof(T), stream));
 
     transformer_engine::Tensor ret;
     ret.data = {dptr, {m, n}, dtype};
+    return ret;
+  }
+
+  template <typename T>
+  transformer_engine::Tensor MakeDeviceTensorFromData(const std::vector<T>& data, size_t mstart,
+                                                      size_t nstart, size_t mb, size_t nb,
+                                                      size_t ld, transformer_engine::DType dtype,
+                                                      cudaStream_t stream) {
+    std::vector<T> values(mb * nb);
+    size_t dst = 0;
+    for (size_t i = mstart; i < mstart + mb; ++i) {
+      for (size_t j = nstart; j < nstart + nb; ++j) {
+        values[dst++] = data[i * ld + j];
+      }
+    }
+    void* dptr{};
+    NVTE_CHECK_CUDA(cudaMallocAsync(&dptr, values.size() * sizeof values[0], stream));
+    NVTE_CHECK_CUDA(cudaMemcpyAsync(dptr, values.data(), values.size() * sizeof values[0],
+                                    cudaMemcpyDefault, stream));
+
+    transformer_engine::Tensor ret;
+    ret.data = {dptr, {mb, nb}, dtype};
     return ret;
   }
 
@@ -68,31 +89,53 @@ class CommGemmTest : public ::testing::TestWithParam<Params> {
     cudaStream_t stream{};
     NVTE_CHECK_CUDA(cudaStreamCreate(&stream));
 
-    // std::mt19937 rng(12);
-    // std::uniform_real_distribution<double> dist(0.0, 0.1);
-    //
-    // std::vector<T> a(m * k);
-    // std::generate(a.begin(), a.end(), [&rng, &dist] { return dist(rng); });
-    // std::vector<T> b(k * n);
-    // std::generate(b.begin(), b.end(), [&rng, &dist] { return dist(rng); });
-    // std::vector<T> d(m * n);
-    // std::generate(d.begin(), d.end(), [&rng, &dist] { return dist(rng); });
+    std::mt19937 rng(12);
+    std::uniform_real_distribution<double> dist(0.0, 1.0);
 
-    auto a = MakeDeviceTensor<T>(k, m / nranks_, 1.0, dtype);
-    auto b = MakeDeviceTensor<T>(k, n / nranks_, 2.0, dtype);
-    auto d = MakeDeviceTensor<T>(m / nranks_, n, 0.0, dtype);
+    std::vector<T> adata(m * k);
+    std::generate(adata.begin(), adata.end(), [&rng, &dist] { return dist(rng); });
+    std::vector<T> bdata(k * n);
+    std::generate(bdata.begin(), bdata.end(), [&rng, &dist] { return dist(rng); });
+
+    auto ga = MakeDeviceTensorFromData<T>(adata, 0, 0, k, m, m, dtype, stream);
+    auto gb = MakeDeviceTensorFromData<T>(bdata, 0, 0, k, n, n, dtype, stream);
+    auto gd = MakeDeviceTensor<T>(m, n, dtype, stream);
+
+    auto a = MakeDeviceTensorFromData<T>(adata, 0, m / nranks_ * rank_, k, m / nranks_, m, dtype,
+                                         stream);
+    auto b = MakeDeviceTensorFromData<T>(bdata, 0, n / nranks_ * rank_, k, n / nranks_, n, dtype,
+                                         stream);
+    auto d = MakeDeviceTensor<T>(m / nranks_, n, dtype, stream);
 
     transformer_engine::Tensor bias;
     transformer_engine::Tensor pre_act_out;
-    nvte_comm_gemm(ctx_, m, n, k, &a, &b, &d, &bias, &pre_act_out, true /*transa*/,
-                   false /*transb*/, false /*grad*/, false /*accumulate*/, 0 /*comm_sm_count*/,
-                   stream);
+    bool transa = true;
+    bool transb = false;
+    bool grad = false;
+    bool accumulate = false;
+    nvte_comm_gemm(ctx_, m, n, k, &a, &b, &d, &bias, &pre_act_out, transa, transb, grad, accumulate,
+                   0 /*comm_sm_count*/, stream);
+    auto workspace =
+        MakeDeviceTensor<uint8_t>(1, 32 << 20, transformer_engine::DType::kByte, stream);
+    nvte_cublas_gemm(&ga, &gb, &gd, &bias, &pre_act_out, transa, transb, grad, &workspace,
+                     accumulate, false /* use_split_accumulator */, 0 /* math_sm_count */, stream);
 
-    std::vector<T> out(m * n);
+    std::vector<T> out(m * n / nranks_);
     NVTE_CHECK_CUDA(cudaMemcpyAsync(out.data(), d.data.dptr, out.size() * sizeof out[0],
                                     cudaMemcpyDefault, stream));
+    std::vector<T> out_golden(m * n / nranks_);
+    NVTE_CHECK_CUDA(cudaMemcpyAsync(
+        out_golden.data(), static_cast<const T*>(gd.data.dptr) + n * k / nranks_ * rank_,
+        out_golden.size() * sizeof out_golden[0], cudaMemcpyDefault, stream));
     NVTE_CHECK_CUDA(cudaStreamSynchronize(stream));
     NVTE_CHECK_CUDA(cudaStreamDestroy(stream));
+
+    for (size_t i = 0; i < out.size(); ++i) {
+      std::ostringstream ss;
+      ss << rank_ << ": " << i << ": " << static_cast<float>(out[i]) << " "
+         << static_cast<float>(out_golden[i]);
+      std::cerr << ss.str() << "\n";
+    }
   }
 
   CommGemmCtx* ctx_{};
@@ -103,48 +146,7 @@ class CommGemmTest : public ::testing::TestWithParam<Params> {
 TEST_P(CommGemmTest, AgGemm) {
   auto [dtype, m, n, k] = GetParam();
   TRANSFORMER_ENGINE_TYPE_SWITCH_INPUT(dtype, DType, { TestAgGemm<DType>(m, n, k, dtype); });
-
-  // std::vector<DType> a(m * k);
-  // std::generate(a.begin(), a.end(), [&rng, &dist] { return dist(rng); });
-  // std::vector<DType> b(k * n);
-  // std::generate(b.begin(), b.end(), [&rng, &dist] { return dist(rng); });
-  // std::vector<DType> d(m * n);
-  // std::generate(d.begin(), d.end(), [&rng, &dist] { return dist(rng); });
-  // test::Tensor a("a", {k, m}, dtype);
-  // test::Tensor b("b", {n, k}, dtype);
-  // test::Tensor d("d", {n, m}, dtype);
-  // test::Tensor bias;
-  // test::Tensor pre_act_out;
-  // // test::Tensor bias("bias", {m, n}, dtype);
-  // // test::Tensor pre_act_out("pre_act_out", {m, n}, dtype);
-  // test::Tensor workspace("workspace", {32 << 20}, transformer_engine::DType::kByte);
-  // bool transa = true;
-  // bool transb = false;
-  // bool grad = false;
-  // bool accumulate = false;
-  // cudaStream_t stream{};
-  // NVTE_CHECK_CUDA(cudaStreamCreate(&stream));
-  // auto pa = a.rowwise_cpu_dptr<float>();
-  // std::iota(pa, pa + m * k, 0);
-  // a.from_cpu();
-  // auto pb = b.rowwise_cpu_dptr<float>();
-  // std::iota(pb, pb + n * k, 10);
-  // b.from_cpu();
-  // // nvte_cublas_gemm(a.data(), b.data(), d.data(), bias.data(), pre_act_out.data(), transa, transb,
-  // //                  grad, workspace.data(), accumulate, false /* use_split_accumulator */,
-  // //                  0 /* math_sm_count */, stream);
-  // NVTE_CHECK_CUDA(cudaStreamSynchronize(stream));
-  // NVTE_CHECK_CUDA(cudaStreamDestroy(stream));
-  // nvte_comm_gemm(ctx_, m, n, k, a.data(), b.data(), d.data(), bias.data(), pre_act_out.data(),
-  //                transa, transb, grad, accumulate, 0 /* comm_sm_count */);
-  // d.to_cpu();
-  // auto pd = d.rowwise_cpu_dptr<float>();
-  // if (rank_ == 0) {
-  //   for (size_t i = 0; i < m * n; ++i) {
-  //     std::cerr << i << ": " << pd[i] << "\n";
-  //   }
-  // }
 }
 
 INSTANTIATE_TEST_SUITE_P(CommGemmShapes, CommGemmTest,
-                         testing::Values(Params{transformer_engine::DType::kFloat32, 8, 4, 2}));
+                         testing::Values(Params{transformer_engine::DType::kFloat16, 8, 4, 2}));
