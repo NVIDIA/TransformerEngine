be84c7612262871759648d0b4173c07599246522
Pawel Gadzinski <pgadzinski@nvidia.com>
2025-04-11 14:21:33 +0200

    fix
    Signed-off-by: Pawel Gadzinski <pgadzinski@nvidia.com>

--- transformer_engine/pytorch/tensor/_internal/float8_blockwise_tensor_base.py
+++ transformer_engine/pytorch/tensor/_internal/float8_blockwise_tensor_base.py
@@ -72,12 +72,13 @@ class Float8BlockwiseQTensorBase:
         self,
     ) -> Tuple[list[Optional[torch.Tensor]], Float8BlockwiseQTensorBase]:
         """
-                Prepare the tensor base for saving for backward
-                This does not clear the tensors currently, because with PP config	
-                that clears the weight cache between micro-batches. If the rowwise	
-                data is not required for backward, this is a possible memory	
-                pessimization, but is consistent with the other quantized tensor	
-                classes.	
+        Prepare the tensor base for saving for backward
+
+        This does not clear the tensors currently, because with PP config	
+        that clears the weight cache between micro-batches. If the rowwise	
+        data is not required for backward, this is a possible memory	
+        pessimization, but is consistent with the other quantized tensor	
+        classes.	
         """
         tensors = [self._rowwise_data, self._columnwise_data]
         return tensors, self
