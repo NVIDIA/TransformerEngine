da10a0328c12533b812fe801291266b34eae0137
Pawel Gadzinski <pgadzinski@nvidia.com>
2025-04-16 12:06:15 +0000

    fix
    Signed-off-by: Pawel Gadzinski <pgadzinski@nvidia.com>

--- transformer_engine/pytorch/tensor/_internal/float8_blockwise_tensor_base.py
+++ transformer_engine/pytorch/tensor/_internal/float8_blockwise_tensor_base.py
@@ -58,10 +58,10 @@ class Float8BlockwiseQTensorBase:
 
     def clear(self):
         """Deallocate this tensor's memory. Typically not needed and must be used carefully."""
-        self._rowwise_data = torch.Tensor() if self._rowwise_data is not None else None
-        self._columnwise_data = torch.Tensor() if self._columnwise_data is not None else None
-        self._rowwise_scale_inv = torch.Tensor() if self._rowwise_scale_inv is not None else None
-        self._columnwise_scale_inv = (
+        self._rowwise_data.data = torch.Tensor() if self._rowwise_data is not None else None
+        self._columnwise_data.data = torch.Tensor() if self._columnwise_data is not None else None
+        self._rowwise_scale_inv.data = torch.Tensor() if self._rowwise_scale_inv is not None else None
+        self._columnwise_scale_inv.data = (
             torch.Tensor() if self._columnwise_scale_inv is not None else None
         )
 
--- transformer_engine/pytorch/tensor/_internal/mxfp8_tensor_base.py
+++ transformer_engine/pytorch/tensor/_internal/mxfp8_tensor_base.py
@@ -83,10 +83,10 @@ class MXFP8TensorBase:
 
     def clear(self):
         """Deallocate this tensor's memory. Typically not needed and must be used carefully."""
-        self._rowwise_data = torch.Tensor() if self._rowwise_data is not None else None
-        self._columnwise_data = torch.Tensor() if self._columnwise_data is not None else None
-        self._rowwise_scale_inv = torch.Tensor() if self._rowwise_scale_inv is not None else None
-        self._columnwise_scale_inv = (
+        self._rowwise_data.data = torch.Tensor() if self._rowwise_data is not None else None
+        self._columnwise_data.data = torch.Tensor() if self._columnwise_data is not None else None
+        self._rowwise_scale_inv.data = torch.Tensor() if self._rowwise_scale_inv is not None else None
+        self._columnwise_scale_inv.data = (
             torch.Tensor() if self._columnwise_scale_inv is not None else None
         )
 
