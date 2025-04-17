e118589136b732256621685d2040ae79df213261
Sudhakar Singh <sudhakars@nvidia.com>
2025-04-16 17:59:12 -0700

    add more info
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>

--- transformer_engine/common/include/transformer_engine/transformer_engine.h
+++ transformer_engine/common/include/transformer_engine/transformer_engine.h
@@ -332,9 +332,9 @@ void nvte_set_quantization_config_attribute(NVTEQuantizationConfig config,
  */
 void nvte_destroy_quantization_config(NVTEQuantizationConfig config);
 
-/*! \brief Check if non-TN Gemm is supported.
+/*! \brief Check if non-TN FP8 Gemm is supported.
  *
- *  \return A flag which indicates whether non-TN Gemm is supported or not.
+ *  \return A flag which indicates whether non-TN FP8 Gemm is supported or not.
  */
 int nvte_is_non_tn_fp8_gemm_supported();
 
