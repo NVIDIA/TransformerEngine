f6eb9842eacda17be02301e918149213839bbcbd
Kshitij Janardan Lakhani <klakhani@nvidia.com>
2025-04-22 14:32:06 -0700

    Refactor attention dir
    Signed-off-by: Kshitij Janardan Lakhani <klakhani@nvidia.com>

---
+++ transformer_engine/pytorch/attention/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# See LICENSE for license information.
+"""Python interface for attention"""
+from .dot_product_attention import DotProductAttention
+from .multi_head_attention import MultiheadAttention
+
+__all__ = ["DotProductAttention", "MultiheadAttention"]
