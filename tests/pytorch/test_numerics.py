85886eb35dcf57a37ddc98a13d283f7a6d8f8e32
Xiaowei Ren <xren@nvidia.com>
2025-04-25 19:50:45 -0700

    fix device compute capability of pytorch tests
    Signed-off-by: Xiaowei Ren <xren@nvidia.com>

--- tests/pytorch/test_numerics.py
+++ tests/pytorch/test_numerics.py
@@ -54,7 +54,7 @@ fp8_block_scaling_available, reason_for_no_fp8_block_scaling = (
     FP8GlobalStateManager.is_fp8_block_scaling_available()
 )
 
-sm_80plus = get_device_compute_capability() >= (8, 0)
+sm_80plus = get_device_compute_capability() >= 80
 
 seed = 1234
 torch.manual_seed(seed)
