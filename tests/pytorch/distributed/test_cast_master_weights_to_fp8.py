b001724ddf4fc5e5ab50c96159aa229c2c74854a
Tim Moon <4406448+timmoon10@users.noreply.github.com>
2025-04-28 18:18:22 -0700

    Update tests/pytorch/distributed/test_cast_master_weights_to_fp8.py
    Signed-off-by: Tim Moon <4406448+timmoon10@users.noreply.github.com>

--- tests/pytorch/distributed/test_cast_master_weights_to_fp8.py
+++ tests/pytorch/distributed/test_cast_master_weights_to_fp8.py
@@ -36,5 +36,5 @@ def test_cast_master_weights_to_fp8(quantization):
     if quantization in ("fp8", "fp8_cs") and not fp8_available:
         pytest.skip(reason_for_no_fp8)
     if quantization == "fp8_block" and not fp8_block_scaling_available:
-        pytest.skip(fp8_block_scaling_available)
+        pytest.skip(reason_for_no_fp8_block_scaling)
     _run_test(quantization)
