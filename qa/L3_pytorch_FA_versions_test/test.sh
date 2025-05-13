6c83e7ebb7559933594b2d474dbbcbd4e8703ce2
Charlene Yang <8636796+cyanguwa@users.noreply.github.com>
2025-05-13 13:45:15 -0700

    add FA env var for all archs
    Signed-off-by: Charlene Yang <8636796+cyanguwa@users.noreply.github.com>

--- qa/L3_pytorch_FA_versions_test/test.sh
+++ qa/L3_pytorch_FA_versions_test/test.sh
@@ -15,12 +15,12 @@ export MAX_JOBS=32
 
 # Iterate over Flash Attention versions
 sm_arch=`python3 -c "import torch; sm = torch.cuda.get_device_capability(0); print(sm[0]*10+sm[1])"`
+export FLASH_ATTN_CUDA_ARCHS=$sm_arch
 if [ $sm_arch -gt 90 ]
 then
   FA_versions=(2.7.3)
 elif [ $sm_arch -eq 90 ]
 then
-  export FLASH_ATTN_CUDA_ARCHS=90
   FA_versions=(2.5.7 2.7.3 3.0.0b1)
 fi
 
