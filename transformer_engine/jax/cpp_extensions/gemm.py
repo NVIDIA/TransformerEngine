9f461912e2157d17508a5b5b74c78fd552b9cae8
Phuong Nguyen <phuonguyen@nvidia.com>
2025-05-27 11:01:39 -0700

    fix graph issues
    Signed-off-by: Phuong Nguyen <phuonguyen@nvidia.com>

--- transformer_engine/jax/cpp_extensions/gemm.py
+++ transformer_engine/jax/cpp_extensions/gemm.py
@@ -142,10 +142,6 @@ def _calculate_remaining_shape(shape, contracting_dims):
     return tuple(shape[dim] for dim in range(len(shape)) if dim not in contracting_dims)
 
 
-def _dequantize(x, scale_inv, dq_dtype):
-    return x.astype(dq_dtype) * scale_inv.astype(dq_dtype)
-
-
 # Apply jit to guarantee correctness of FP8 GEMM.
 @partial(
     jax.jit,
@@ -164,11 +160,11 @@ def __jitted_jax_gemm_tensor_scaling_fp8(lhs, rhs, lhs_dn, rhs_dn, precision):
 
     dim_nums = (((2,), (2,)), ((0,), (0,)))
     out_fp8 = jax.lax.dot_general(
-        lhs_3d, rhs_3d, dim_nums, precision=precision, preferred_element_type=lhs.dq_dtype
+        lhs_3d, rhs_3d, dim_nums, preferred_element_type=jnp.float32
     )
-    out_3d = _dequantize(out_fp8, lhs.scale_inv * rhs.scale_inv, lhs.dq_dtype)
+    out_fp32 = (out_fp8 * (lhs.scale_inv * rhs.scale_inv).astype(jnp.float32))
 
-    return out_3d
+    return out_fp32.astype(lhs.dq_dtype)
 
 
 def _jax_gemm_tensor_scaling_fp8(
