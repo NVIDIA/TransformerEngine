59f1466e9a71ddd55f8334a1224471daa8c325ad
Przemek Tredak <ptredak@nvidia.com>
2025-05-30 13:54:48 -0700

    Use versioned flavor of get driver entrypoint function
    Signed-off-by: Przemek Tredak <ptredak@nvidia.com>

--- transformer_engine/common/util/cuda_driver.cpp
+++ transformer_engine/common/util/cuda_driver.cpp
@@ -13,10 +13,15 @@ namespace transformer_engine {
 
 namespace cuda_driver {
 
-void *get_symbol(const char *symbol) {
+void *get_symbol(const char *symbol, int cuda_version) {
   void *entry_point;
   cudaDriverEntryPointQueryResult driver_result;
+#if CUDA_VERSION < 13000
   NVTE_CHECK_CUDA(cudaGetDriverEntryPoint(symbol, &entry_point, cudaEnableDefault, &driver_result));
+#else
+  NVTE_CHECK_CUDA(cudaGetDriverEntryPointByVersion(symbol, &entry_point, cuda_version,
+                                                   cudaEnableDefault, &driver_result));
+#endif
   NVTE_CHECK(driver_result == cudaDriverEntryPointSuccess,
              "Could not find CUDA driver entry point for ", symbol);
   return entry_point;
