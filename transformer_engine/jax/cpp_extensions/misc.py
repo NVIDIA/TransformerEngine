b392179adb38df352fc3f782aa43bd42fbe83cb7
Jeremy Berchtold <jberchtold@nvidia.com>
2025-06-03 10:39:27 -0700

    Fix 1x quantize kernel availability check on hopper
    Signed-off-by: Jeremy Berchtold <jberchtold@nvidia.com>

--- transformer_engine/jax/cpp_extensions/misc.py
+++ transformer_engine/jax/cpp_extensions/misc.py
@@ -183,6 +183,16 @@ def get_xla_flag(flag: str, default=None, cast=str):
     return default
 
 
+def get_max_device_compute_capability():
+    """
+    Returns the maximum compute capability of all local devices.
+    """
+    return max(
+        transformer_engine_jax.get_device_compute_capability(local_gpu_id)
+        for local_gpu_id in range(len(jax.local_devices()))
+    )
+
+
 def should_apply_1x_fused_dbias_war_for_arch_l_100(is_dbias: bool = False, quantizer=None):
     """
     Fused dbias is not supported for arch < 100 for 1x quantization, so we need to apply a workaround to
