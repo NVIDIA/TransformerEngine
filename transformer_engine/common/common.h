bc50ee6c3a44ecea2c4f13950bf3e2db94db36d2
Oleg Goncharov <64355998+Oleg-Goncharov@users.noreply.github.com>
2025-06-11 16:30:33 +0200

    Typo fix
    Signed-off-by: Oleg Goncharov <64355998+Oleg-Goncharov@users.noreply.github.com>

--- transformer_engine/common/common.h
+++ transformer_engine/common/common.h
@@ -707,7 +707,7 @@ CUtensorMapDataType get_CUtensorMapDataType(DType dtype);
 void create_2D_tensor_map(CUtensorMap &tensorMap, const SimpleTensor &tensor,
                           const uint64_t globalY, const uint64_t globalX, const uint32_t shmemY,
                           const uint32_t shmemX, const uint32_t stride_elems,
-                          const uint32_t offset_elems, const double type_size);
+                          const uint32_t offset_elems, const size_t type_num_bits);
 
 bool is_supported_by_CC_100();
 
