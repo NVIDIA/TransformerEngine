06ad2349d5f8ba2c03fd3bbf6f5a86f2c2a99e3e
Jeremy Berchtold <jberchtold@nvidia.com>
2025-06-11 13:45:59 -0700

    Fix cppunittest test.sh for editable installs
    Signed-off-by: Jeremy Berchtold <jberchtold@nvidia.com>

--- qa/L0_cppunittest/test.sh
+++ qa/L0_cppunittest/test.sh
@@ -6,7 +6,7 @@ set -e
 
 # Find TE
 : ${TE_PATH:=/opt/transformerengine}
-TE_LIB_PATH=`pip3 show transformer-engine | grep Location | cut -d ' ' -f 2`
+TE_LIB_PATH=$(pip3 show transformer-engine | grep -E "Location:|Editable project location:" | tail -n 1 | awk '{print $NF}')
 export LD_LIBRARY_PATH=$TE_LIB_PATH:$LD_LIBRARY_PATH
 
 # Set parallelization parameters
@@ -14,7 +14,7 @@ NUM_PHYSICAL_CORES=$(nproc)
 NUM_PARALLEL_JOBS=4
 
 cd $TE_PATH/tests/cpp
-cmake -GNinja -Bbuild .
+cmake -GNinja -Bbuild -DTE_LIB_PATH="$TE_LIB_PATH" .
 cmake --build build
 export OMP_NUM_THREADS=$((NUM_PHYSICAL_CORES / NUM_PARALLEL_JOBS))
 ctest --test-dir build -j$NUM_PARALLEL_JOBS
