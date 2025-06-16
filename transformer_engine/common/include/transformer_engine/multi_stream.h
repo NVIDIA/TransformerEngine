01a504c4bdbb21aa2bfaddb8fc49c3a7f0e1fbac
Hua Huang <huah@nvidia.com>
2025-06-16 08:33:37 -0700

    [JAX] Grouped GEMM & Dense support MXFP8 and handle empty matrices (#1871)
    * Support MXFP8 and handle empty matrices
    
    Signed-off-by: Hua Huang <huah@nvidia.com>
    
    ---------
    
    Signed-off-by: Hua Huang <huah@nvidia.com>

--- transformer_engine/common/include/transformer_engine/multi_stream.h
+++ transformer_engine/common/include/transformer_engine/multi_stream.h
@@ -11,12 +11,34 @@
 #ifndef TRANSFORMER_ENGINE_MULTI_STREAM_H
 #define TRANSFORMER_ENGINE_MULTI_STREAM_H
 
+#include "cuda_runtime.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /*! \brief Number of CUDA streams to use in multi-stream operations */
 int nvte_get_num_compute_streams();
+
+/*! \brief Get a CUDA stream for compute operations.
+ *
+ *  \param[in] idx Index of the stream to retrieve.Add commentMore actions
+ *  \return A cudaStream_t.
+ *
+ *  This function returns a CUDA stream that can be used for compute operations.
+ *  The index should be in the range [0, nvte_get_num_compute_streams() - 1].
+ */
+cudaStream_t nvte_get_compute_stream(const int idx);
+
+/*! \brief Get a CUDA event for compute operations.
+ *
+ *  \param[in] idx Index of the event to retrieve.
+ *  \return A cudaEvent_t.
+ *
+ *  This function returns a CUDA event that can be used to synchronize compute operations.
+ *  The index should be in the range [0, nvte_get_num_compute_streams() - 1].
+ */
+cudaEvent_t nvte_get_compute_stream_event(const int idx);
 
 #ifdef __cplusplus
 }  // extern "C"
