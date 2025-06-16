e1c5ec099092fcbdcc758a280bc7c25196705360
Phuong Nguyen <phuonguyen@nvidia.com>
2025-06-16 05:40:32 -0700

    include previously accidentally excluded tests
    Signed-off-by: Phuong Nguyen <phuonguyen@nvidia.com>

--- examples/jax/encoder/run_test_multiprocessing_encoder.sh
+++ examples/jax/encoder/run_test_multiprocessing_encoder.sh
@@ -6,13 +6,13 @@ NUM_GPUS=${NUM_GPUS:-$(nvidia-smi -L | wc -l)}
 
 # Define the test cases to run
 TEST_CASES=(
-# "test_te_bf16"
+"test_te_bf16"
 "test_te_delayed_scaling_fp8"
-# "test_te_current_scaling_fp8"
-# "test_te_mxfp8"
-# "test_te_bf16_shardy"
+"test_te_current_scaling_fp8"
+"test_te_mxfp8"
+"test_te_bf16_shardy"
 "test_te_delayed_scaling_fp8_shardy"
-# "test_te_current_scaling_fp8_shardy"
+"test_te_current_scaling_fp8_shardy"
 )
 
 echo
