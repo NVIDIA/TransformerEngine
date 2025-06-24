feb99d77388740abb57f34f5ebe92568841e2589
Xin Yao <xiny@nvidia.com>
2025-06-24 13:19:24 +0800

    add license
    Signed-off-by: Xin Yao <xiny@nvidia.com>

--- tests/cpp/operator/test_multi_unpadding.cu
+++ tests/cpp/operator/test_multi_unpadding.cu
@@ -1,3 +1,9 @@
+/*************************************************************************
+ * Copyright (c) 2022-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ *
+ * See LICENSE for license information.
+ ************************************************************************/
+
 #include <cstring>
 #include <iomanip>
 #include <iostream>
