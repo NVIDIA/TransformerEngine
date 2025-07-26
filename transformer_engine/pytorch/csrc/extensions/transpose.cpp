de5d05d9475c61bac9e6897a0fd70ae33c3d06c0
Tim Moon <tmoon@nvidia.com>
2025-07-26 01:19:40 +0000

    Add PyTorch extension for swap first dims kernel
    Signed-off-by: Tim Moon <tmoon@nvidia.com>

--- transformer_engine/pytorch/csrc/extensions/transpose.cpp
+++ transformer_engine/pytorch/csrc/extensions/transpose.cpp
@@ -44,5 +44,30 @@ at::Tensor fp8_transpose(at::Tensor input, DType otype, std::optional<at::Tensor
   return out;
 }
 
+at::Tensor swap_first_dims(at::Tensor input, std::optional<at::Tensor> output) {
+  init_extension();
+
+  // Make sure input is contiguous
+  input = input.contiguous();
+
+  // Allocate output tensor if needed
+  if (!output) {
+    auto in_shape = getTensorShape(input);
+    NVTE_CHECK(in_shape.size() >= 2, "Invalid input tensor dimensions (shape=", in_shape, ")");
+    std::vector<int64_t> out_shape_int64(in_shape.begin(), in_shape.end());
+    out_shape_int64[0] = static_cast<int64_t>(in_shape[1]);
+    out_shape_int64[1] = static_cast<int64_t>(in_shape[0]);
+    auto opts = at::TensorOptions().dtype(input.dtype()).device(input.device());
+    output = at::empty(out_shape_int64, opts);
+  }
+
+  // Launch kernel
+  const TensorWrapper te_input = makeTransformerEngineTensor(input);
+  TensorWrapper te_output = makeTransformerEngineTensor(*output);
+  nvte_swap_first_dims(te_input.data(), te_output.data(), at::cuda::getCurrentCUDAStream());
+
+  return std::move(*output);
+}
+
 }  // namespace pytorch
 }  // namespace transformer_engine
