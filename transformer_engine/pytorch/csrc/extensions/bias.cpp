ebf6fda15201d1f46c0a1a3109aaa8db53a65e95
Tim Moon <tmoon@nvidia.com>
2025-07-29 22:50:53 +0000

    Unfused impl for dbias-quantize
    Signed-off-by: Tim Moon <tmoon@nvidia.com>

--- transformer_engine/pytorch/csrc/extensions/bias.cpp
+++ transformer_engine/pytorch/csrc/extensions/bias.cpp
@@ -4,80 +4,87 @@
  * See LICENSE for license information.
  ************************************************************************/
 
-#include "common.h"
-#include "pybind.h"
+#include "extensions.h"
+
+#include <utility>
+#include <vector>
+
+#include <ATen/ATen.h>
+#include <pybind11/pybind11.h>
 #include "transformer_engine/cast.h"
 #include "transformer_engine/transformer_engine.h"
 
+#include "common.h"
+#include "pybind.h"
+
 namespace transformer_engine::pytorch {
 
-std::vector<py::object> bgrad_quantize(const at::Tensor& input, py::handle py_quantizer) {
-  auto quantizer = convert_quantizer(py_quantizer);
+std::vector<py::object> bgrad_quantize(const at::Tensor &grad_output, py::handle quantizer) {
+  using namespace transformer_engine::pytorch::detail;
 
-  auto input_tensor = makeTransformerEngineTensor(input);
+  // Grad output tensor
+  auto grad_output_torch = grad_output.contiguous();
+  const TensorWrapper &grad_output_te = makeTransformerEngineTensor(grad_output_torch);
 
-  auto dbias = allocateTorchTensor(input.size(-1), input_tensor.dtype());
-
-  std::vector<size_t> output_shape;
-  for (auto s : input.sizes()) {
-    output_shape.emplace_back(static_cast<size_t>(s));
-  }
-  auto [out_tensor, out] = quantizer->create_tensor(output_shape, input_tensor.dtype());
+  // Construct grad input and grad bias tensors
+  auto quantizer_cpp = convert_quantizer(quantizer);
+  const auto shape = getTensorShape(grad_output_torch);
+  auto grad_output_dtype = GetTransformerEngineDType(grad_output_torch.scalar_type());
+  auto [grad_input_te, grad_input_py] = quantizer_cpp->create_tensor(shape, grad_output_dtype);
+  auto grad_bias_torch = allocateTorchTensor(shape.back(), grad_output_dtype);
+  auto grad_bias_te = makeTransformerEngineTensor(grad_bias_torch);
 
   // Return immediately if tensors are empty
-  if (product(output_shape) == 0) {
-    return {py::cast(dbias.zero_()), out};
+  if (product(shape) == 0) {
+    grad_bias_torch.zero_();
+    return {py::cast(std::move(grad_bias_torch)), std::move(grad_input_py)};
   }
 
-  auto dbias_tensor = makeTransformerEngineTensor(dbias);
+  // Determine whether to use fused kernel
+  bool with_fused_kernel = false;
+  if (quantizer.is_none()) {
+    // No need for separate quantization step if output is unquantized
+    with_fused_kernel = true;
+  } else if (IsFloat8Quantizers(quantizer.ptr()) || IsMXFP8Quantizers(quantizer.ptr())) {
+    // FP8 delayed scaling and MXFP8 support fused kernel
+    with_fused_kernel = true;
+  }
+
+  // Allocate temporary buffer for grad input if needed for unfused impl
+  TensorWrapper temp_grad_input_te;
+  py::object temp_grad_input_py;
+  if (!with_fused_kernel) {
+    NoneQuantizer q{py::none()};
+    std::tie(temp_grad_input_te, temp_grad_input_py) = q.create_tensor(shape, grad_output_dtype);
+  }
+  TensorWrapper &kernel_grad_input_te = with_fused_kernel ? grad_input_te : temp_grad_input_te;
+
   // Query workspace size and allocate workspace
-  transformer_engine::TensorWrapper workspace;
+  TensorWrapper workspace_te;
+  at::Tensor workspace_torch;
   NVTE_SCOPED_GIL_RELEASE({
-    nvte_quantize_dbias(input_tensor.data(), out_tensor.data(), dbias_tensor.data(),
-                        workspace.data(), at::cuda::getCurrentCUDAStream());
+    nvte_quantize_dbias(grad_output_te.data(), kernel_grad_input_te.data(), grad_bias_te.data(),
+                        workspace_te.data(), at::cuda::getCurrentCUDAStream());
   });
-
-  void* workspace_data_ptr = nullptr;
-  if (workspace.shape().ndim > 0) {
-    auto workspace_data = allocateSpace(workspace.shape(), workspace.dtype());
-    workspace_data_ptr = workspace_data.data_ptr();
+  if (workspace_te.ndim() > 0 && workspace_te.numel() > 0) {
+    workspace_torch = allocateSpace(workspace_te.shape(), workspace_te.dtype());
+    workspace_te = makeTransformerEngineTensor(workspace_torch.data_ptr(),
+                                               workspace_te.shape(),
+                                               workspace_te.dtype());
   }
-  workspace = makeTransformerEngineTensor(workspace_data_ptr, workspace.shape(), workspace.dtype());
 
   // Launch kernel
-  if (detail::IsFloat8CurrentScalingQuantizers(py_quantizer.ptr())) {
-    // my_quantizer here has to be a Float8CurrentScalingQuantizer
-    auto my_quantizer_cs = static_cast<Float8CurrentScalingQuantizer*>(quantizer.get());
-    NVTE_SCOPED_GIL_RELEASE({
-      nvte_compute_amax(input_tensor.data(), out_tensor.data(), at::cuda::getCurrentCUDAStream());
-    });
-    // check if we need to do amax reudction (depending on model parallel configs)
-    if (my_quantizer_cs->with_amax_reduction) {
-      c10::intrusive_ptr<dist_group_type> process_group_ptr = my_quantizer_cs->amax_reduction_group;
-      // construct torch tesnor from NVTEBasicTensor without reallocating memory
-      at::Tensor& amax_tensor_torch = my_quantizer_cs->amax;
-      std::vector<at::Tensor> tensors = {amax_tensor_torch};
-      // allreduce amax tensor
-      c10d::AllreduceOptions allreduce_opts;
-      allreduce_opts.reduceOp = c10d::ReduceOp::MAX;
-      process_group_ptr->allreduce(tensors, allreduce_opts)->wait();
-    }
-    QuantizationConfigWrapper quant_config;
-    quant_config.set_force_pow_2_scales(my_quantizer_cs->force_pow_2_scales);
-    quant_config.set_amax_epsilon(my_quantizer_cs->amax_epsilon);
-    NVTE_SCOPED_GIL_RELEASE({
-      nvte_compute_scale_from_amax(out_tensor.data(), quant_config,
-                                   at::cuda::getCurrentCUDAStream());
-    });
-    // set amax ptr to null in te_output TensorWrapper to avoid atomic amax updates in kernel
-    out_tensor.set_amax(nullptr, DType::kFloat32, out_tensor.defaultShape);
-  }
   NVTE_SCOPED_GIL_RELEASE({
-    nvte_quantize_dbias(input_tensor.data(), out_tensor.data(), dbias_tensor.data(),
-                        workspace.data(), at::cuda::getCurrentCUDAStream());
+    nvte_quantize_dbias(grad_output_te.data(), kernel_grad_input_te.data(), grad_bias_te.data(),
+                        workspace_te.data(), at::cuda::getCurrentCUDAStream());
   });
 
-  return {py::cast(dbias), out};
+  // Quantize grad input if needed
+  if (!with_fused_kernel) {
+    quantizer_cpp->quantize(temp_grad_input_te, grad_input_te);
+  }
+
+  return {py::cast(std::move(grad_bias_torch)), std::move(grad_input_py)};
 }
 
 }  // namespace transformer_engine::pytorch
