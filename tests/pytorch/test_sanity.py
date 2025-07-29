3576b10f791903cfa6a1185b5405597a4b2d0098
Kirthi Shankar Sivamani <ksivamani@nvidia.com>
2025-07-29 19:16:16 +0000

    Add test; tmp
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>

--- tests/pytorch/test_sanity.py
+++ tests/pytorch/test_sanity.py
@@ -90,6 +90,7 @@ model_configs = {
 fp8_recipes = []
 if mxfp8_available:
     fp8_recipes.append(recipe.MXFP8BlockScaling())
+    fp8_recipes.append(recipe.HybridNVFP4BlockScaling())  # TODO: fix check for this
 if fp8_block_scaling_available:
     fp8_recipes.append(recipe.Float8BlockScaling())
 if fp8_available:
