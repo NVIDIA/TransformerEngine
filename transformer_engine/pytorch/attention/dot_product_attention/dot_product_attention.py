2e8acc5dea8aa16ef5a1584150116d4bc50d0185
Charlene Yang <8636796+cyanguwa@users.noreply.github.com>
2025-08-04 11:54:16 -0700

    fix non-determinism and shapes
    Signed-off-by: Charlene Yang <8636796+cyanguwa@users.noreply.github.com>

--- transformer_engine/pytorch/attention/dot_product_attention/dot_product_attention.py
+++ transformer_engine/pytorch/attention/dot_product_attention/dot_product_attention.py
@@ -321,11 +321,11 @@ class DotProductAttention(TransformerEngineBaseModule):
         if self.softmax_type == "learnable":
             self.register_parameter(
                 "softmax_offset",
-                Parameter(torch.empty(self.num_attention_heads // self.tp_size)),
+                Parameter(torch.empty(self.num_attention_heads // self.tp_size, device="cuda")),
                 get_rng_state_tracker=get_rng_state_tracker,
             )
         if self.softmax_type == "off-by-one":
-            self.softmax_offset = torch.zeros(1, self.num_attention_heads // self.tp_size, 1, 1, device="cuda")
+            self.softmax_offset = torch.zeros(self.num_attention_heads // self.tp_size, device="cuda")
         if self.softmax_type == "vanilla":
             self.softmax_offset = None
 
