ef836481acba5b371d0762e042f4e8a5d49342f2
Kshitij Janardan Lakhani <klakhani@nvidia.com>
2025-08-09 11:26:49 -0700

    Move some dist fused attn tests to L2 1. TestReorderCausalLoadBalancing: Run two (non symmetric) BSHD/SBHD data shape combination 2. TestDistributedSelfAttn: Run only one (smaller) BSHD type data shape combination 3. TestDistributedCrossAttn: Run only one (smaller) BSHD type data shape combination 4. TestDistributedContextParallelSelfAttn: Run all cp1 combinations
    Signed-off-by: Kshitij Janardan Lakhani <klakhani@nvidia.com>

--- qa/L1_jax_distributed_unittest/test.sh
+++ qa/L1_jax_distributed_unittest/test.sh
@@ -10,12 +10,12 @@ mkdir -p "$XML_LOG_DIR"
 
 python3 -m pytest -c $TE_PATH/tests/jax/pytest.ini -v --junitxml=$XML_LOG_DIR/pytest.xml $TE_PATH/tests/jax/test_distributed_layernorm*
 python3 -m pytest -c $TE_PATH/tests/jax/pytest.ini -v --junitxml=$XML_LOG_DIR/pytest.xml $TE_PATH/tests/jax/test_distributed_softmax.py
-# Run partial distributed fused attn tests in L1 and the left over in L2
-# TestReorderCausalLoadBalancing: Run only one (non symmetric) BSHD/SBHD data shape combination in L1
+# Run partial distributed fused attn tests in L1
+# TestReorderCausalLoadBalancing: Run only one (non symmetric) BSHD/SBHD data shape combination
 python3 -m pytest -c $TE_PATH/tests/jax/pytest.ini -v --junitxml=$XML_LOG_DIR/pytest.xml $TE_PATH/tests/jax/test_distributed_fused_attn.py -k "TestReorderCausalLoadBalancing and 3-32-8-64"
-# TestDistributedSelfAttn: Run only one (larger) BSHD type data shape combination in L1
+# TestDistributedSelfAttn: Run only one (larger) BSHD type data shape combination
 python3 -m pytest -c $TE_PATH/tests/jax/pytest.ini -v --junitxml=$XML_LOG_DIR/pytest.xml $TE_PATH/tests/jax/test_distributed_fused_attn.py -k "TestDistributedSelfAttn and 32-1024-16-128"
-# TestDistributedCrossAttn: Run only one (larger) BSHD type data shape combination in L1
+# TestDistributedCrossAttn: Run only one (larger) BSHD type data shape combination
 python3 -m pytest -c $TE_PATH/tests/jax/pytest.ini -v --junitxml=$XML_LOG_DIR/pytest.xml $TE_PATH/tests/jax/test_distributed_fused_attn.py -k "TestDistributedCrossAttn and data_shape1"
-# TestDistributedContextParallelSelfAttn: Run only non cp1 combinations in L1
+# TestDistributedContextParallelSelfAttn: Run only non cp1 combinations
 python3 -m pytest -c $TE_PATH/tests/jax/pytest.ini -v --junitxml=$XML_LOG_DIR/pytest.xml $TE_PATH/tests/jax/test_distributed_fused_attn.py -k "TestDistributedContextParallelSelfAttn and not cp1"
