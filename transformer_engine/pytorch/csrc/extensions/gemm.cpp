cba0f8413903e078b8d00dce130fd6c15a7e5137
Varun Thumbe <vthumbe@nvidia.com>
2025-09-03 06:40:55 +0000

    minor code cleanup
    Signed-off-by: Varun Thumbe <vthumbe@nvidia.com>

--- transformer_engine/pytorch/csrc/extensions/gemm.cpp
+++ transformer_engine/pytorch/csrc/extensions/gemm.cpp
@@ -148,12 +148,17 @@ std::vector<py::object> gemm(py::handle A, bool transa, py::handle B, bool trans
   // 1. Inputs: BF16, Output: FP8 (GEMM output has to be BF16, so FP8 quantization needed after that)
   // 2. Inputs: FP8, Output: FP8 (Current Scaling Quantization used, Output needs to be in BF16 to do a current
   //scaling quantization)
-  bool unfused_quantization_needed = !quantizer.is_none();
+  bool unfused_quantization_needed;
   if (low_precision) {
     // Anything apart from currentscaling quantization can be handled in the fused cublas kernel
     unfused_quantization_needed =
         (IsFloat8CurrentScalingQuantizers(quantizer.ptr()) or IsMXFP8Quantizers(quantizer.ptr()));
   }
+  else
+  {
+    unfused_quantization_needed = !quantizer.is_none();
+  }
+
   if (unfused_quantization_needed) {
     NoneQuantizer q{none};
     std::tie(unquantized_D_tensor, unquantized_out) = q.create_tensor(D_shape, output_dtype);
