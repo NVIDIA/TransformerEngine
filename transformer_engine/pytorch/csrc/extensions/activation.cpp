1f2c65bda2a75e974ba337196f22bf9b16006e95
Varun Thumbe <vthumbe@nvidia.com>
2025-09-08 23:26:33 +0000

    accidentally removed the copyright
    Signed-off-by: Varun Thumbe <vthumbe@nvidia.com>

--- transformer_engine/pytorch/csrc/extensions/activation.cpp
+++ transformer_engine/pytorch/csrc/extensions/activation.cpp
@@ -1,3 +1,8 @@
+/*************************************************************************
+ * Copyright (c) 2022-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ *
+ * See LICENSE for license information.
+ ************************************************************************/
 #include "../extensions.h"
 #include "common.h"
 #include "pybind.h"
