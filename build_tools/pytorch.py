af8c226c456535617759339901994c179aa44c3b
Pawel Gadzinski <pgadzinski@nvidia.com>
2025-09-15 08:42:50 +0000

    code drop
    Signed-off-by: Pawel Gadzinski <pgadzinski@nvidia.com>

--- build_tools/pytorch.py
+++ build_tools/pytorch.py
@@ -14,12 +14,12 @@ from typing import List
 
 def install_requirements() -> List[str]:
     """Install dependencies for TE/PyTorch extensions."""
-    return ["torch>=2.1", "einops", "onnxscript==0.3.1", "onnx"]
+    return ["torch>=2.1", "einops", "onnxscript==0.3.1", "onnx", "nvdlfw-inspect"]
 
 
 def test_requirements() -> List[str]:
-    """Test dependencies for TE/JAX extensions."""
-    return ["numpy", "torchvision", "transformers"]
+    """Test dependencies for TE/PyTorch extensions."""
+    return ["numpy", "torchvision", "transformers", "onnxruntime==1.20.1", "onnxruntime_extensions==0.13.0"]
 
 
 def setup_pytorch_extension(
