bug in dbias computation
Signed-off-by: Varun Thumbe <vthumbe@nvidia.com>

transformer_engine/jax/cpp_extensions/activation.py
@@ -1281,6 +1281,7 @@ def quantize_dact_dbias(
         )
 
     is_gated = act_len == 2
+    print(f"is_gated: {is_gated}, act_len: {act_len}")
     # TE/common does not support DelayedScaling2x for gated-act yet
     if is_gated:
         war_output = try_apply_delayed_scaling_2x_war(
@@ -1299,8 +1300,8 @@ def quantize_dact_dbias(
     if quantizer.scaling_mode == ScalingMode.CURRENT_TENSOR_SCALING:
         # Current scaling does not support fused operations. Perform dact in higher precision then quantize after.
         out = dact_lu(
-            dz=dz,
-            x=x,
+            dz=dz.astype(jnp.float32),
+            x=x.astype(jnp.float32),
             activation_type=activation_type,
             quantizer=None,
             act_params=act_params,