b3eb60ab0d7fdf3909a45611214da0f45bbd5530
Tim Moon <tmoon@nvidia.com>
2025-10-04 01:59:08 +0000

    Require cuDNN 9.14.0+ for fused attention with FP8 current scaling
    Signed-off-by: Tim Moon <tmoon@nvidia.com>

--- transformer_engine/pytorch/attention/dot_product_attention/utils.py
+++ transformer_engine/pytorch/attention/dot_product_attention/utils.py
@@ -472,10 +472,13 @@ def get_attention_backend(
         if (
             use_fused_attention
             and fp8_recipe.float8_current_scaling()
-            and device_compute_capability < (10, 0)
         ):
-            logger.debug("Disabling FusedAttention for FP8 current scaling on arch < sm100")
-            use_fused_attention = False
+            if device_compute_capability < (10, 0):
+                logger.debug("Disabling FusedAttention for FP8 current scaling on arch < sm100")
+                use_fused_attention = False
+            elif cudnn_version < (9, 14, 0):
+                logger.debug("Disabling FusedAttention for FP8 current scaling with cuDNN < 9.14.0")
+                use_fused_attention = False
 
     # Filter: KV cache
     # backend  | precision      |    KV cache     | architecture | qkv_format    | page_size
