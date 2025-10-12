reduce ew kernels; fix causal masks; add more tests
Signed-off-by: Charlene Yang <8636796+cyanguwa@users.noreply.github.com>

tests/pytorch/attention/test_attention.py
@@ -267,15 +267,15 @@ def test_dpa_checkpoint(dtype, model_configs, model):
 
 
 model_configs_max_score = {
-    #     test:             b,  h, hg,  d
+    # test: ModelConfig(b, sq, hq, dqk)
     "max_score_1_0": ModelConfig(8, 128, 16, 64),
     "max_score_1_1": ModelConfig(4, 128, 16, 64, max_seqlen_kv=256),
-    "max_score_2_0": ModelConfig(2, 2048, 24, 128),
+    "max_score_2_0": ModelConfig(2, 2048, 24, 128, attn_mask_type="causal"),
     "max_score_2_1": ModelConfig(1, 2048, 24, 128, max_seqlen_kv=4096),
-    "max_score_3_0": ModelConfig(8, 1, 16, 128, max_seqlen_kv=2048),
+    "max_score_3_0": ModelConfig(8, 1, 16, 128, max_seqlen_kv=2048, attn_mask_type="padding_causal"),
     "max_score_3_1": ModelConfig(8, 1, 16, 256, max_seqlen_kv=2048),
     "max_score_4_0": ModelConfig(8, 1, 16, 192, max_seqlen_kv=2048),
-    "max_score_4_1": ModelConfig(8, 128, 16, 192, max_seqlen_kv=2048),
+    "max_score_4_1": ModelConfig(8, 128, 16, 192, max_seqlen_kv=2048, attn_bias_type="post_scale_bias"),
     "max_score_5_0": ModelConfig(8, 1, 16, 512, max_seqlen_kv=2048),
     "max_score_5_1": ModelConfig(8, 128, 16, 512, max_seqlen_kv=2048),
     "max_score_6_0": ModelConfig(8, 1, 16, 1024, max_seqlen_kv=2048),
@@ -1117,7 +1117,7 @@ def _run_dot_product_attention(
         k = inp[1]
         v = inp[2]
         d_out = out_grad
-    out, max_score = block(
+    out = block(
         q,
         k,
         v,
@@ -1137,13 +1137,10 @@ def _run_dot_product_attention(
         alibi_slopes=alibi_slopes,
         fast_zero_fill=True,
     )
-    if is_training:
-        out.backward((d_out, torch.zeros(1, device="cuda")))
-
-    if config.return_max_score:
-        out = (out, max_score)
-    else:
+    if not config.return_max_score:
         out = (out, None)
+    if is_training:
+        out[0].backward(d_out)
 
     d_softmax_offset = None
     if is_training and config.softmax_type != "vanilla":

transformer_engine/pytorch/cpp_extensions/fused_attn.py
@@ -325,11 +325,11 @@ def fused_attn_fwd(
     if return_max_score:
         # output_tensors: out [b, sq, h, d] or [sq, b, h, d], Max [b, h, sq, 1], Sum_Exp [b, h, sq, 1]
         stats = output_tensors[1] + torch.log(output_tensors[2])
-        max_score = output_tensors[1].squeeze(-1).to(dtype=output_tensors[0].dtype)
-        max_score = torch.max(max_score, dim=-1)[0]
-        max_score = torch.max(max_score, dim=0)[0]
-        # return out [b, sq, h, d] or [sq, b, h, d], stats [b, h, sq, 1], max_score [h]
-        return output_tensors[0], stats, max_score
+        # Max [b, h, sq, 1] -> max_score [h]
+        max_score = torch.amax(output_tensors[1], dim=(0,2,3)).to(dtype=output_tensors[0].dtype)
+        aux_ctx_tensors = [stats]
+        aux_ctx_tensors.extend(output_tensors[3:])
+        return output_tensors[0], aux_ctx_tensors, max_score
 
     # out, aux_ctx_tensors
     return output_tensors[0], output_tensors[1:], None