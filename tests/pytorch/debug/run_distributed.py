0af7bc6e16d1c22150ce922c438c3b43a658f005
Pawel Gadzinski <pgadzinski@nvidia.com>
2025-10-13 09:19:07 +0000

    fix
    Signed-off-by: Pawel Gadzinski <pgadzinski@nvidia.com>

--- tests/pytorch/debug/run_distributed.py
+++ tests/pytorch/debug/run_distributed.py
@@ -283,8 +283,6 @@ def _compute_dynamic_range(tensor):
 
 @run_debug_test
 def test_log_distributed(parallel_mode, gather_weight, **kwargs):
-    if not fp8_available:
-        return  # skip - test requires FP8
     _prepare_config_test_log_distributed(kwargs["config_file"])
     _init_debug(kwargs["config_file"].name, kwargs["log_dir"], FEATURE_DIRS)
     set_weight_tensor_tp_group_reduce(gather_weight)
@@ -368,8 +366,6 @@ def test_log_distributed(parallel_mode, gather_weight, **kwargs):
 
 @run_debug_test
 def sanity_test_log_quantized_stats(parallel_mode, gather_weight, **kwargs):
-    if not fp8_available:
-        return  # skip - test requires FP8
     from test_log import LOG_QUANTIZED_CONFIG
 
     kwargs["config_file"].write(LOG_QUANTIZED_CONFIG)
@@ -672,11 +668,13 @@ if __name__ == "__main__":
     _init_distributed()
 
     test_log_expert_parallel()
-    for parallel_mode in ["column", "row"]:
-        for gather_weight in [True, False]:
-            test_log_distributed(parallel_mode, gather_weight)
 
     if fp8_available:
+        for parallel_mode in ["column", "row"]:
+            for gather_weight in [True, False]:
+                test_log_distributed(parallel_mode, gather_weight)
+
+
         for parallel_mode in ["row", "column"]:
             test_disable_fp8_layer(parallel_mode)
 
