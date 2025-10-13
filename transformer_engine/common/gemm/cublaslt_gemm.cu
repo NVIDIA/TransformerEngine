ab466c892350d41db7ebf605c4ef0a6c904e5dd5
Jeremy Berchtold <jberchtold@nvidia.com>
2025-10-13 09:02:26 -0700

    rename inner dim -> leading dim
    Signed-off-by: Jeremy Berchtold <jberchtold@nvidia.com>

--- transformer_engine/common/gemm/cublaslt_gemm.cu
+++ transformer_engine/common/gemm/cublaslt_gemm.cu
@@ -145,7 +145,7 @@ GemmParam CanonicalizeGemmInput(const transformer_engine::Tensor &A, const cubla
     if (is_fp8_dtype(ret.Atype)) {
       // Requirements from https://docs.nvidia.com/cuda/cublas/#tensor-core-usage
       NVTE_CHECK(ret.lda % 16 == 0,
-                 "Inner dimension requirement on A for FP8 GEMM. Caller must pad.");
+                 "Leading dimension requirement on A for FP8 GEMM. Caller must pad.");
     }
   } else if (nvfp4) {
     // NVFP4 GEMM. Either the pure NVFP4 recipe or the FWD pass of the Hybrid NVFP4/MXFP8 recipe.
@@ -193,7 +193,7 @@ GemmParam CanonicalizeGemmInput(const transformer_engine::Tensor &A, const cubla
 
     // Requirements from https://docs.nvidia.com/cuda/cublas/#tensor-core-usage
     NVTE_CHECK((ret.lda % 16) == 0,
-               "Inner dimension requirement on NVTE_BLOCK_SCALING GEMM. Caller must pad.");
+               "Leading dimension requirement on NVTE_BLOCK_SCALING GEMM. Caller must pad.");
     // Divisibility of 8 derived from FP8 (m * CTypeSize) % 16 == 0 requirement.
     // Smallest supported CType is 2 bytes in this scaling mode.
     NVTE_CHECK((m % 8) == 0,
@@ -226,7 +226,7 @@ GemmParam CanonicalizeGemmInput(const transformer_engine::Tensor &A, const cubla
     if (is_fp8_dtype(ret.Atype)) {
       // Requirements from https://docs.nvidia.com/cuda/cublas/#tensor-core-usage
       NVTE_CHECK(ret.ldb % 16 == 0,
-                 "Inner dimension requirement on B for FP8 GEMM. Caller must pad.");
+                 "Leading dimension requirement on B for FP8 GEMM. Caller must pad.");
     }
   } else if (nvfp4) {
     if (is_B_transposed) {
