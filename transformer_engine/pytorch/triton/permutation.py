4e4571790876c87a3008c18188075cd399295321
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2025-10-20 10:41:03 +0800

    Update permutation.py
    Signed-off-by: fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>

--- transformer_engine/pytorch/triton/permutation.py
+++ transformer_engine/pytorch/triton/permutation.py
@@ -18,6 +18,9 @@ from triton.language.standard import _log2
 # the issue https://github.com/triton-lang/triton/issues/3698
 
 
+get_int_dtype_wrapped = triton.constexpr_function(core.get_int_dtype)
+
+
 @triton.jit
 def _compare_and_swap(x, indices, flip, i: tl.constexpr, n_dims: tl.constexpr):
     n_outer: tl.constexpr = x.numel >> n_dims
@@ -37,7 +40,7 @@ def _compare_and_swap(x, indices, flip, i: tl.constexpr, n_dims: tl.constexpr):
     l_indice = tl.reshape(tl.broadcast_to(tl.sum(z * (1 - mask), 1)[:, None, :], shape), x.shape)
     r_indice = tl.reshape(tl.broadcast_to(tl.sum(z * mask, 1)[:, None, :], shape), x.shape)
 
-    idtype = core.get_int_dtype(bitwidth=x.dtype.primitive_bitwidth, signed=True)
+    idtype = get_int_dtype_wrapped(bitwidth=x.dtype.primitive_bitwidth, signed=True)
 
     il_value = l_value.to(idtype, bitcast=True)
     ir_value = r_value.to(idtype, bitcast=True)
