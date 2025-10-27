d7c9777e611a90337fffb0482a62ee2b60ef0353
Kirthi Shankar Sivamani <ksivamani@nvidia.com>
2025-10-27 18:13:30 -0400

    Remove `nvidia-mathdx` dependency (#2295)
    * Remove nvidia-mathdx dep
    
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>
    
    * Fix SR
    
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>
    
    * Add comment
    
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>
    
    ---------
    
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>

--- build_tools/wheel_utils/build_wheels.sh
+++ build_tools/wheel_utils/build_wheels.sh
@@ -23,7 +23,7 @@ git checkout $TARGET_BRANCH
 git submodule update --init --recursive
 
 # Install deps
-/opt/python/cp310-cp310/bin/pip install cmake pybind11[global] ninja setuptools wheel nvidia-mathdx==25.1.1
+/opt/python/cp310-cp310/bin/pip install cmake pybind11[global] ninja setuptools wheel
 
 if $BUILD_METAPACKAGE ; then
         cd /TransformerEngine
