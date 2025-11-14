6a3893aaa9c1be8af6a8782344ed92b6dcfb65c2
Zhongbo Zhu <zhongboz@nvidia.com>
2025-11-14 11:59:15 -0800

    add larger test case
    Signed-off-by: Zhongbo Zhu <zhongboz@nvidia.com>

--- tests/pytorch/nvfp4/test_nvfp4_group_quantize.py
+++ tests/pytorch/nvfp4/test_nvfp4_group_quantize.py
@@ -247,6 +247,7 @@ def check_group_quantization_nvfp4_versus_reference(
         (1024, 256),
         # larger sizes
         (8192, 1024),
+        (16384, 16384),
     ],
 )
 @pytest.mark.parametrize("x_dtype", [torch.bfloat16], ids=str)
