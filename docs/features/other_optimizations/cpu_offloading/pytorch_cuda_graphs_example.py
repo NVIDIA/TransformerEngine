14e9cee908b97137fecd182ee12a05846114cb02
Pawel Gadzinski <pgadzinski@nvidia.com>
2025-12-15 16:31:34 +0100

    fxi
    Signed-off-by: Pawel Gadzinski <pgadzinski@nvidia.com>

--- docs/features/other_optimizations/cpu_offloading/pytorch_cuda_graphs_example.py
+++ docs/features/other_optimizations/cpu_offloading/pytorch_cuda_graphs_example.py
@@ -4,13 +4,14 @@ from transformer_engine.pytorch import get_cpu_offload_context, make_graphed_cal
 
 # Setup
 num_layers = 12
+offloaded_layers = 3
 layers = [torch.nn.Linear(1024, 1024).cuda() for _ in range(num_layers)]
 
 # Enable offloading with retained buffers for CUDA graphs
 cpu_offload_context, sync_function = get_cpu_offload_context(
     enabled=True,
     model_layers=num_layers,
-    num_layers=num_layers,
+    num_layers=offloaded_layers,
     retain_pinned_cpu_buffers=True,
 )
 
