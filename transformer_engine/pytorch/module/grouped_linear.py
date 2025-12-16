3df4986691959a599a3844eea2f1c6000a5052ac
LucienXian <fl.xian@foxmail.com>
2025-12-16 15:59:04 +0800

    Fix meta device check failure when passing torch.device objects
    Signed-off-by: LucienXian <fl.xian@foxmail.com>

--- transformer_engine/pytorch/module/grouped_linear.py
+++ transformer_engine/pytorch/module/grouped_linear.py
@@ -702,7 +702,8 @@ class GroupedLinear(TransformerEngineBaseModule):
         if self.primary_weights_in_fp8:
             self.init_fp8_metadata(num_gemms=self.num_gemms)
 
-        self.reset_parameters(defer_init=device == "meta")
+        is_meta = torch.device(device).type == 'meta'
+        self.reset_parameters(defer_init=is_meta)
 
         if self.wgrad_store.delay_wgrad_compute():
             for name, param in self.named_parameters():
