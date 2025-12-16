410ab8dad2df5f818b03abe54b4b1e219d9297be
Vladimir Cherepanov <vcherepanov@nvidia.com>
2025-12-15 18:18:43 -0800

    Remove test skip logic completely
    Signed-off-by: Vladimir Cherepanov <vcherepanov@nvidia.com>

--- tests/cpp_distributed/test_comm_gemm.cu
+++ tests/cpp_distributed/test_comm_gemm.cu
@@ -63,12 +63,6 @@ int main(int argc, char* argv[]) {
   return ret;
 }
 
-bool IsMulticastSupported(int device_id) {
-  int supported = 0;
-  CHECK_CU(cuDeviceGetAttribute(&supported, CU_DEVICE_ATTRIBUTE_MULTICAST_SUPPORTED, device_id));
-  return supported;
-}
-
 int GetDeviceComputeCapability(int device_id) {
   int major{};
   int minor{};
@@ -369,13 +363,6 @@ struct GemmAr : public CommGemmFixure {
     nvte_gemm_all_reduce(ctx_, m, n, k, a, b, d, bias, pre_act_out, transa, transb, grad,
                          accumulate, comm_sm_count, stream, kNVTECommGemmAlgoDefault);
   }
-
-#if CUBLASMP_VERSION < 600
-  void SetUp() override {
-    if (!IsMulticastSupported(rank_))
-      GTEST_SKIP() << "Multicast is not supported on device " << rank_;
-  }
-#endif
 };
 
 TEST_P(AgGemm, Gemm) {
