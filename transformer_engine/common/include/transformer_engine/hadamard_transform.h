69d556d8365dbbc0bf460a35b6533bc6441e884c
Zhongbo Zhu <zhongboz@nvidia.com>
2026-01-02 17:15:04 -0800

    format
    Signed-off-by: Zhongbo Zhu <zhongboz@nvidia.com>

--- transformer_engine/common/include/transformer_engine/hadamard_transform.h
+++ transformer_engine/common/include/transformer_engine/hadamard_transform.h
@@ -72,10 +72,10 @@ void nvte_hadamard_transform_cast_fusion_columnwise(const NVTETensor input, NVTE
  *  \param[in]      quant_config    Quantization configuration.
  *  \param[in]      stream          CUDA stream used for the operation.
  */
- void nvte_hadamard_transform_cast_fusion(const NVTETensor input, NVTETensor output,
-    const NVTETensor hadamard_matrix,
-    const NVTEQuantizationConfig quant_config,
-    cudaStream_t stream);
+void nvte_hadamard_transform_cast_fusion(const NVTETensor input, NVTETensor output,
+                                         const NVTETensor hadamard_matrix,
+                                         const NVTEQuantizationConfig quant_config,
+                                         cudaStream_t stream);
 
 /*! \brief Split a tensor along dimension 0 and compute RHT amaxes for each split.
  *
