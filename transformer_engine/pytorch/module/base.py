63cd6d131d85b97a79c41d8f0bf40fea1e98a2a6
Przemek Tredak <ptredak@nvidia.com>
2026-01-09 16:47:56 -0800

    Fix lint
    Signed-off-by: Przemek Tredak <ptredak@nvidia.com>

--- transformer_engine/pytorch/module/base.py
+++ transformer_engine/pytorch/module/base.py
@@ -10,7 +10,7 @@ import pickle
 import warnings
 from enum import Enum
 from abc import ABC, abstractmethod
-from typing import Any, Dict, Generator, List, Optional, Set, Tuple, Union
+from typing import Any, Dict, Generator, List, Optional, Tuple, Union
 from contextlib import contextmanager
 import logging
 from types import MethodType
@@ -634,9 +634,18 @@ class TransformerEngineBaseModule(torch.nn.Module, ABC):
             TEDebugState.initialize()
 
     def fast_setattr(self, name: str, value: Any) -> None:
+        """
+        Fast version of the Module's set attribute function.
+        Should be used for regular attributes, but not properties nor parameters/buffers.
+        """
         self.__dict__[name] = value
 
     def module_setattr(self, name: str, value: Any) -> None:
+        """
+        Regular version of the Module's set attribute function.
+        Should be used only when the fast version cannot be used - for the properties,
+        parameters and buffers.
+        """
         super().__setattr__(name, value)
 
     def __setattr__(self, name: str, value: Any) -> None:
@@ -1034,7 +1043,7 @@ class TransformerEngineBaseModule(torch.nn.Module, ABC):
         allow_non_contiguous: bool = False,
         allow_different_data_and_param_types: bool = False,
     ) -> torch.Tensor:
-        """Checks and prepare for FWD execution."""
+        """Checks and prepares for FWD execution."""
         self.fast_setattr(
             "allow_different_data_and_param_types", allow_different_data_and_param_types
         )
@@ -1075,6 +1084,10 @@ class TransformerEngineBaseModule(torch.nn.Module, ABC):
         return inp
 
     def end_forward(self):
+        """
+        Required to be called at the end of the forward function to properly handle
+        DelayedScaling metadata handling and the NVTX ranges.
+        """
         delayed_scaling_recipe = self.fp8 and self.fp8_meta["recipe"].delayed()
         if delayed_scaling_recipe and self.fp8 and in_fp8_activation_recompute_phase():
             FP8GlobalStateManager.restore_fp8_meta_tensors(self.fp8_meta)
@@ -1088,6 +1101,7 @@ class TransformerEngineBaseModule(torch.nn.Module, ABC):
         allow_non_contiguous: bool = False,
         allow_different_data_and_param_types: bool = False,
     ) -> Generator[torch.Tensor, None, None]:
+        """Checks and prepares for FWD execution."""
         yield self.prepare_forward(
             inp, num_gemms, allow_non_contiguous, allow_different_data_and_param_types
         )
