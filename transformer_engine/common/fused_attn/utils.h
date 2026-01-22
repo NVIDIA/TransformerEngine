c6a92a4dced73ffabdd41d77bf3bfa2eb67f6f1c
Sudhakar Singh <sudhakars@nvidia.com>
2026-01-22 12:07:20 -0800

    Add support for SWA (left, right) with FusedAttention  (#2477)
    * SWA (left, right) with FusedAttention changes cherry-picked from https://github.com/NVIDIA/TransformerEngine/pull/1369
    
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix test_kv_cache failures
    
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * remove unnecessary comments
    
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * fix some more filter issues, address feedback
    
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * fix for local test case failures - `bottom_right_diagonal` should be calculated in `fused_attn_fwd` call as well
    
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * make conditions more accurate
    
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * add cp tests to test swa (left, right)
    
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * remove dead code and make conditions better
    
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix lint
    
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * feedback form Charlene
    
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * small er
    
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * plumb `bottom_right_diagonal` through jax
    
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * plumb `bottom_right_diagonal` through jax
    
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add missing fields
    
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * use proper mask type in CP
    
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- transformer_engine/common/fused_attn/utils.h
+++ transformer_engine/common/fused_attn/utils.h
@@ -110,6 +110,7 @@ struct FADescriptor_v1 {
   NVTE_Softmax_Type softmax_type;
   std::int64_t window_size_left;
   std::int64_t window_size_right;
+  bool bottom_right_diagonal;
   bool deterministic;
   cudnn_frontend::DataType_t qkv_tensor_type;
   cudnn_frontend::DataType_t o_tensor_type;
@@ -121,15 +122,16 @@ struct FADescriptor_v1 {
     return std::tie(b, h, hg, s_q, s_kv, d_qk, d_v, num_pages_k, num_pages_v, page_size_k,
                     page_size_v, max_pages_per_seq_k, max_pages_per_seq_v, bias_b, bias_h,
                     attnScale, isTraining, dropoutProbability, layout, mask_type, softmax_type,
-                    window_size_left, window_size_right, deterministic, bias_type, qkv_tensor_type,
-                    o_tensor_type, do_tensor_type, dqkv_tensor_type, generate_max_sum_exp) <
+                    window_size_left, window_size_right, bottom_right_diagonal, deterministic,
+                    bias_type, qkv_tensor_type, o_tensor_type, do_tensor_type, dqkv_tensor_type,
+                    generate_max_sum_exp) <
            std::tie(rhs.b, rhs.h, rhs.hg, rhs.s_q, rhs.s_kv, rhs.d_qk, rhs.d_v, rhs.num_pages_k,
                     rhs.num_pages_v, rhs.page_size_k, rhs.page_size_v, rhs.max_pages_per_seq_k,
                     rhs.max_pages_per_seq_v, rhs.bias_b, rhs.bias_h, rhs.attnScale, rhs.isTraining,
                     rhs.dropoutProbability, rhs.layout, rhs.mask_type, rhs.softmax_type,
-                    rhs.window_size_left, rhs.window_size_right, rhs.deterministic, rhs.bias_type,
-                    rhs.qkv_tensor_type, rhs.o_tensor_type, rhs.do_tensor_type,
-                    rhs.dqkv_tensor_type, rhs.generate_max_sum_exp);
+                    rhs.window_size_left, rhs.window_size_right, rhs.bottom_right_diagonal,
+                    rhs.deterministic, rhs.bias_type, rhs.qkv_tensor_type, rhs.o_tensor_type,
+                    rhs.do_tensor_type, rhs.dqkv_tensor_type, rhs.generate_max_sum_exp);
   }
 };
 
