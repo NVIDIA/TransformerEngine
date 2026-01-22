0f0e229b57a2a2357a33b89d274b2bb8b4f955a2
Chen Cui <chcui@nvidia.com>
2026-01-22 12:00:04 -0800

    [PyT] Update THD sink attention logic for cudnn >=9.18.0 (#2568)
    * Update THD sink attention logic for newer cudnn versions
    
    THD Sink attention is supported in 9.18.0
    
    Signed-off-by: Chen Cui <chcui@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * update thd sink attention logic for cp>1
    
    Signed-off-by: Chen Cui <chcui@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add unit test for thd + sink attention
    
    Signed-off-by: Chen Cui <chcui@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * address comments
    
    Signed-off-by: Chen Cui <chcui@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * do not skip thd cp sink attention test
    
    Signed-off-by: Chen Cui <chcui@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * disable deterministic mode for sink attention
    
    Signed-off-by: Chen Cui <chcui@nvidia.com>
    
    ---------
    
    Signed-off-by: Chen Cui <chcui@nvidia.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Charlene Yang <8636796+cyanguwa@users.noreply.github.com>

--- transformer_engine/pytorch/attention/dot_product_attention/utils.py
+++ transformer_engine/pytorch/attention/dot_product_attention/utils.py
@@ -716,22 +716,14 @@ def get_attention_backend(
             )
             use_unfused_attention = False
         if qkv_format == "thd":
-            logger.debug(
-                "Disabling FusedAttention for softmax_type = %s and qkv_format = thd", softmax_type
-            )
-            use_fused_attention = False
-            logger.debug(
-                "Disabling UnfusedDotProductAttention for softmax_type = %s and qkv_format = thd",
-                softmax_type,
-            )
-            use_unfused_attention = False
+            if cudnn_version < (9, 18, 0):
+                logger.debug(
+                    "Disabling FusedAttention for softmax_type = %s, qkv_format = thd and cuDNN"
+                    " version < 9.18",
+                    softmax_type,
+                )
+                use_fused_attention = False
         if context_parallel:
-            logger.debug(
-                "Disabling UnfusedDotProductAttention for context parallelism with softmax_type"
-                " = %s",
-                softmax_type,
-            )
-            use_unfused_attention = False
             if cp_comm_type != "a2a":
                 logger.debug(
                     "Disabling FusedAttention for context parallelism with softmax_type = %s and"
@@ -1049,6 +1041,15 @@ def get_attention_backend(
             )
             use_flash_attention_2 = False
     if use_fused_attention and deterministic:
+        if softmax_type != "vanilla":
+            logger.debug(
+                "Disabling FusedAttention for determinism reasons with softmax_type = %s. "
+                "Sink attention (off-by-one and learnable softmax) requires "
+                "NVTE_ALLOW_NONDETERMINISTIC_ALGO=1",
+                softmax_type,
+            )
+            use_fused_attention = False
+            fused_attention_backend = None
         if fused_attention_backend == FusedAttnBackend["FP8"] and is_training:
             logger.debug("Disabling FusedAttention for determinism reasons with FP8")
             use_fused_attention = False
