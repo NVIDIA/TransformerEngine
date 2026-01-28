f04b094c20d075374baa8b6dcbc566247b32bd36
Paweł Gadziński <62263673+pggPL@users.noreply.github.com>
2026-01-28 01:27:17 +0100

    [PyTorch] ONNX test fix + export for FP8 attention (#2598)
    * jjit bug fix
    
    Signed-off-by: Pawel Gadzinski <pgadzinski@nvidia.com>
    
    * fix'
    
    Signed-off-by: Pawel Gadzinski <pgadzinski@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix
    
    Signed-off-by: Pawel Gadzinski <pgadzinski@nvidia.com>
    
    * fix
    
    Signed-off-by: Pawel Gadzinski <pgadzinski@nvidia.com>
    
    * fixes
    
    Signed-off-by: Pawel Gadzinski <pgadzinski@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * lint fixes
    
    Signed-off-by: Pawel Gadzinski <pgadzinski@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix
    
    Signed-off-by: Pawel Gadzinski <pgadzinski@nvidia.com>
    
    ---------
    
    Signed-off-by: Pawel Gadzinski <pgadzinski@nvidia.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- transformer_engine/pytorch/attention/dot_product_attention/dot_product_attention.py
+++ transformer_engine/pytorch/attention/dot_product_attention/dot_product_attention.py
@@ -1552,7 +1552,9 @@ class DotProductAttention(TransformerEngineBaseModule):
                 )
 
             if use_unfused_attention:
-                allow_emulation = os.getenv("NVTE_UnfusedDPA_Emulate_FP8", "0") == "1"
+                allow_emulation = (
+                    os.getenv("NVTE_UnfusedDPA_Emulate_FP8", "0") == "1" or is_in_onnx_export_mode()
+                )
                 if checkpoint_core_attention:
                     return self._checkpointed_attention_forward(
                         self.unfused_attention,
