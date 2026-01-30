c3769cb799d633af1ea741bfdaf5f05a3584fd09
Paweł Gadziński <62263673+pggPL@users.noreply.github.com>
2026-01-30 10:17:59 -0800

    Fix minimum version of cublas for grouped gemm (#2631)
    * version change
    
    Signed-off-by: Pawel Gadzinski <pgadzinski@nvidia.com>
    
    * fix
    
    Signed-off-by: Pawel Gadzinski <pgadzinski@nvidia.com>
    
    * ifx
    
    Signed-off-by: Pawel Gadzinski <pgadzinski@nvidia.com>
    
    * fix
    
    Signed-off-by: Pawel Gadzinski <pgadzinski@nvidia.com>
    
    * fix
    
    Signed-off-by: Pawel Gadzinski <pgadzinski@nvidia.com>
    
    * fix
    
    Signed-off-by: Pawel Gadzinski <pgadzinski@nvidia.com>
    
    ---------
    
    Signed-off-by: Pawel Gadzinski <pgadzinski@nvidia.com>

--- tests/cpp/operator/test_grouped_gemm.cu
+++ tests/cpp/operator/test_grouped_gemm.cu
@@ -102,8 +102,8 @@ std::vector<std::tuple<size_t, size_t, size_t>> make_shapes(ShapeCase scase) {
 }
 
 void run_grouped_gemm_case(const TestParams& params) {
-#if CUBLAS_VERSION < 130100
-  GTEST_SKIP() << "Grouped GEMM requires cuBLAS 13.1+, but compile-time cuBLAS version is "
+#if CUBLAS_VERSION < 130200
+  GTEST_SKIP() << "Grouped GEMM requires cuBLAS 13.2+, but compile-time cuBLAS version is "
                << CUBLAS_VERSION << ".";
 #else
   if (getDeviceComputeCapability() < blackwellComputeCapability) {
@@ -267,7 +267,7 @@ void run_grouped_gemm_case(const TestParams& params) {
                    atol,
                    rtol);
   }
-#endif  // CUBLAS_VERSION >= 130100
+#endif  // CUBLAS_VERSION >= 130200
 }
 
 class GroupedGemmTest : public ::testing::TestWithParam<TestParams> {};
