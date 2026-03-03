c13fd56a474caad1445764d9778fab7d0daf2daf
Cory Ye <cye@nvidia.com>
2026-03-03 14:05:41 -0800

    Add TP to FSDP and HSDP functional tests.
    Signed-off-by: Cory Ye <cye@nvidia.com>

--- transformer_engine/pytorch/module/layernorm_linear.py
+++ transformer_engine/pytorch/module/layernorm_linear.py
@@ -1107,6 +1107,7 @@ class LayerNormLinear(TransformerEngineBaseModule):
             For example:
             - device_mesh["dp"] for FSDP.
             - device_mesh["dp_cp"] if using CP ranks in FSDP.
+            - device_mesh["dp_shard"] if using HSDP ("dp_replicate", "dp_shard").
             - device_mesh["tp"] if using TP.
             - device_mesh["dp_cp_tp"] if strided-sharding with FSDP-TP.
 
@@ -1484,6 +1485,7 @@ class LayerNormLinear(TransformerEngineBaseModule):
             For example:
                 - device_mesh["dp"] for FSDP.
                 - device_mesh["dp_cp"] if using CP ranks in FSDP.
+                - device_mesh["dp_shard"] if using HSDP ("dp_replicate", "dp_shard").
                 - device_mesh["tp"] if using TP.
                 - device_mesh["dp_cp_tp"] if strided-sharding with FSDP-TP.
         """
--- transformer_engine/pytorch/module/layernorm_mlp.py
+++ transformer_engine/pytorch/module/layernorm_mlp.py
@@ -1730,6 +1730,7 @@ class LayerNormMLP(TransformerEngineBaseModule):
             For example:
             - device_mesh["dp"] for FSDP.
             - device_mesh["dp_cp"] if using CP ranks in FSDP.
+            - device_mesh["dp_shard"] if using HSDP ("dp_replicate", "dp_shard").
             - device_mesh["tp"] if using TP.
             - device_mesh["dp_cp_tp"] if strided-sharding with FSDP-TP.
 
@@ -2047,6 +2048,7 @@ class LayerNormMLP(TransformerEngineBaseModule):
             For example:
                 - device_mesh["dp"] for FSDP.
                 - device_mesh["dp_cp"] if using CP ranks in FSDP.
+                - device_mesh["dp_shard"] if using HSDP ("dp_replicate", "dp_shard").
                 - device_mesh["tp"] if using TP.
                 - device_mesh["dp_cp_tp"] if strided-sharding with FSDP-TP.
         """
