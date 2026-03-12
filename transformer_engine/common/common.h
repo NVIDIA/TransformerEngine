NVIDIA/TransformerEngine

commit 61f95942178b8d0cd3bdad886586c7a65aa1839a

[All] Added better error messages (#2705)
* Added better error messages

Signed-off-by: Przemek Tredak <ptredak@nvidia.com>

* Update transformer_engine/pytorch/distributed.py

Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>
Signed-off-by: Przemyslaw Tredak <ptrendx@gmail.com>

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

* Fixes

Signed-off-by: Przemek Tredak <ptredak@nvidia.com>

---------

Signed-off-by: Przemek Tredak <ptredak@nvidia.com>
Signed-off-by: Przemyslaw Tredak <ptrendx@gmail.com>
Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>
Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

transformer_engine/common/transformer_engine.cpp
@@ -650,7 +650,7 @@ NVTEShape nvte_make_shape(const size_t *data, size_t ndim) {
 NVTEShape nvte_tensor_shape(const NVTETensor tensor) {
   auto *t = transformer_engine::convertNVTETensor(tensor);
   if (t == nullptr) {
-    NVTE_ERROR("Invalid tensor");
+    NVTE_ERROR("Invalid tensor: received null pointer in nvte_tensor_shape");
   }
 
   // Determine tensor shape depending on tensor format
@@ -662,7 +662,7 @@ NVTEShape nvte_tensor_shape(const NVTETensor tensor) {
 NVTEShape nvte_tensor_columnwise_shape(const NVTETensor tensor) {
   auto *t = transformer_engine::convertNVTETensor(tensor);
   if (t == nullptr) {
-    NVTE_ERROR("Invalid tensor");
+    NVTE_ERROR("Invalid tensor: received null pointer in nvte_tensor_columnwise_shape");
   }
   const std::vector<size_t> &shape = t->columnwise_data.shape;
   return nvte_make_shape(shape.data(), shape.size());

transformer_engine/common/fused_attn/fused_attn.cpp
@@ -15,6 +15,88 @@
 #include "fused_attn_fp8.h"
 #include "utils.h"
 
+namespace transformer_engine {
+
+std::string to_string(NVTE_QKV_Layout layout) {
+  switch (layout) {
+    case NVTE_SB3HD:
+      return "NVTE_SB3HD";
+    case NVTE_SBH3D:
+      return "NVTE_SBH3D";
+    case NVTE_SBHD_SB2HD:
+      return "NVTE_SBHD_SB2HD";
+    case NVTE_SBHD_SBH2D:
+      return "NVTE_SBHD_SBH2D";
+    case NVTE_SBHD_SBHD_SBHD:
+      return "NVTE_SBHD_SBHD_SBHD";
+    case NVTE_BS3HD:
+      return "NVTE_BS3HD";
+    case NVTE_BSH3D:
+      return "NVTE_BSH3D";
+    case NVTE_BSHD_BS2HD:
+      return "NVTE_BSHD_BS2HD";
+    case NVTE_BSHD_BSH2D:
+      return "NVTE_BSHD_BSH2D";
+    case NVTE_BSHD_BSHD_BSHD:
+      return "NVTE_BSHD_BSHD_BSHD";
+    case NVTE_T3HD:
+      return "NVTE_T3HD";
+    case NVTE_TH3D:
+      return "NVTE_TH3D";
+    case NVTE_THD_T2HD:
+      return "NVTE_THD_T2HD";
+    case NVTE_THD_TH2D:
+      return "NVTE_THD_TH2D";
+    case NVTE_THD_THD_THD:
+      return "NVTE_THD_THD_THD";
+    case NVTE_SBHD_BSHD_BSHD:
+      return "NVTE_SBHD_BSHD_BSHD";
+    case NVTE_BSHD_SBHD_SBHD:
+      return "NVTE_BSHD_SBHD_SBHD";
+    case NVTE_THD_BSHD_BSHD:
+      return "NVTE_THD_BSHD_BSHD";
+    case NVTE_THD_SBHD_SBHD:
+      return "NVTE_THD_SBHD_SBHD";
+    case NVTE_Paged_KV_BSHD_BSHD_BSHD:
+      return "NVTE_Paged_KV_BSHD_BSHD_BSHD";
+    case NVTE_Paged_KV_BSHD_SBHD_SBHD:
+      return "NVTE_Paged_KV_BSHD_SBHD_SBHD";
+    case NVTE_Paged_KV_SBHD_BSHD_BSHD:
+      return "NVTE_Paged_KV_SBHD_BSHD_BSHD";
+    case NVTE_Paged_KV_SBHD_SBHD_SBHD:
+      return "NVTE_Paged_KV_SBHD_SBHD_SBHD";
+    case NVTE_Paged_KV_THD_BSHD_BSHD:
+      return "NVTE_Paged_KV_THD_BSHD_BSHD";
+    case NVTE_Paged_KV_THD_SBHD_SBHD:
+      return "NVTE_Paged_KV_THD_SBHD_SBHD";
+    default:
+      return "UNKNOWN_QKV_LAYOUT(" + std::to_string(static_cast<int>(layout)) + ")";
+  }
+}
+
+std::string to_string(NVTE_QKV_Format format) {
+  switch (format) {
+    case NVTE_SBHD:
+      return "NVTE_SBHD";
+    case NVTE_BSHD:
+      return "NVTE_BSHD";
+    case NVTE_THD:
+      return "NVTE_THD";
+    case NVTE_BSHD_2SBHD:
+      return "NVTE_BSHD_2SBHD";
+    case NVTE_SBHD_2BSHD:
+      return "NVTE_SBHD_2BSHD";
+    case NVTE_THD_2BSHD:
+      return "NVTE_THD_2BSHD";
+    case NVTE_THD_2SBHD:
+      return "NVTE_THD_2SBHD";
+    default:
+      return "UNKNOWN_QKV_FORMAT(" + std::to_string(static_cast<int>(format)) + ")";
+  }
+}
+
+}  // namespace transformer_engine
+
 // map NVTE_QKV_Layout to NVTE_QKV_Layout_Group
 NVTE_QKV_Layout_Group nvte_get_qkv_layout_group(NVTE_QKV_Layout qkv_layout) {
   switch (qkv_layout) {
@@ -50,7 +132,8 @@ NVTE_QKV_Layout_Group nvte_get_qkv_layout_group(NVTE_QKV_Layout qkv_layout) {
     case NVTE_QKV_Layout::NVTE_Paged_KV_THD_SBHD_SBHD:
       return NVTE_QKV_Layout_Group::NVTE_Paged_KV_HD_HD_HD;
     default:
-      NVTE_ERROR("qkv_layout not supported!");
+      NVTE_ERROR("Unsupported qkv_layout ", transformer_engine::to_string(qkv_layout),
+                 " in nvte_get_qkv_layout_group.");
   }
 }
 
@@ -90,7 +173,8 @@ NVTE_QKV_Format nvte_get_qkv_format(NVTE_QKV_Layout qkv_layout) {
     case NVTE_QKV_Layout::NVTE_Paged_KV_THD_SBHD_SBHD:
       return NVTE_QKV_Format::NVTE_THD_2SBHD;
     default:
-      NVTE_ERROR("qkv_layout not supported!");
+      NVTE_ERROR("Unsupported qkv_layout ", transformer_engine::to_string(qkv_layout),
+                 " in nvte_get_qkv_format.");
   }
 }
 
@@ -109,7 +193,8 @@ NVTE_QKV_Format nvte_get_q_format(NVTE_QKV_Layout qkv_layout) {
     case NVTE_QKV_Format::NVTE_THD_2SBHD:
       return NVTE_QKV_Format::NVTE_THD;
     default:
-      NVTE_ERROR("qkv_layout not supported!");
+      NVTE_ERROR("Unsupported qkv_format ", transformer_engine::to_string(qkv_format),
+                 " in nvte_get_q_format.");
   }
 }
 
@@ -128,7 +213,8 @@ NVTE_QKV_Format nvte_get_kv_format(NVTE_QKV_Layout qkv_layout) {
     case NVTE_QKV_Format::NVTE_THD:
       return NVTE_QKV_Format::NVTE_THD;
     default:
-      NVTE_ERROR("qkv_layout not supported!");
+      NVTE_ERROR("Unsupported qkv_format ", transformer_engine::to_string(qkv_format),
+                 " in nvte_get_kv_format.");
   }
 }
 

transformer_engine/common/fused_router/utils.h
@@ -250,46 +250,49 @@ __device__ inline void naive_topk_and_mask(CompType *scores, int data_size, int 
 }
 
 // Current TE only support float32/bf16/fp16, float64 probs should be considered in the future
-#define TE_ROUTER_PROBS_TYPE_SWITCH_ALL(dtype, type, ...) \
-  switch (dtype) {                                        \
-    using namespace transformer_engine;                   \
-    case DType::kFloat32: {                               \
-      using type = float;                                 \
-      { __VA_ARGS__ }                                     \
-    } break;                                              \
-    case DType::kFloat16: {                               \
-      using type = fp16;                                  \
-      { __VA_ARGS__ }                                     \
-    } break;                                              \
-    case DType::kBFloat16: {                              \
-      using type = bf16;                                  \
-      { __VA_ARGS__ }                                     \
-    } break;                                              \
-    default:                                              \
-      NVTE_ERROR("Invalid type.");                        \
+#define TE_ROUTER_PROBS_TYPE_SWITCH_ALL(dtype, type, ...)                                 \
+  switch (dtype) {                                                                        \
+    using namespace transformer_engine;                                                   \
+    case DType::kFloat32: {                                                               \
+      using type = float;                                                                 \
+      { __VA_ARGS__ }                                                                     \
+    } break;                                                                              \
+    case DType::kFloat16: {                                                               \
+      using type = fp16;                                                                  \
+      { __VA_ARGS__ }                                                                     \
+    } break;                                                                              \
+    case DType::kBFloat16: {                                                              \
+      using type = bf16;                                                                  \
+      { __VA_ARGS__ }                                                                     \
+    } break;                                                                              \
+    default:                                                                              \
+      NVTE_ERROR("Unsupported router probs dtype ", to_string(static_cast<DType>(dtype)), \
+                 ". Expected one of: Float32, Float16, BFloat16.");                       \
   }
 
-#define TE_ROUTER_INDEX_TYPE_SWITCH_ALL(dtype, type, ...) \
-  switch (dtype) {                                        \
-    using namespace transformer_engine;                   \
-    case DType::kInt32: {                                 \
-      using type = int32_t;                               \
-      { __VA_ARGS__ }                                     \
-    } break;                                              \
-    case DType::kInt64: {                                 \
-      using type = int64_t;                               \
-      { __VA_ARGS__ }                                     \
-    } break;                                              \
-    case DType::kBFloat16: {                              \
-      using type = bf16;                                  \
-      { __VA_ARGS__ }                                     \
-    } break;                                              \
-    case DType::kFloat32: {                               \
-      using type = float;                                 \
-      { __VA_ARGS__ }                                     \
-    } break;                                              \
-    default:                                              \
-      NVTE_ERROR("Invalid type.");                        \
+#define TE_ROUTER_INDEX_TYPE_SWITCH_ALL(dtype, type, ...)                                 \
+  switch (dtype) {                                                                        \
+    using namespace transformer_engine;                                                   \
+    case DType::kInt32: {                                                                 \
+      using type = int32_t;                                                               \
+      { __VA_ARGS__ }                                                                     \
+    } break;                                                                              \
+    case DType::kInt64: {                                                                 \
+      using type = int64_t;                                                               \
+      { __VA_ARGS__ }                                                                     \
+    } break;                                                                              \
+    case DType::kBFloat16: {                                                              \
+      using type = bf16;                                                                  \
+      { __VA_ARGS__ }                                                                     \
+    } break;                                                                              \
+    case DType::kFloat32: {                                                               \
+      using type = float;                                                                 \
+      { __VA_ARGS__ }                                                                     \
+    } break;                                                                              \
+    default:                                                                              \
+      NVTE_ERROR("Unsupported router index dtype ", to_string(static_cast<DType>(dtype)), \
+                 ". Expected one of: Int32, Int64, BFloat16, "                            \
+                 "Float32.");                                                             \
   }
 }  // namespace fused_router
 }  // namespace transformer_engine

transformer_engine/common/gemm/cutlass_grouped_gemm.cuh
@@ -326,17 +326,17 @@ void CutlassGroupedGemm(const NVTETensor* A, const NVTETensor* B, NVTETensor* D,
 
   // Check can implement the kernel.
   if (gemm.can_implement(arguments) != cutlass::Status::kSuccess) {
-    NVTE_CHECK(false, "Failed to implement CUTLASS Grouped GEMM");
+    NVTE_ERROR("Failed to implement CUTLASS Grouped GEMM with ", num_gemms, " GEMMs");
   }
 
   // Initialize the kernel.
   if (gemm.initialize(arguments, kernel_workspace_ptr) != cutlass::Status::kSuccess) {
-    NVTE_CHECK(false, "Failed to initialize CUTLASS Grouped GEMM");
+    NVTE_ERROR("Failed to initialize CUTLASS Grouped GEMM with ", num_gemms, " GEMMs");
   }
 
   // Execute the kernel in the current stream.
   if (gemm.run(stream) != cutlass::Status::kSuccess) {
-    NVTE_CHECK(false, "Failed to run CUTLASS Grouped GEMM");
+    NVTE_ERROR("Failed to run CUTLASS Grouped GEMM with ", num_gemms, " GEMMs");
   }
 }
 

transformer_engine/common/normalization/common.cpp
@@ -116,7 +116,9 @@ void TeNormalizationPlan<BackwardKernelParams>::execute(Tensor* z, void* x_dptr,
                                                         void* beta_dptr, void* mean_dptr,
                                                         void* eps_dptr, void* rsigma_dptr,
                                                         void* workspace_dptr, cudaStream_t stream) {
-  NVTE_ERROR("Backward normalization should not call the forward execute function!");
+  NVTE_ERROR(
+      "Backward normalization should not call the forward execute function. "
+      "Use the backward-specific execute overload instead.");
 }
 
 template <typename KernelParamsType>
@@ -165,7 +167,9 @@ void TeNormalizationPlan<ForwardKernelParams>::execute(void* x_dptr, void* gamma
                                                        void* dx_dptr, void* dz_dptr, void* add_dptr,
                                                        void* dbeta_dptr, void* dgamma_dptr,
                                                        void* workspace_dptr, cudaStream_t stream) {
-  NVTE_ERROR("Forward normalization should not call the backward execute function!");
+  NVTE_ERROR(
+      "Forward normalization should not call the backward execute function. "
+      "Use the forward-specific execute overload instead.");
 }
 
 template <>

transformer_engine/common/transpose/quantize_transpose_square_blockwise.cu
@@ -463,7 +463,8 @@ CUtensorMap get_tensor_map(const SimpleTensor& tensor, size_t global_dim_x, size
                 std::is_same_v<OutputType, __nv_fp8_e5m2>) {
     dataType = CUtensorMapDataType::CU_TENSOR_MAP_DATA_TYPE_UINT8;
   } else {
-    NVTE_CHECK(false, "Invalid Output type (must be FP8).");
+    NVTE_ERROR(
+        "Invalid output type for blockwise transpose (must be FP8: Float8E4M3 or Float8E5M2).");
   }
 
   CUtensorMap tensor_map_output_trans{};

transformer_engine/jax/cpp_extensions/attention.py
@@ -165,13 +165,25 @@ class FusedAttnHelper:
             kv_max_seqlen = q_max_seqlen
             num_gqa_groups = attn_heads
             v_head_dim = q_head_dim
-            assert nqkv == 3
+            assert nqkv == 3, (
+                f"Expected nqkv == 3 for qkvpacked layout, but got nqkv={nqkv} from"
+                f" q_aval.shape={q_aval.shape}"
+            )
         elif qkv_layout.is_kvpacked():
             *q_batch_shape, q_max_seqlen, attn_heads, q_head_dim = q_aval.shape
             *kv_batch_shape, kv_max_seqlen, nkv, num_gqa_groups, v_head_dim = k_aval.shape
-            assert q_batch_shape == kv_batch_shape
-            assert q_head_dim == v_head_dim
-            assert nkv == 2
+            assert q_batch_shape == kv_batch_shape, (
+                f"Mismatched batch shapes for kvpacked layout: q_batch_shape={q_batch_shape},"
+                f" kv_batch_shape={kv_batch_shape}"
+            )
+            assert q_head_dim == v_head_dim, (
+                f"Mismatched head dims for kvpacked layout: q_head_dim={q_head_dim},"
+                f" v_head_dim={v_head_dim}"
+            )
+            assert nkv == 2, (
+                f"Expected nkv == 2 for kvpacked layout, but got nkv={nkv} from"
+                f" k_aval.shape={k_aval.shape}"
+            )
         elif qkv_layout.is_separate():
             *q_batch_shape, q_max_seqlen, attn_heads, q_head_dim = q_aval.shape
             *k_batch_shape, k_max_seqlen, k_num_gqa_groups, k_head_dim = k_aval.shape
@@ -244,9 +256,13 @@ class _FusedAttnRNGStateChecker:
             )
             seed = seed.astype(self.rng_state_dtype)
 
-        assert seed.dtype == self.rng_state_dtype
+        assert (
+            seed.dtype == self.rng_state_dtype
+        ), f"Expected seed.dtype={self.rng_state_dtype}, but got seed.dtype={seed.dtype}"
         # Backend takes an int64_t seed, so only the first two u32 elements are taken
-        assert seed.size >= self.seed_size
+        assert (
+            seed.size >= self.seed_size
+        ), f"Expected seed.size >= {self.seed_size}, but got seed.size={seed.size}"
 
         return seed
 
@@ -363,7 +379,9 @@ class FusedAttnFwdPrimitive(BasePrimitive):
         # 32-bit unsigned int to get the buffer size we need in the C++ kernel
         checker = _FusedAttnRNGStateChecker()
         seed_dtype = dtypes.canonicalize_dtype(seed_aval.dtype)
-        assert seed_dtype == checker.rng_state_dtype
+        assert (
+            seed_dtype == checker.rng_state_dtype
+        ), f"Expected seed_dtype={checker.rng_state_dtype}, but got seed_dtype={seed_dtype}"
         rng_state_shape = (seed_aval.shape[0], checker.rng_state_size)
         rng_state_aval = seed_aval.update(shape=rng_state_shape, dtype=checker.rng_state_dtype)
 
@@ -408,11 +426,19 @@ class FusedAttnFwdPrimitive(BasePrimitive):
             shape=wkspace_info[0], dtype=te_dtype_to_jax_dtype(wkspace_info[1])
         )
 
-        assert softmax_offset_aval.dtype == jnp.float32
+        assert (
+            softmax_offset_aval.dtype == jnp.float32
+        ), f"Expected softmax_offset_aval.dtype=float32, but got {softmax_offset_aval.dtype}"
         if config.softmax_type != AttnSoftmaxType.VANILLA_SOFTMAX:
-            assert softmax_offset_aval.shape == (1, attn_heads, 1, 1)
+            assert softmax_offset_aval.shape == (1, attn_heads, 1, 1), (
+                f"Expected softmax_offset_aval.shape=(1, {attn_heads}, 1, 1) for"
+                f" {config.softmax_type}, but got {softmax_offset_aval.shape}"
+            )
         else:
-            assert softmax_offset_aval.shape == (0,)
+            assert softmax_offset_aval.shape == (0,), (
+                "Expected softmax_offset_aval.shape=(0,) for VANILLA_SOFTMAX, but got"
+                f" {softmax_offset_aval.shape}"
+            )
 
         return out_aval, softmax_aux_aval, rng_state_aval, wkspace_aval
 
@@ -533,7 +559,9 @@ class FusedAttnFwdPrimitive(BasePrimitive):
         _kv_segment_pos,
         config: _FusedAttnConfig,
     ):
-        assert FusedAttnFwdPrimitive.inner_primitive is not None
+        assert (
+            FusedAttnFwdPrimitive.inner_primitive is not None
+        ), "FusedAttnFwdPrimitive.inner_primitive has not been registered"
 
         sequence_descriptor = SequenceDescriptor(
             seqlens=(q_seqlen, kv_seqlen),
@@ -628,7 +656,9 @@ class FusedAttnFwdPrimitive(BasePrimitive):
     def batcher(batched_args, batch_dims, *, config):
         # batch_dims: each element is the batch axis (0, ...) or None. Only 0 or None allowed.
         check_valid_batch_dims(batch_dims)
-        assert FusedAttnFwdPrimitive.outer_primitive is not None
+        assert (
+            FusedAttnFwdPrimitive.outer_primitive is not None
+        ), "FusedAttnFwdPrimitive.outer_primitive has not been registered"
         q_bdim, _, _, _, _, seed_bdim, *_ = batch_dims
         # Pass through; segment_ids/segment_pos may have different batch dims (e.g. vmapped ids,
         # replicated pos). get_seqlens_and_offsets() in attention.py handles conversion without expanding.
@@ -780,8 +810,15 @@ class FusedAttnBwdPrimitive(BasePrimitive):
         v_dtype = dtypes.canonicalize_dtype(v_aval.dtype)
         bias_dtype = dtypes.canonicalize_dtype(bias_aval.dtype)
         doutput_dtype = dtypes.canonicalize_dtype(doutput_aval.dtype)
-        assert q_dtype == k_dtype == v_dtype == bias_dtype == doutput_dtype
-        assert q_seqlen_or_cu_seqlen_aval.dtype == kv_seqlen_or_cu_seqlen_aval.dtype
+        assert q_dtype == k_dtype == v_dtype == bias_dtype == doutput_dtype, (
+            f"Mismatched dtypes: q_dtype={q_dtype}, k_dtype={k_dtype}, v_dtype={v_dtype},"
+            f" bias_dtype={bias_dtype}, doutput_dtype={doutput_dtype}"
+        )
+        assert q_seqlen_or_cu_seqlen_aval.dtype == kv_seqlen_or_cu_seqlen_aval.dtype, (
+            "Mismatched seqlen dtypes:"
+            f" q_seqlen_or_cu_seqlen_aval.dtype={q_seqlen_or_cu_seqlen_aval.dtype},"
+            f" kv_seqlen_or_cu_seqlen_aval.dtype={kv_seqlen_or_cu_seqlen_aval.dtype}"
+        )
 
         (
             batch_shape,
@@ -985,7 +1022,9 @@ class FusedAttnBwdPrimitive(BasePrimitive):
         _kv_segment_pos,
         config,
     ):
-        assert FusedAttnBwdPrimitive.inner_primitive is not None
+        assert (
+            FusedAttnBwdPrimitive.inner_primitive is not None
+        ), "FusedAttnBwdPrimitive.inner_primitive has not been registered"
 
         sequence_descriptor = SequenceDescriptor(
             seqlens=(q_seqlen, kv_seqlen),
@@ -1025,7 +1064,9 @@ class FusedAttnBwdPrimitive(BasePrimitive):
             batch, q_max_seqlen, kv_max_seqlen, *_ = FusedAttnHelper.parse_qkv_aval(
                 q, k, v, config.qkv_layout
             )
-            assert len(batch) == 1
+            assert (
+                len(batch) == 1
+            ), f"Expected len(batch) == 1, but got len(batch)={len(batch)}, batch={batch}"
             kv_batch = q_batch = batch[0]
 
             # Gather valid q_seqlen, which is greater than 0
@@ -1084,7 +1125,9 @@ class FusedAttnBwdPrimitive(BasePrimitive):
     @staticmethod
     def batcher(batched_args, batch_dims, *, config):
         check_valid_batch_dims(batch_dims)
-        assert FusedAttnBwdPrimitive.outer_primitive is not None
+        assert (
+            FusedAttnBwdPrimitive.outer_primitive is not None
+        ), "FusedAttnBwdPrimitive.outer_primitive has not been registered"
         q_bdim, k_bdim, v_bdim, bias_bdim, softmax_offset_bdim, *_ = batch_dims
         # Pass through; segment_ids/segment_pos may have different batch dims. Conversion is in attention.py.
         out_bdims = q_bdim, k_bdim, v_bdim, bias_bdim, softmax_offset_bdim
@@ -3398,7 +3441,9 @@ def fused_attn_fwd(
         raise ValueError(f"Unknown {qkv_layout=}")
 
     if attn_bias_type == AttnBiasType.NO_BIAS:
-        assert bias is None
+        assert (
+            bias is None
+        ), f"bias must be None when attn_bias_type is NO_BIAS, but got bias={bias}"
         bias = jnp.zeros(0, dtype=qkv[0].dtype)
 
     if softmax_offset is None:
@@ -3416,10 +3461,16 @@ def fused_attn_fwd(
                 softmax_offset, (None, HEAD_AXES, None, None)
             )
         else:
-            assert softmax_type == AttnSoftmaxType.VANILLA_SOFTMAX
+            assert softmax_type == AttnSoftmaxType.VANILLA_SOFTMAX, (
+                "Expected VANILLA_SOFTMAX when softmax_offset is None and not OFF_BY_ONE_SOFTMAX,"
+                f" but got softmax_type={softmax_type}"
+            )
             softmax_offset = jnp.zeros(0, dtype=jnp.float32)
     else:
-        assert softmax_offset.dtype == jnp.float32
+        assert softmax_offset.dtype == jnp.float32, (
+            "Expected softmax_offset.dtype=float32, but got"
+            f" softmax_offset.dtype={softmax_offset.dtype}"
+        )
         # Shard by heads dimension if not VANILLA_SOFTMAX
         if softmax_type != AttnSoftmaxType.VANILLA_SOFTMAX:
             softmax_offset = with_sharding_constraint_by_logical_axes(
@@ -3558,7 +3609,9 @@ def fused_attn_bwd(
         raise ValueError(f"Unknown {qkv_layout=}")
 
     if attn_bias_type == AttnBiasType.NO_BIAS:
-        assert bias is None
+        assert (
+            bias is None
+        ), f"bias must be None when attn_bias_type is NO_BIAS, but got bias with type={type(bias)}"
         bias = jnp.zeros(0, dtype=qkv[0].dtype)
 
     if softmax_offset is None:

commit e96f89d4d28dbe37b73c1145aea0a5b400713e7e

[Common] Fix linker error for to_string(DType) in distributed tests
Make transformer_engine::to_string(DType) inline in common.h so that
translation units outside libtransformer_engine.so can resolve it
without requiring the symbol to be exported.

Regression introduced by 61f95942 which added to_string(DType) calls
into TRANSFORMER_ENGINE_TYPE_SWITCH_* macros, causing test object files
to reference the symbol that the linker version script hides.

Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>

transformer_engine/common/common.h
@@ -41,7 +41,34 @@ static_assert(NVTE_BUILD_NUM_PHILOX_ROUNDS > 0,
 
 namespace transformer_engine {
 
-std::string to_string(const DType type);
+inline std::string to_string(const DType type) {
+  switch (type) {
+    case DType::kByte:
+      return "Byte";
+    case DType::kBFloat16:
+      return "BFloat16";
+    case DType::kFloat16:
+      return "Float16";
+    case DType::kFloat32:
+      return "Float32";
+    case DType::kFloat8E4M3:
+      return "Float8E4M3";
+    case DType::kFloat8E5M2:
+      return "Float8E5M2";
+    case DType::kFloat8E8M0:
+      return "Float8E8M0";
+    case DType::kFloat4E2M1:
+      return "Float4E2M1";
+    case DType::kInt16:
+      return "Int16";
+    case DType::kInt32:
+      return "Int32";
+    case DType::kInt64:
+      return "Int64";
+    default:
+      return std::string("Invalid type ") + std::to_string(static_cast<int>(type));
+  }
+}
 std::string to_string(const NVTEScalingMode &mode);
 
 inline std::string to_string_like(const DType &val) { return to_string(val); }

transformer_engine/common/transformer_engine.cpp
@@ -33,34 +33,6 @@ size_t typeToSize(const DType type) {
   return typeToNumBits(type) / 8;
 }
 
-std::string to_string(const DType type) {
-  switch (type) {
-    case DType::kByte:
-      return "Byte";
-    case DType::kBFloat16:
-      return "BFloat16";
-    case DType::kFloat16:
-      return "Float16";
-    case DType::kFloat32:
-      return "Float32";
-    case DType::kFloat8E4M3:
-      return "Float8E4M3";
-    case DType::kFloat8E5M2:
-      return "Float8E5M2";
-    case DType::kFloat8E8M0:
-      return "Float8E8M0";
-    case DType::kFloat4E2M1:
-      return "Float4E2M1";
-    case DType::kInt16:
-      return "Int16";
-    case DType::kInt32:
-      return "Int32";
-    case DType::kInt64:
-      return "Int64";
-    default:
-      return concat_strings("Invalid type ", static_cast<int>(type));
-  }
-}
 
 std::string to_string(const NVTEScalingMode &mode) {
   switch (mode) {