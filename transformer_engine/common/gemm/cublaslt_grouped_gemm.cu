f17e674f8319628802e0d1f31708f5a2730fcc8a
Kirthi Shankar Sivamani <ksivamani@nvidia.com>
2026-03-17 01:30:49 +0530

    Update transformer_engine/common/gemm/cublaslt_grouped_gemm.cu
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>

--- transformer_engine/common/gemm/cublaslt_grouped_gemm.cu
+++ transformer_engine/common/gemm/cublaslt_grouped_gemm.cu
@@ -283,6 +283,7 @@ inline void check_grouped_gemm_requirements(const char *api_name) {
              " requires Blackwell (SM100) or newer architecture.");
   NVTE_CHECK(transformer_engine::cuda::cublas_version() >= CUBLAS_GROUPED_GEMM_VERSION, api_name,
              " requires cuBLAS 13.3+, but run-time cuBLAS version is ",
+             transformer_engine::cuda::cublas_version());
 }
 
 inline transformer_engine::GroupedMatmulConfig parse_grouped_gemm_config(
