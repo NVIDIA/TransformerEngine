f2266f48193fdc78390c788ce4693e6e8db775f8
Charlene Yang <8636796+cyanguwa@users.noreply.github.com>
2026-03-20 14:58:04 -0700

    fix a2a+p2p for sbhd
    Signed-off-by: Charlene Yang <8636796+cyanguwa@users.noreply.github.com>

--- transformer_engine/pytorch/attention/dot_product_attention/context_parallel.py
+++ transformer_engine/pytorch/attention/dot_product_attention/context_parallel.py
@@ -424,13 +424,15 @@ def flash_attn_a2a_communicate(
     cp_stream: torch.cuda.Stream,
     before_attn: bool,
     qkv_format: str = "bshd",
-    cu_seqlens_padded: torch.Tensor = None,
+    cu_seqlens_q_padded: torch.Tensor = None,
+    cu_seqlens_kv_padded: torch.Tensor = None,
+    a2a_input_names: List[str] = None,
 ) -> Union[torch.Tensor, List[torch.Tensor]]:
     """A2A communication for context parallelism."""
-
     assert (
-        qkv_format != "thd" or cu_seqlens_padded is not None
-    ), "cu_seqlens_padded is required for THD format!"
+        qkv_format != "thd" or cu_seqlens_q_padded is not None and cu_seqlens_kv_padded is not None
+    ), "cu_seqlens_q_padded and cu_seqlens_kv_padded are required for THD format!"
+    assert a2a_input_names in [["q", "k", "v"], ["out"], ["dout"], ["dq", "dk", "dv"]], "a2a_input_names must be one of ['q', 'k', 'v'], ['out'], ['dout'], ['dq', 'dk', 'dv']!"
     a2a_inputs = [a2a_inputs] if not isinstance(a2a_inputs, list) else a2a_inputs
     a2a_outputs, a2a_reqs = [None] * len(a2a_inputs), [None] * len(a2a_inputs)
     _, _, head_dim = get_bsh_dims(qkv_format)
@@ -457,6 +459,7 @@ def flash_attn_a2a_communicate(
                             *x.shape[:seq_dim], -1, *x.shape[(seq_dim + 2) :]
                         )
                     else:  # qkv_format == "thd"
+                        cu_seqlens_padded = cu_seqlens_q_padded if a2a_input_names[i - 1] in ["q", "out", "dout", "dq"] else cu_seqlens_kv_padded
                         # [cp, t, h//cp, d] -> [cp*t, h//cp, d]
                         x = x.view(-1, *x.shape[2:])
                         # reorder the sequence chunks
@@ -500,6 +503,7 @@ def flash_attn_a2a_communicate(
                         x, chunk_ids_for_a2a, seq_dim, cp_size
                     )
                 else:  # qkv_format == "thd"
+                    cu_seqlens_padded = cu_seqlens_q_padded if a2a_input_names[i - 1] in ["q", "out", "dout", "dq"] else cu_seqlens_kv_padded
                     # reorder the sequence chunks
                     x = reorder_seq_chunks_before_a2a_after_attn_thd(x, cu_seqlens_padded, cp_size)
                     # [cp*t, h//cp, d] -> [cp, t, h//cp, d]
@@ -1448,7 +1452,7 @@ class AttnFuncWithCPAndKVP2P(torch.autograd.Function):
                     q, k, v = [q_fp8._data, k_fp8._data, v_fp8._data]
             chunk_ids_for_a2a = get_seq_chunk_ids_for_reordering_before_attn(cp_size_a2a, q.device)
             q, k, v = flash_attn_a2a_communicate(
-                [q, k, v], chunk_ids_for_a2a, seq_dim, cp_size_a2a, cp_group_a2a, cp_stream, True
+                [q, k, v], chunk_ids_for_a2a, seq_dim, cp_size_a2a, cp_group_a2a, cp_stream, True, qkv_format=qkv_format, cu_seqlens_q_padded=cu_seqlens_q_padded, cu_seqlens_kv_padded=cu_seqlens_kv_padded, a2a_input_names=["q", "k", "v"]
             )
             if fp8 and is_input_fp8 and not fp8_recipe.mxfp8():
                 q_fp8, k_fp8, v_fp8 = [
@@ -1963,7 +1967,7 @@ class AttnFuncWithCPAndKVP2P(torch.autograd.Function):
         if cp_size_a2a > 1:
             chunk_ids_for_a2a = get_seq_chunk_ids_for_reordering_after_attn(cp_size_a2a, out.device)
             out = flash_attn_a2a_communicate(
-                out, chunk_ids_for_a2a, seq_dim, cp_size_a2a, cp_group_a2a, cp_stream, False
+                out, chunk_ids_for_a2a, seq_dim, cp_size_a2a, cp_group_a2a, cp_stream, False, qkv_format=o_format, cu_seqlens_q_padded=cu_seqlens_q_padded, a2a_input_names=["out"]
             )
             out = out.view(orig_o_shape)
             if return_max_logit:
@@ -2332,13 +2336,16 @@ class AttnFuncWithCPAndKVP2P(torch.autograd.Function):
                 cp_size_a2a, out.device
             )
             dout = flash_attn_a2a_communicate(
-                [dout],
+                dout,
                 chunk_ids_for_a2a,
                 seq_dim,
                 cp_size_a2a,
                 ctx.cp_group_a2a,
                 ctx.cp_stream,
                 True,
+                qkv_format=ctx.qkv_format,
+                cu_seqlens_q_padded=cu_seqlens_q_padded,
+                a2a_input_names=["dout"],
             )
         out = out.view(*ctx.o_shape)
         dout = dout.view(*ctx.o_shape)
@@ -2791,6 +2798,10 @@ class AttnFuncWithCPAndKVP2P(torch.autograd.Function):
                 ctx.cp_group_a2a,
                 ctx.cp_stream,
                 False,
+                qkv_format=ctx.qkv_format,
+                cu_seqlens_q_padded=cu_seqlens_q_padded,
+                cu_seqlens_kv_padded=cu_seqlens_kv_padded,
+                a2a_input_names=["dq", "dk", "dv"],
             )
             if ctx.fp8 and ctx.is_input_fp8 and not ctx.fp8_recipe.mxfp8():
                 dq, dk, dv = [
@@ -3889,7 +3900,9 @@ class AttnFuncWithCPAndQKVOA2A(torch.autograd.Function):
             cp_stream,
             before_attn=True,
             qkv_format=qkv_format,
-            cu_seqlens_padded=cu_seqlens_q_padded,
+            cu_seqlens_q_padded=cu_seqlens_q_padded,
+            cu_seqlens_kv_padded=cu_seqlens_kv_padded,
+            a2a_input_names=["q", "k", "v"],
         )
 
         # softmax_offset: split h
@@ -4015,7 +4028,8 @@ class AttnFuncWithCPAndQKVOA2A(torch.autograd.Function):
             cp_stream,
             before_attn=False,
             qkv_format=o_format,
-            cu_seqlens_padded=cu_seqlens_q_padded,
+            cu_seqlens_q_padded=cu_seqlens_q_padded,
+            a2a_input_names=["out"],
         )
         # [b*s//cp, h, d] -> [b, s//cp, h, d]
         # [s//cp*b, h, d] -> [s//cp, b, h, d]
@@ -4207,7 +4221,8 @@ class AttnFuncWithCPAndQKVOA2A(torch.autograd.Function):
             ctx.cp_stream,
             before_attn=True,
             qkv_format=ctx.o_format,
-            cu_seqlens_padded=cu_seqlens_q_padded,
+            cu_seqlens_q_padded=cu_seqlens_q_padded,
+            a2a_input_names=["dout"],
         )
 
         flash_attn_bwd = None
@@ -4349,7 +4364,9 @@ class AttnFuncWithCPAndQKVOA2A(torch.autograd.Function):
             ctx.cp_stream,
             before_attn=False,
             qkv_format=ctx.dqkv_format,
-            cu_seqlens_padded=cu_seqlens_q_padded,
+            cu_seqlens_q_padded=cu_seqlens_q_padded,
+            cu_seqlens_kv_padded=cu_seqlens_kv_padded,
+            a2a_input_names=["dq", "dk", "dv"],
         )
         dq, dk, dv = [
             x.view(y)
