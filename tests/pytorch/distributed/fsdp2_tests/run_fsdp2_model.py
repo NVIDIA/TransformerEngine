0a088c1d5059a2e0df44f9bd9df19d071bc5712a
Peter St. John <pstjohn@nvidia.com>
2026-04-22 09:48:06 -0600

    [PyT] Fix FSDP2 memory leaks for FP8 weight workspaces and transpose caches (#2805)
    * fixing mem leaks
    
    Signed-off-by: Peter St. John <pstjohn@nvidia.com>
    
    * update xfail message
    
    Signed-off-by: Peter St. John <pstjohn@nvidia.com>
    
    * addressing greptile comments
    
    Signed-off-by: Peter St. John <pstjohn@nvidia.com>
    
    * remove fsdp_safe
    
    Signed-off-by: Peter St. John <pstjohn@nvidia.com>
    
    * fix Float8BlockScaling backward override, unused imports, xfail MXFP8 HSDP
    
    Fix three issues:
    
    1. LayerNormLinear weight quantizer was requesting columnwise usage even
       when backward_override is set. The FSDP2 refactor on this branch
       changed the columnwise condition to `is_grad_enabled and not is_fsdp2`
       but omitted the `backward_override is None` guard. This caused the
       weight to carry unnecessary columnwise data, which the dequantized
       backward path rejects. Also harden Float8BlockQuantizer::create_tensor
       to pass py::none() based on usage flags rather than relying on
       at::Tensor::defined() which is unreliable for default-constructed
       tensors in some PyTorch builds.
    
    2. Remove unused imports: Float8Tensor from linear.py, and the entire
       float8_tensor import line from layernorm_linear.py (Float8Quantizer,
       Float8CurrentScalingQuantizer, Float8Tensor were all unused).
    
    3. xfail MXFP8BlockScaling + fp8_init + HSDP in FSDP2 model tests.
       Pre-existing bug (confirmed on main) where fsdp_post_all_gather
       receives fewer output tensors than fsdp_pre_all_gather sent when the
       HSDP shard dimension is trivial (size 1).
    
    Signed-off-by: Peter St. John <pstjohn@nvidia.com>
    
    * address review comments
    
    Signed-off-by: Peter St. John <pstjohn@nvidia.com>
    
    * address greptile review
    
    Signed-off-by: Peter St. John <pstjohn@nvidia.com>
    
    * no need
    
    Signed-off-by: Varun Thumbe <vthumbe@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * revert grouped linear changes
    
    Signed-off-by: Varun Thumbe <vthumbe@nvidia.com>
    
    * fix all tests
    
    Signed-off-by: Varun Thumbe <vthumbe@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix cudnn commit
    
    Signed-off-by: Varun Thumbe <vthumbe@nvidia.com>
    
    * fix lint
    
    Signed-off-by: Varun Thumbe <vthumbe@nvidia.com>
    
    * revert
    
    Signed-off-by: Varun Thumbe <vthumbe@nvidia.com>
    
    * fix lint
    
    Signed-off-by: Varun Thumbe <vthumbe@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Signed-off-by: Peter St. John <pstjohn@nvidia.com>
    Signed-off-by: Varun Thumbe <vthumbe@nvidia.com>
    Signed-off-by: vthumbe1503 <vthumbe@nvidia.com>
    Co-authored-by: Varun Thumbe <vthumbe@nvidia.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/pytorch/distributed/fsdp2_tests/run_fsdp2_model.py
+++ tests/pytorch/distributed/fsdp2_tests/run_fsdp2_model.py
@@ -370,6 +370,16 @@ NUM_PROCS = int(os.environ.get("WORLD_SIZE", "1"))
 @pytest.mark.parametrize("fp8_init", [False, True])
 @pytest.mark.parametrize("layer_type", ["LayerNormLinear", "TransformerLayer"])
 def test_distributed(recipe_name, fp8_init, sharding_dims, layer_type):
+    if recipe_name == "MXFP8BlockScaling" and fp8_init and len(sharding_dims) == 2:
+        pytest.xfail(
+            "MXFP8BlockScaling + fp8_init + HSDP: fsdp_post_all_gather receives fewer "
+            "all_gather_outputs than the number of tensors sent by fsdp_pre_all_gather "
+            "when the HSDP shard dimension is trivial (size 1). MXFP8 sends 2 tensors "
+            "(data + scale_inv, both uint8) but gets back 1. Float8Tensor avoids this by "
+            "sending only 1 tensor (scale is per-tensor metadata). Fix: concatenate MXFP8 "
+            "data and scale_inv into a single buffer in pre_all_gather, split in post."
+        )
+
     if recipe_name == "Float8BlockScaling" and fp8_init:
         pytest.xfail(
             "Float8BlockScaling + fp8_init: scale inverse padding is not handled "
