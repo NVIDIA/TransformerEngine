56565787a97b7a8feb4dc1f426b85ef9f0b89e4e
Phuong Nguyen <phuonguyen@nvidia.com>
2026-06-08 16:58:00 -0700

    common/ep: fold nvte_ep_* stubs into ep_api.cpp under #if NVTE_WITH_NCCL_EP
    Signed-off-by: Phuong Nguyen <phuonguyen@nvidia.com>

--- transformer_engine/common/ep/ep_api.cpp
+++ transformer_engine/common/ep/ep_api.cpp
@@ -6,13 +6,20 @@
 
 /*! \file ep_api.cpp
  *  \brief nvte_ep_* C API: thin delegations to the EPBackend singleton.
+ *
+ *  When NVTE_WITH_NCCL_EP is undefined, the entry points become throwing
+ *  stubs so framework bindings still link without NCCL EP support.
  */
 
-#include <nccl.h>
 #include <transformer_engine/ep.h>
 
-#include "../common.h"
 #include "../util/logging.h"
+
+#if defined(NVTE_WITH_NCCL_EP)
+
+#include <nccl.h>
+
+#include "../common.h"
 #include "ep_backend.h"
 
 using transformer_engine::ep::EPBackend;
@@ -70,3 +77,54 @@ void nvte_ep_combine_bwd(NVTETensor handle_mem, NVTETensor grad, NVTECommWindow 
   EPBackend::get().combine_bwd(handle_mem_ptr(handle_mem), grad, grad_win, grad_expert_out,
                                grad_expert_out_win, stream);
 }
+
+#else  // !NVTE_WITH_NCCL_EP — throwing stubs.
+
+namespace {
+[[noreturn]] void ep_not_built() {
+  NVTE_ERROR(
+      "NCCL EP is not built into this TransformerEngine. Rebuild TE with "
+      "NVTE_BUILD_WITH_NCCL_EP=1 and CUDA arch >= 90 (e.g. NVTE_CUDA_ARCHS=\"90\").");
+}
+}  // namespace
+
+void nvte_ep_initialize(void* /*ep_comm*/, NVTEEpGroupConfig /*group_config*/) { ep_not_built(); }
+
+void nvte_ep_shutdown(void) {}
+
+size_t nvte_ep_handle_mem_size(NVTEEpLayerConfig /*layer_cfg*/) { ep_not_built(); }
+
+void nvte_ep_prepare(NVTETensor /*handle_mem*/, NVTETensor /*topk_idx*/,
+                     NVTETensor /*token_counts*/, NVTEEpLayerConfig /*layer_cfg*/,
+                     cudaStream_t /*stream*/) {
+  ep_not_built();
+}
+
+void nvte_ep_dispatch(NVTETensor /*handle_mem*/, NVTETensor /*topk_idx*/, NVTETensor /*tokens*/,
+                      NVTECommWindow /*tokens_win*/, NVTETensor /*topk_weights*/,
+                      NVTECommWindow /*topk_weights_win*/, NVTETensor /*recv_tokens*/,
+                      NVTECommWindow /*recv_tokens_win*/, NVTETensor /*recv_topk_weights*/,
+                      NVTECommWindow /*recv_topk_weights_win*/, cudaStream_t /*stream*/) {
+  ep_not_built();
+}
+
+void nvte_ep_combine(NVTETensor /*handle_mem*/, NVTETensor /*expert_out*/,
+                     NVTECommWindow /*expert_out_win*/, NVTETensor /*result*/,
+                     cudaStream_t /*stream*/) {
+  ep_not_built();
+}
+
+void nvte_ep_dispatch_bwd(NVTETensor /*handle_mem*/, NVTETensor /*grad*/,
+                          NVTECommWindow /*grad_win*/, NVTETensor /*g_recv_topk_weights*/,
+                          NVTECommWindow /*g_recv_topk_weights_win*/, NVTETensor /*grad_tokens*/,
+                          NVTETensor /*grad_topk_weights*/, cudaStream_t /*stream*/) {
+  ep_not_built();
+}
+
+void nvte_ep_combine_bwd(NVTETensor /*handle_mem*/, NVTETensor /*grad*/,
+                         NVTECommWindow /*grad_win*/, NVTETensor /*grad_expert_out*/,
+                         NVTECommWindow /*grad_expert_out_win*/, cudaStream_t /*stream*/) {
+  ep_not_built();
+}
+
+#endif  // NVTE_WITH_NCCL_EP
