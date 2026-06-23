a3378db7ea705b3a1146f3b5d325dcd1e53c8acd
Phuong Nguyen <phuonguyen@nvidia.com>
2026-06-23 08:35:11 -0700

    Unify NCCL EP build flag: rename to NVTE_WITH_NCCL_EP, share arch check via nccl_ep_enabled()
    Signed-off-by: Phuong Nguyen <phuonguyen@nvidia.com>

--- build_tools/jax.py
+++ build_tools/jax.py
@@ -10,7 +10,7 @@ from packaging import version
 
 import setuptools
 
-from .utils import get_cuda_include_dirs, all_files_in_dir, debug_build_enabled, setup_mpi_flags
+from .utils import get_cuda_include_dirs, all_files_in_dir, debug_build_enabled, setup_mpi_flags, nccl_ep_enabled
 from typing import List
 
 
@@ -120,7 +120,7 @@ def setup_jax_extension(
     if bool(int(os.getenv("NVTE_WITH_CUBLASMP", 0))):
         cxx_flags.append("-DNVTE_WITH_CUBLASMP")
 
-    if bool(int(os.getenv("NVTE_BUILD_WITH_NCCL_EP", "1"))):
+    if nccl_ep_enabled():
         cxx_flags.append("-DNVTE_WITH_NCCL_EP")
 
     # Define TE/JAX as a Pybind11Extension
