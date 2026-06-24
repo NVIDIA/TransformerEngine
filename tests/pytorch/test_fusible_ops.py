a04aea08040a31ec2b0785fc45a381a0c9a991c5
vthumbe1503 <vthumbe@nvidia.com>
2026-06-23 20:29:48 -0700

    Update tolerance handling for quantization tests
    Set default tolerance values for quantization checks.
    
    Signed-off-by: vthumbe1503 <vthumbe@nvidia.com>

--- tests/pytorch/test_fusible_ops.py
+++ tests/pytorch/test_fusible_ops.py
@@ -3603,7 +3603,7 @@ class TestSequentialModules:
         y_test.backward(dy_test)
 
         # Loose tols for sanity checking
-        tols = quantization_tols(quantization)
+        tols = quantization_tols(quantization) if quantization is not None else {"rtol": 0.125, "atol": 0.25}
 
         # Check values
         assert_close(y_test, y_ref, **tols)
