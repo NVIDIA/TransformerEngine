4329ff84bfbdaa778a33cba02a15fb0807c64689
Kirthi Shankar Sivamani <ksivamani@nvidia.com>
2026-08-04 00:34:01 +0000

    Changed version to 2.17.1
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>

--- build_tools/wheel_utils/launch_x86.sh
+++ build_tools/wheel_utils/launch_x86.sh
@@ -3,7 +3,7 @@
 # See LICENSE for license information.
 
 # Remove leftovers.
-rm -rf x86_wheelhouse_cu12 x86_wheelhouse_cu13
+rm -rf x86_wheelhouse_cu12
 
 # CUDA 12.
 docker build --no-cache \
@@ -17,14 +17,3 @@ docker build --no-cache \
 docker run --runtime=nvidia --gpus=all --ipc=host "x86_wheel"
 docker cp $(docker ps -aq | head -1):/wheelhouse x86_wheelhouse_cu12
 
-# CUDA 13.
-docker build --no-cache \
-  --build-arg CUDA_MAJOR=13 \
-  --build-arg CUDA_MINOR=0 \
-  --build-arg BUILD_METAPACKAGE=false \
-  --build-arg BUILD_COMMON=true \
-  --build-arg BUILD_PYTORCH=false \
-  --build-arg BUILD_JAX=false \
-  -t "x86_wheel" -f build_tools/wheel_utils/Dockerfile.x86 .
-docker run --runtime=nvidia --gpus=all --ipc=host "x86_wheel"
-docker cp $(docker ps -aq | head -1):/wheelhouse x86_wheelhouse_cu13
