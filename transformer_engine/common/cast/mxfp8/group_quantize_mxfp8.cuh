7ee52e28956052da4d8b2570e4815e5980650521
Kaining Zhong <kainingz@nvidia.com>
2026-08-17 23:02:59 +0000

    [Common] Make ptx.cuh arch agnoistic by moving arch specific helpers to a new header
    Signed-off-by: Kaining Zhong <kainingz@nvidia.com>

--- transformer_engine/common/cast/mxfp8/group_quantize_mxfp8.cuh
+++ transformer_engine/common/cast/mxfp8/group_quantize_mxfp8.cuh
@@ -19,7 +19,7 @@
 #include "../../common.h"
 #include "../../util/cuda_runtime.h"
 #include "../../util/math.h"
-#include "../../util/ptx.cuh"
+#include "../../util/ptx_arch_spec.cuh"
 #include "../../utils.cuh"
 #include "../core/common.cuh"
 #include "swizzle.cuh"
