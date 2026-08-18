12f86c1f1d7537fa57d4d39ab751187771fa2685
Kirthi Shankar Sivamani <ksivamani@nvidia.com>
2026-08-18 16:39:48 -0700

    Update min FE version
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>

--- build_tools/pytorch.py
+++ build_tools/pytorch.py
@@ -30,7 +30,7 @@ def install_requirements() -> List[str]:
         "packaging",
         "pydantic",
         "nvdlfw-inspect",
-        "nvidia-cudnn-frontend>=1.25.0",
+        "nvidia-cudnn-frontend>=1.27.0",
     ]
 
 
