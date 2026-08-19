07be442f0a6737b5cb1a7589149e07a882384f91
Francesco Bertolotti <francesco.bertolotti@igenius.ai>
2026-08-19 07:43:10 +0200

    bump FA
    Signed-off-by: Francesco Bertolotti <francesco.bertolotti@igenius.ai>

--- transformer_engine/pytorch/attention/dot_product_attention/utils.py
+++ transformer_engine/pytorch/attention/dot_product_attention/utils.py
@@ -122,7 +122,7 @@ class FlashAttentionUtils:
     version = PkgVersion("0")
     version_required = PkgVersion("2.1.1")
     version_required_blackwell = PkgVersion("2.7.3")
-    max_version = PkgVersion("2.8.4")
+    max_version = PkgVersion("2.8.5")
     v2_plus = False
     v2_1_plus = False
     v2_3_plus = False
